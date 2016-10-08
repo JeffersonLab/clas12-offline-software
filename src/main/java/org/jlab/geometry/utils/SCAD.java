b1a814d46dd104584e449cd6485ef6c0958b7069
Andrey Kim <drewkenjo@users.noreply.github.com>
2016-10-08 02:44:36 -0400

    SCAD export class uses Color class from javafx

--- src/main/java/org/jlab/geometry/utils/SCAD.java
+++ src/main/java/org/jlab/geometry/utils/SCAD.java
@@ -12,6 +12,7 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javafx.scene.paint.Color;
 import org.jlab.detector.geant4.v2.Geant4Factory;
 import org.jlab.detector.volume.Geant4Basic;
 
@@ -21,28 +22,28 @@ import org.jlab.detector.volume.Geant4Basic;
  */
 public class SCAD {
 
-    private static final Map<String, int[]> CLAS12COLORS;
+    private static final Map<String, Color> CLAS12COLORS;
 
     static {
         CLAS12COLORS = new HashMap<>();
-        CLAS12COLORS.put("PCAL_Lead_Layer", new int[]{0x66, 0xff, 0x33});
-        CLAS12COLORS.put("U-view_single_strip", new int[]{0xff, 0x66, 0x33});
-        CLAS12COLORS.put("V-view_single_strip", new int[]{0x66, 0x00, 0xff});
-        CLAS12COLORS.put("W-view_single_strip", new int[]{0x66, 0x00, 0xff});
-        CLAS12COLORS.put("Stainless_Steel", new int[]{0xD4, 0xE3, 0xEE});
-        CLAS12COLORS.put("Last-a-Foam", new int[]{0xEE, 0xD1, 0x8C});
-        CLAS12COLORS.put("eclid1_s", new int[]{0xFC, 0xFF, 0xF0});
-        CLAS12COLORS.put("eclid2_s", new int[]{0xEE, 0xD1, 0x8C});
-        CLAS12COLORS.put("eclid3_s", new int[]{0xFC, 0xFF, 0xF0});
-        CLAS12COLORS.put("lead", new int[]{0x7C, 0xFC, 0x00});
-        CLAS12COLORS.put("U_strip", new int[]{0xff, 0x66, 0x33});
-        CLAS12COLORS.put("V_strip", new int[]{0x66, 0x00, 0xff});
-        CLAS12COLORS.put("W_strip", new int[]{0x66, 0x00, 0xff});
-        CLAS12COLORS.put("panel1a_sector", new int[]{0xff, 0x11, 0xaa});
-        CLAS12COLORS.put("panel1b_sector", new int[]{0x11, 0xff, 0xaa});
-        CLAS12COLORS.put("panel2_sector", new int[]{0xff, 0x11, 0xaa});
-        CLAS12COLORS.put("sc", new int[]{0x00, 0xff, 0xff});
-        CLAS12COLORS.put("lg", new int[]{0x32, 0xCD, 0x32});
+        CLAS12COLORS.put("PCAL_Lead_Layer", Color.web("66ff33"));
+        CLAS12COLORS.put("U-view_single_strip", Color.web("ff6633"));
+        CLAS12COLORS.put("V-view_single_strip", Color.web("6600ff"));
+        CLAS12COLORS.put("W-view_single_strip", Color.web("6600ff"));
+        CLAS12COLORS.put("Stainless_Steel", Color.web("D4E3EE"));
+        CLAS12COLORS.put("Last-a-Foam", Color.web("EED18C"));
+        CLAS12COLORS.put("eclid1_s", Color.web("FCFFF0"));
+        CLAS12COLORS.put("eclid2_s", Color.web("EED18C"));
+        CLAS12COLORS.put("eclid3_s", Color.web("FCFFF0"));
+        CLAS12COLORS.put("lead", Color.web("7CFC00"));
+        CLAS12COLORS.put("U_strip", Color.web("ff6633"));
+        CLAS12COLORS.put("V_strip", Color.web("6600ff"));
+        CLAS12COLORS.put("W_strip", Color.web("6600ff"));
+        CLAS12COLORS.put("panel1a_sector", Color.web("ff11aa"));
+        CLAS12COLORS.put("panel1b_sector", Color.web("11ffaa"));
+        CLAS12COLORS.put("panel2_sector", Color.web("ff11aa"));
+        CLAS12COLORS.put("sc", Color.web("00ffff"));
+        CLAS12COLORS.put("lg", Color.web("32CD32"));
     }
 
     public static String toSCAD(Geant4Factory factory, String dirname) throws IOException {
@@ -54,10 +55,11 @@ public class SCAD {
             String stlpath = detdir.getAbsolutePath() + "/" + compname + ".stl";
             component.toCSG().toStlFile(stlpath);
 
-            for (Map.Entry<String, int[]> entry : CLAS12COLORS.entrySet()) {
+            for (Map.Entry<String, Color> entry : CLAS12COLORS.entrySet()) {
                 if (compname.contains(entry.getKey())) {
-                    stlout.append(String.format("color([%d/255, %d/255, %d/255]) import(\"%s\");\n",
-                            entry.getValue()[0], entry.getValue()[1], entry.getValue()[2], stlpath));
+                    Color col = entry.getValue();
+                    stlout.append(String.format("color([%.4f, %.4f, %.4f]) import(\"%s\");\n",
+                            col.getRed(), col.getGreen(), col.getBlue(), stlpath));
                     break;
                 }
             }
