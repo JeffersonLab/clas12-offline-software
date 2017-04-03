09eb13f667e275b0e94494b281ac48c597fd896a
Joseph Newton <jnewton@jlab.org>
2017-04-03 17:20:01 -0400

    Update EBConstants.java

--- reco/src/main/java/org/jlab/service/eb/EBConstants.java
+++ reco/src/main/java/org/jlab/service/eb/EBConstants.java
@@ -5,6 +5,8 @@
  */
 package org.jlab.service.eb;
 
+import org.jlab.geom.prim.Vector3D;
+
 /**
  *
  * @author gavalian
@@ -22,14 +24,34 @@ public class EBConstants {
     public static final double ECAL_SAMPLINGFRACTION = 0.24;
     public static final double ECAL_SAMPLINGFRACTION_CUT = 0.218;
     
+    public static final double TARGET_POSITION = -4.5;
+    
     public static final double      RF_BUCKET_LENGTH = 2.004;
     public static final double             RF_OFFSET = 0.0;
     public static final int                 RF_SHIFT = 800;   // NOT USED
     public static final int                RF_CYCLES = 80;   
     public static final int                    RF_ID = 1;     // signal ID that will be used in reconstruction
     public static final double           RF_TDC2TIME = 0.0234358;
+    public static final int             RF_LARGE_INTEGER = 800;
     
     public static final double        SPEED_OF_LIGHT = 29.9792;
+    
+    public static final Vector3D PCAL_hitRes = new Vector3D(0.0,0.0,0.0);
+    public static final Vector3D ECIN_hitRes = new Vector3D(0.0,0.0,0.0);
+    public static final Vector3D ECOUT_hitRes = new Vector3D(0.0,0.0,0.0);
+    public static final Vector3D FTOF1A_hitRes = new Vector3D(0.0,0.0,0.0);
+    public static final Vector3D FTOF1B_hitRes = new Vector3D(0.0,0.0,0.0);
+   
+    public static final double PCAL_TimingRes = 0.0;
+    public static final double ECIN_TimingRes = 0.0;
+    public static final double ECOUT_TimingRes = 0.0;
+    public static final double FTOF1A_TimingRes = 0.0;
+    public static final double FTOF1B_TimingRes = 0.0;
+    
+    public static final int HTCC_NPHE_CUT = 2;
+    public static final double HTCC_PION_THRESHOLD = 4.9;
+    
+    public static final int TRIGGER_ID = 11;
     
     
 }
