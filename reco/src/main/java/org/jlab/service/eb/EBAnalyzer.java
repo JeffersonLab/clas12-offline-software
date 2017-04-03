cd78c956d5f0d06053b4485832a1df4c45576ce9
Joseph Newton <jnewton@jlab.org>
2017-04-03 17:19:31 -0400

    Update EBAnalyzer.java

--- reco/src/main/java/org/jlab/service/eb/EBAnalyzer.java
+++ reco/src/main/java/org/jlab/service/eb/EBAnalyzer.java
@@ -28,7 +28,7 @@ import org.jlab.service.pid.*;
 public class EBAnalyzer {
     
     private int[]  pidPositive = new int[]{  -11, 211, 321, 2212};
-    private int[]  pidNegative = new int[]{ 11, -211,-321,-2212};
+    private int[]  pidNegative = new int[]{ -211,-321,-2212};
     
     public EBAnalyzer(){
         
@@ -39,7 +39,7 @@ public class EBAnalyzer {
             DetectorParticle trigger = event.getParticle(0);
             
             //System.out.println(" trigger pid = " + trigger.getPid());
-            if(abs(trigger.getPid())==11){
+            if(trigger.getPid()==EBConstants.TRIGGER_ID){
                 trigger.setBeta(1.0);
                 trigger.setMass(0.0005);
                 
@@ -55,11 +55,13 @@ public class EBAnalyzer {
                     time = trigger.getTime(DetectorType.FTOF, 2);
                     path = trigger.getPathLength(DetectorType.FTOF, 2);
                 }
-                //System.out.println("FTOF time" + time);
-                double t_of_f = path/29.9792;
+
+                double t_of_f = path/EBConstants.SPEED_OF_LIGHT;
                 double start_time = time - t_of_f;
-                double deltatr = start_time - event.getRfTime() - (trigger.vertex().z() - (-4.5))/(29.9792)+800*2.004 + EBConstants.RF_OFFSET;
-                double t_0corr = deltatr%2.004 - 2.004/2;//RF correction term
+                double deltatr = start_time - event.getRfTime() - (trigger.vertex().z() 
+                        - (EBConstants.TARGET_POSITION))/(EBConstants.SPEED_OF_LIGHT)
+                        +EBConstants.RF_LARGE_INTEGER*EBConstants.RF_BUCKET_LENGTH + EBConstants.RF_OFFSET;
+                double t_0corr = deltatr%EBConstants.RF_BUCKET_LENGTH - EBConstants.RF_BUCKET_LENGTH/2;//RF correction term
                 event.setStartTime(start_time + t_0corr);
                 ///System.out.println(start_time);
                 //System.out.println(" TIME = " + t_of_f + "  time from TOF = " + time);
@@ -81,7 +83,7 @@ public class EBAnalyzer {
         
         int np = event.getParticles().size();
         //System.out.println("======================= ANALYSIS");
-        for(int i = 0; i < np; i++) {
+        for(int i = 1; i < np; i++) {
             DetectorParticle p = event.getParticle(i);
             double start_time  = event.getStartTime();
             double beta = 0.0;
@@ -105,17 +107,7 @@ public class EBAnalyzer {
                 //        i,p.vector().mag(),beta,mass));
             }
             //System.out.println("----------------");
-            
-            if(p.hasHit(DetectorType.FTOF, 3)==true){
-                //System.out.println("2");
-                beta = p.getBeta(DetectorType.FTOF, 3, start_time);
-                mass = p.getMass(DetectorType.FTOF, 3, start_time);
-                p.setBeta(beta);
-            }
-            
-            
-            
-            List<PidHypothesis> pidHyp = new ArrayList<PidHypothesis>();
+
             
            
             //p.getBeta(DetectorType.BST, start_time)
@@ -124,39 +116,23 @@ public class EBAnalyzer {
     
     public void assignPids(DetectorEvent event) {
         int np = event.getParticles().size();
-        List<PidHypothesis> pidHyp = new ArrayList<PidHypothesis>();
-        
-        for(int i = 0; i < np; i++){
+        PIDHypothesis pidHyp = new PIDHypothesis();
+        for(int i = 1; i < np; i++){
             DetectorParticle p = event.getParticle(i);
-            //System.out.println("New Particle with Charge = " + p.getCharge());
-            //System.out.println("This particle has a speed (v/c) of  = " + p.getBeta());
-            pidHyp.clear();
             if(p.getCharge()==0) break;
             if(p.getCharge()>0){
                 for(int b = 0; b < this.pidPositive.length; b++){
-                    PidHypothesis hyp = new PidHypothesis();
-                    //System.out.println("ID Analysis..... " + this.pidPositive[b]);
-                    hyp.initParticle(p, this.pidPositive[b]);
-                    //System.out.println(p.getPid());
-                    pidHyp.add(hyp);
+                    pidHyp.setEvent(event);
+                    pidHyp.PIDMatch(p, this.pidPositive[b]); 
+                    //pidHyp.PIDQuality(p,this.pidPositive[b],event);
                 }
-                //System.out.println("BEFORE SORTING");
-                for(PidHypothesis h : pidHyp){
-                    //System.out.println(" hyp = " + h.getchi2()+ " pid = " + h.getPid());
-                }
-                //Collections.sort(pidHyp);
-                //System.out.println("AFTER SORTING");
-                for(PidHypothesis h : pidHyp){
-                    //System.out.println(" hyp = " + h.getchi2() + " pid = " + h.getPid());
-                }
+
                 //p.setPid(pidHyp.get(0).getPid());
             } else {
                 for(int b = 0; b < this.pidNegative.length; b++){
-                    PidHypothesis hyp = new PidHypothesis();
-                    //System.out.println("ID Analysis..... " + this.pidNegative[b]);
-                    hyp.initParticle(p, this.pidNegative[b]);
-                    //System.out.println(p.getPid());
-                    pidHyp.add(hyp);
+                    pidHyp.setEvent(event);
+                    pidHyp.PIDMatch(p, this.pidNegative[b]);
+                    //pidHyp.PIDQuality(p, this.pidNegative[b],event);
                 }
                 //Collections.sort(pidHyp); 
                // System.out.println(pidHyp.get(0).getPid());
@@ -166,106 +142,87 @@ public class EBAnalyzer {
         }
     }
     
-    
-    
-    public static class PidHypothesis {
-        private int   pidTheory = 0;
-        private double betaSigma = 0.0;
-        private double sfSigma = 0.0;
-        private Boolean betaCheck = null;
-        private Boolean sfCheck = null;
-        private Boolean htccSignalCheck = null;
-        private Boolean htccPionThreshold = null;
-        private double chi2 = 0.0;
 
+public class PIDHypothesis{
+    
+        private int theoryPID = -1;
+        private double PIDquality = 0.0;
+        private DetectorEvent event;
         
         
-        public PidHypothesis(){
-            
-        }
+    
+        public PIDHypothesis() {
         
-        public void   initParticle(DetectorParticle p, int pid) {
-            
-            HashMap<Integer,Double> betaDiffs = new HashMap<Integer,Double>(); //Beta Differences
-            betaDiffs.put(0,abs(p.getTheoryBeta(11) - p.getBeta())); //How close is track beta to an electron's?
-            betaDiffs.put(1,abs(p.getTheoryBeta(2212) - p.getBeta()));//How close is track beta to a proton's's?
-            betaDiffs.put(2,abs(p.getTheoryBeta(211) - p.getBeta()));//How close is track beta to a pion's?
-            betaDiffs.put(3,abs(p.getTheoryBeta(321) - p.getBeta()));//How close is track beta to a kaon's?
-            double min = betaDiffs.get(0);
-            int beta_index = 0;
-            for (int i = 0; i <= 3; i++) {
-                if (betaDiffs.get(i) < min) {
-                min = betaDiffs.get(i);
-                beta_index = i;
-                }
             }
+        
+        public void setEvent(DetectorEvent e) {event = e;}
+    
+        public void PIDMatch(DetectorParticle p, int pid) {
             
             double beta = p.getTheoryBeta(pid);
-            this.pidTheory = pid;
-            betaSigma = Math.abs(beta-p.getBeta());
-            betaCheck = (abs(pid)==11 && beta_index==0 && p.getBeta()>0.0) || (abs(pid)==211 && beta_index==2 && p.getBeta()>0.0) 
-                        || (abs(pid)==2212 && beta_index==1 && p.getBeta()>0.0) || (abs(pid)==321 && beta_index==3 && p.getBeta()>0.0);
-            sfCheck = p.getEnergyFraction(DetectorType.EC)>EBConstants.ECAL_SAMPLINGFRACTION_CUT;
-            htccSignalCheck = p.getNphe()>1;
-            htccPionThreshold = p.vector().mag()>4.9;
-            //System.out.println("betaCheck  sfCheck  htccSignalCheck  htccPionThreshold");
-            //System.out.println(betaCheck + "  " + sfCheck + "  " + "  " + htccSignalCheck + "  " + htccPionThreshold);
-            this.PID(p, pid);
-
-        }
-
-        
-        public double getSigma(){return betaSigma;}
-        public int    getPid(){ return pidTheory;}
-        public double getchi2(){ return chi2; }
-        
-
-        public void PID(DetectorParticle p, int pid) {
-                //System.out.println("Pre-PID " + pid);
+            double beta_index = p.MinBetaAssociation(pid); //Associaton for the PID Candidate
+                                                               //closest speed to measured track
+            
+            int pidCandidate = pid;
+            boolean betaCheck = (abs(pid)==211 && beta_index==1 && p.getBeta()>0.0) || 
+                        (abs(pid)==2212 && beta_index==0 && p.getBeta()>0.0) || 
+                        (abs(pid)==321 && beta_index==2 && p.getBeta()>0.0);
+            boolean sfCheck = p.getEnergyFraction(DetectorType.EC)>EBConstants.ECAL_SAMPLINGFRACTION_CUT;
+            boolean htccSignalCheck = p.getNphe()>EBConstants.HTCC_NPHE_CUT;
+            boolean htccPionThreshold = p.vector().mag()>EBConstants.HTCC_PION_THRESHOLD;
+            
             switch(abs(pid)) {
                 case 11:
-                    boolean ElectronCondition1 = betaCheck==true && sfCheck==true && htccSignalCheck==true;
-                    boolean ElectronCondition2 = betaCheck==true && sfCheck==false && htccSignalCheck==true && htccPionThreshold==false;
-                    boolean ElectronCondition3 = betaCheck==false && sfCheck==true && htccSignalCheck==true;
-                    boolean ElectronCondition4 = betaCheck==false && sfCheck==false && htccSignalCheck==true && htccPionThreshold==false;
-                    if(ElectronCondition1==true || ElectronCondition2==true || ElectronCondition3==true || ElectronCondition4==true){
-                        p.setPid(pid);
-                        this.pidTheory = pid;
+                    if(betaCheck==true && htccSignalCheck==true && sfCheck==true){
+                        this.finalizePID(p, pid);
+                        break;
+                    }
+                    if(betaCheck==false && htccSignalCheck==true && sfCheck==true){
+                        this.finalizePID(p, pid);
                         break;
                     }
                 case 211:
-                    boolean PionCondition1 = betaCheck==true && sfCheck==false && htccSignalCheck==false;
-                    boolean PionCondition2 = betaCheck==false && sfCheck==false && htccSignalCheck==true && htccPionThreshold==true;
-                    boolean PionCondition3 = betaCheck==true && sfCheck==false && htccSignalCheck==true && htccPionThreshold==true;
-                    if(PionCondition1==true || PionCondition2==true || PionCondition3==true){
-                        p.setPid(pid);
-                        this.pidTheory = pid;
+                    if(betaCheck==true && htccSignalCheck==true && sfCheck==false 
+                            && htccPionThreshold==true) {
+                        this.finalizePID(p, pid);
                         break;
                     }
+                    if(betaCheck==false && htccSignalCheck==true && sfCheck==false 
+                            && htccPionThreshold==true) {
+                        this.finalizePID(p, pid);
+                        break;
+                    }               
                 case 321:
-                    boolean KaonCondition1 = betaCheck==true && sfCheck==false && htccSignalCheck==false;
-                    if(KaonCondition1==true){
-                       // System.out.println("Negative Kaon?");
-                    p.setPid(pid);
-                    this.pidTheory = pid;
-                    break;
+                    if(betaCheck==true && sfCheck==false && htccSignalCheck==false){
+                        this.finalizePID(p, pid);
+                        break;
                     }
                 case 2212:
-                    boolean ProtonCondition1 = betaCheck==true && sfCheck==false && htccSignalCheck==false;
-                    if(ProtonCondition1==true){
-                    p.setPid(pid);
-                    this.pidTheory = pid;
-                    break;
+                    if(betaCheck==true && sfCheck==false && htccSignalCheck==false){
+                        this.finalizePID(p, pid);
+                        break;
                     }
             }
 
         }
-
-        public int compareTo(Object o) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+                
+        public double PIDQuality(DetectorParticle p, int pid, DetectorEvent event) {
+            
+            return 0.0;
         }
+        
+        private void finalizePID(DetectorParticle p, int pid) {
+                        p.setPid(pid);
+                        theoryPID = pid;
+                        PIDquality = this.PIDQuality(p, pid, event);
+        }
+                
 
-    
-}
+
+
+              
+    }
+
 }
 
