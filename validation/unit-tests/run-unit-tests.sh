6d994496c16a3ecf80654d03aee64f9c34085096
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2017-05-01 18:04:18 -0400

    Added some validation tools
    Former-commit-id: 1e18b441dae9b34e2bc3abba163fc909ae9e14f1

--- validation/unit-tests/run-unit-tests.sh
+++ validation/unit-tests/run-unit-tests.sh
@@ -6,16 +6,22 @@
 COAT="../../coatjava/"
 classPath="$COAT/lib/services/*:$COAT/lib/clas/*:$COAT/lib/utils/*:../lib/*:src/"
 
-# compile tests
+
+# compile codes
 javac -cp $classPath src/events/TestEvent.java
+if [ $? != 0 ] ; then echo "TestEvent compilation failure" ; exit 1 ; fi
+
+javac -cp $classPath src/events/RandomEventGenerator.java
+if [ $? != 0 ] ; then echo "RandomEventGenerator compilation failure" ; exit 1 ; fi
+
 javac -cp $classPath src/dc/DCReconstructionTest.java
+if [ $? != 0 ] ; then echo "DCReconstructionTest compilation failure" ; exit 1 ; fi
+
 
 # run dc junit tests
 java -DCLAS12DIR="$COAT" -Xmx1536m -Xms1024m -cp $classPath org.junit.runner.JUnitCore dc.DCReconstructionTest
-if [ $? != 0 ]
-then
-	echo "dc unit test failure"
-	exit 1
-else
-	echo "dc passed unit tests"
-fi
+if [ $? != 0 ] ; then echo "dc unit test failure" ; exit 1 ; else ; echo "dc passed unit tests" ; fi
+
+
+# run event generator
+#java -DCLAS12DIR="$COAT" -Xmx1536m -Xms1024m -cp $classPath events.RandomEventGenerator
