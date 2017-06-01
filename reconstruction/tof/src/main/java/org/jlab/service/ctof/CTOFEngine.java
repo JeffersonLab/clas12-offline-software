3aa888ceb37aba9019620f0684e1de1c4f493f90
Veronique Ziegler <ziegler@jlab.org>
2017-06-01 12:53:22 -0400

    LSB conversion factor read from ccdb

--- reconstruction/tof/src/main/java/org/jlab/service/ctof/CTOFEngine.java
+++ reconstruction/tof/src/main/java/org/jlab/service/ctof/CTOFEngine.java
@@ -24,176 +24,186 @@ import org.jlab.rec.tof.hit.AHit;
 import org.jlab.rec.tof.hit.ctof.Hit;
 
 /**
- * 
+ *
  * @author ziegler
  *
  */
 public class CTOFEngine extends ReconstructionEngine {
 
-	public CTOFEngine() {
-		super("CTOFRec", "carman, ziegler", "0.4");
-	}
+    public CTOFEngine() {
+        super("CTOFRec", "carman, ziegler", "0.4");
+    }
 
-	CTOFGeant4Factory geometry;
+    CTOFGeant4Factory geometry;
 
-	int Run = -1;
-	RecoBankWriter rbc;
-	@Override
-	public boolean init() {
-		// Load the Constants
-		// if (Constants.CSTLOADED == false) {
-		Constants.Load();
-		// }
-		rbc = new RecoBankWriter();
-		geometry = new CTOFGeant4Factory();
-		// CalibrationConstantsLoader.Load();
-		// }
-		return true;
-	}
+    int Run = -1;
+    RecoBankWriter rbc;
 
-	@Override
-	public boolean processDataEvent(DataEvent event) {
-		setRunConditionsParameters(event);
+    @Override
+    public boolean init() {
+        // Load the Constants
+        // if (Constants.CSTLOADED == false) {
+        Constants.Load();
+        // }
+        rbc = new RecoBankWriter();
+        geometry = new CTOFGeant4Factory();
+        // CalibrationConstantsLoader.Load();
+        // }
+        return true;
+    }
 
-		if (geometry == null) {
-			System.err.println(" CTOF Geometry not loaded !!!");
-			return false;
-		}
-		// Get the list of track lines which will be used for matching the CTOF
-		// hit to the CVT track
-		TrackReader trkRead = new TrackReader();
-		trkRead.fetch_Trks(event);
-		List<Line3d> trkLines = trkRead.get_TrkLines();
-		double[] paths = trkRead.get_Paths();
+    @Override
+    public boolean processDataEvent(DataEvent event) {
+        setRunConditionsParameters(event);
 
-		List<Hit> hits = new ArrayList<Hit>(); // all hits
-		List<Cluster> clusters = new ArrayList<Cluster>(); // all clusters
-		// read in the hits for CTOF
-		HitReader hitRead = new HitReader();
-		hitRead.fetch_Hits(event, geometry, trkLines, paths);
+        if (geometry == null) {
+            System.err.println(" CTOF Geometry not loaded !!!");
+            return false;
+        }
+        // Get the list of track lines which will be used for matching the CTOF
+        // hit to the CVT track
+        TrackReader trkRead = new TrackReader();
+        trkRead.fetch_Trks(event);
+        List<Line3d> trkLines = trkRead.get_TrkLines();
+        double[] paths = trkRead.get_Paths();
 
-		// 1) get the hits
-		List<Hit> CTOFHits = hitRead.get_CTOFHits();
+        List<Hit> hits = new ArrayList<Hit>(); // all hits
+        List<Cluster> clusters = new ArrayList<Cluster>(); // all clusters
+        // read in the hits for CTOF
+        HitReader hitRead = new HitReader();
+        hitRead.fetch_Hits(event, geometry, trkLines, paths);
 
-		// 1.1) exit if hit list is empty
-		if (CTOFHits != null)
-			hits.addAll(CTOFHits);
+        // 1) get the hits
+        List<Hit> CTOFHits = hitRead.get_CTOFHits();
 
-		if (hits.size() == 0) {
-			// System.err.println(" no hits ....");
-			return true;
-		}
+        // 1.1) exit if hit list is empty
+        if (CTOFHits != null) {
+            hits.addAll(CTOFHits);
+        }
 
-		// 1.2) Sort the hits for clustering
-		Collections.sort(hits);
+        if (hits.size() == 0) {
+            // System.err.println(" no hits ....");
+            return true;
+        }
 
-		// 2) find the clusters from these hits
-		ClusterFinder clusFinder = new ClusterFinder();
-		int[] npaddles = Constants.NPAD;
-		int npanels = 1;
-		int nsectors = 1;
-		List<Cluster> CTOFClusters = null;
-		if (CTOFHits != null && CTOFHits.size() > 0)
-			CTOFClusters = clusFinder.findClusters(hits, nsectors, npanels,
-					npaddles);
+        // 1.2) Sort the hits for clustering
+        Collections.sort(hits);
 
-		if (CTOFClusters != null)
-			clusters.addAll(CTOFClusters);
+        // 2) find the clusters from these hits
+        ClusterFinder clusFinder = new ClusterFinder();
+        int[] npaddles = Constants.NPAD;
+        int npanels = 1;
+        int nsectors = 1;
+        List<Cluster> CTOFClusters = null;
+        if (CTOFHits != null && CTOFHits.size() > 0) {
+            CTOFClusters = clusFinder.findClusters(hits, nsectors, npanels,
+                    npaddles);
+        }
 
-		// 2.1) exit if cluster list is empty but save the hits
-		if (clusters.size() == 0) {
-			rbc.appendCTOFBanks( event, hits, null);
-			return true;
-		}
-		// continuing ... there are clusters
-		if (Constants.DEBUGMODE) { // if running in DEBUG MODE print out the
-									// reconstructed info about the hits and the
-									// clusters
-			System.out.println("=============== All Hits ===============");
-			for (Hit hit : hits)
-				hit.printInfo();
-			System.out
-					.println("==================================================");
-			for (Cluster cls : clusters) {
-				cls.printInfo();
-				System.out.println("contains:");
-				for (AHit hit : cls)
-					hit.printInfo();
-				System.out
-						.println("---------------------------------------------");
-			}
-		}
-		//rbc.appendCTOFBanks( event, hits, clusters);
-		rbc.appendCTOFBanks( event, hits, null); // json file needs clusters...
-		return true;
+        if (CTOFClusters != null) {
+            clusters.addAll(CTOFClusters);
+        }
 
-	}
+        // 2.1) exit if cluster list is empty but save the hits
+        if (clusters.size() == 0) {
+            rbc.appendCTOFBanks(event, hits, null);
+            return true;
+        }
+        // continuing ... there are clusters
+        if (Constants.DEBUGMODE) { // if running in DEBUG MODE print out the
+            // reconstructed info about the hits and the
+            // clusters
+            System.out.println("=============== All Hits ===============");
+            for (Hit hit : hits) {
+                hit.printInfo();
+            }
+            System.out
+                    .println("==================================================");
+            for (Cluster cls : clusters) {
+                cls.printInfo();
+                System.out.println("contains:");
+                for (AHit hit : cls) {
+                    hit.printInfo();
+                }
+                System.out
+                        .println("---------------------------------------------");
+            }
+        }
+        //rbc.appendCTOFBanks( event, hits, clusters);
+        rbc.appendCTOFBanks(event, hits, null); // json file needs clusters...
+        return true;
 
-	public void setRunConditionsParameters(DataEvent event) {
-		 
-		if(event.hasBank("RUN::config")==false) {
-			System.err.println("RUN CONDITIONS NOT READ!");
-			return;
-		}
-		boolean isMC = false;
-		boolean isCosmics = false;
-		DataBank bank = event.getBank("RUN::config");
-        
-		if(bank.getByte("type",0)==0)
-			isMC = true;
-		if(bank.getByte("mode",0)==1)
-			isCosmics = true;
-		// force cosmics
-		//isCosmics = true;
-		//System.out.println(bank.getInt("Event")[0]);
-		boolean isCalib = isCosmics;  // all cosmics runs are for calibration right now
-		//
-	
-		// Load the constants
-		//-------------------
-		int newRun = bank.getInt("run",0);
-		
-		if(Run!=newRun) {
-			CCDBConstantsLoader.Load(newRun);
-			DatabaseConstantProvider db = CCDBConstantsLoader.getDB();
-			
-			geometry = new CTOFGeant4Factory();
-		}
-		Run = newRun;
-		
-	}
-	public static void main(String[] args) throws FileNotFoundException,
-			EvioException {
+    }
 
-		String inputFile = "/Users/ziegler/data/hipo/sidis_0100.hipo";
-		// String inputFile = args[0];
-		// String outputFile = args[1];
+    public void setRunConditionsParameters(DataEvent event) {
 
-		System.err.println(" \n[PROCESSING FILE] : " + inputFile);
+        if (event.hasBank("RUN::config") == false) {
+            System.err.println("RUN CONDITIONS NOT READ!");
+            return;
+        }
+        boolean isMC = false;
+        boolean isCosmics = false;
+        DataBank bank = event.getBank("RUN::config");
 
-		CTOFEngine en = new CTOFEngine();
-		en.init();
+        if (bank.getByte("type", 0) == 0) {
+            isMC = true;
+        }
+        if (bank.getByte("mode", 0) == 1) {
+            isCosmics = true;
+        }
+        // force cosmics
+        //isCosmics = true;
+        //System.out.println(bank.getInt("Event")[0]);
+        boolean isCalib = isCosmics;  // all cosmics runs are for calibration right now
+        //
 
-		HipoDataSource reader = new HipoDataSource();
+        // Load the constants
+        //-------------------
+        int newRun = bank.getInt("run", 0);
 
-		int counter = 0;
+        if (Run != newRun) {
+            CCDBConstantsLoader.Load(newRun);
+            DatabaseConstantProvider db = CCDBConstantsLoader.getDB();
 
-		reader.open(inputFile);
-		long t1 = System.currentTimeMillis();
-		while (reader.hasEvent()) {
+            geometry = new CTOFGeant4Factory();
+        }
+        Run = newRun;
 
-			counter++;
-			DataEvent event = reader.getNextEvent();
-			en.processDataEvent(event);
+    }
 
-			if (counter > 50)
-				break;
-			// if(counter%100==0)
-			 System.out.println("run "+counter+" events");
+    public static void main(String[] args) throws FileNotFoundException,
+            EvioException {
 
-		}
-		double t = System.currentTimeMillis() - t1;
-		System.out.println("TOTAL  PROCESSING TIME = " + t);
-	}
+        String inputFile = "/Users/ziegler/data/hipo/sidis_0100.hipo";
+        // String inputFile = args[0];
+        // String outputFile = args[1];
+
+        System.err.println(" \n[PROCESSING FILE] : " + inputFile);
+
+        CTOFEngine en = new CTOFEngine();
+        en.init();
+
+        HipoDataSource reader = new HipoDataSource();
+
+        int counter = 0;
+
+        reader.open(inputFile);
+        long t1 = System.currentTimeMillis();
+        while (reader.hasEvent()) {
+
+            counter++;
+            DataEvent event = reader.getNextEvent();
+            en.processDataEvent(event);
+
+            if (counter > 50) {
+                break;
+            }
+            // if(counter%100==0)
+            System.out.println("run " + counter + " events");
+
+        }
+        double t = System.currentTimeMillis() - t1;
+        System.out.println("TOTAL  PROCESSING TIME = " + t);
+    }
 
 }
