15de1c05845b9c96b4aa9ab3575740e6cec59abb
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2017-06-27 15:31:51 -0400

    Debugging Travis stuff

--- validation/advanced-tests/src/kpptracking/KppTrackingTest.java
+++ validation/advanced-tests/src/kpptracking/KppTrackingTest.java
@@ -3,7 +3,9 @@ package kpptracking;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.jlab.io.base.DataBank;
 import org.jlab.io.base.DataEvent;
+import org.jlab.io.hipo.HipoDataSource;
 
 /**
  *
@@ -14,8 +16,18 @@ public class KppTrackingTest {
 	@Test
 	public void testKppTracking() {
 		
-		int a = 1 + 2;
-		assertEquals(a, (int) 3);
+		HipoDataSource reader = new HipoDataSource();
+		reader.open("out_twoTrackEvents_809.hipo");
+
+		int recCount = 0;
+		while(reader.hasEvent()) {
+			DataEvent event = reader.getNextEvent();
+			if(event.hasBank("REC::Particle")) recCount++;
+		}
+
+		reader.close();
+
+		assertEquals(recCount > 100, true);
 		
 	}
 
