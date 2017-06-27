f8c911d4db46e3c45705e7ee66d9a7b69e9e5d96
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2017-06-27 15:09:38 -0400

    Started adding more validation tests

---
+++ validation/advanced-tests/src/kpptracking/KppTrackingTest.java
@@ -0,0 +1,22 @@
+package kpptracking;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.jlab.io.base.DataEvent;
+
+/**
+ *
+ * @author naharrison
+ */
+public class KppTrackingTest {
+	
+	@Test
+	public void testKppTracking() {
+		
+		int a = 1 + 2;
+		assertEquals(a, (int) 3);
+		
+	}
+
+}
