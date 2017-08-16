9e5eb7c262ef23783676abab01f2918a2c6ea7ba
Nathan Baltzell <baltzell@llama.jlab.org>
2017-08-16 08:44:57 -0400

    validation:  small eb upgrades
    Former-commit-id: 6e81d27b2c5f0e56498f18b5790773d3cef92a26

--- validation/unit-tests/src/eb/EBReconstructionTest.java
+++ validation/unit-tests/src/eb/EBReconstructionTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.*;
 import events.TestEvent;
 
 import org.jlab.io.base.DataEvent;
+import org.jlab.service.dc.DCHBEngine;
+import org.jlab.service.dc.DCTBEngine;
 import org.jlab.service.ec.ECEngine;
 import org.jlab.service.ftof.FTOFEngine;
 import org.jlab.service.htcc.HTCCReconstructionService;
@@ -20,6 +22,14 @@ import org.jlab.service.eb.EBTBEngine;
 public class EBReconstructionTest {
 
     public void processAllEngines(DataEvent ev) {
+
+        DCHBEngine engineDCHB = new DCHBEngine();
+        engineDCHB.init();
+        engineDCHB.processDataEvent(ev);
+
+        DCTBEngine engineDCTB = new DCTBEngine();
+        engineDCTB.init();
+        engineDCTB.processDataEvent(ev);
 
         ECEngine engineEC = new ECEngine();
         engineEC.init();
@@ -51,13 +61,8 @@ public class EBReconstructionTest {
     public void testEBReconstruction() {
 
         DataEvent photonEvent = TestEvent.getECSector1PhotonEvent();
-       
-        //DataEvent electronEvent = TestEvent.getECSector1ElectronEvent();
-       
         processAllEngines(photonEvent);
-        
         //testEvent.show();
-
         // check bank existence:
         assertEquals(photonEvent.hasBank("RECHB::Event"), true);
         assertEquals(photonEvent.hasBank("RECHB::Particle"), true);
@@ -66,9 +71,13 @@ public class EBReconstructionTest {
         assertEquals(photonEvent.hasBank("REC::Particle"), true);
         assertEquals(photonEvent.hasBank("REC::Calorimeter"), true);
 
-        // photon event doesn't pass this:
-        //assertEquals(photonEvent.hasBank("REC::Cherenkov"), true);
-        //assertEquals(photonEvent.hasBank("REC::Scintillator"), true);
+        DataEvent electronEvent = TestEvent.getDCSector1ElectronEvent();
+        processAllEngines(electronEvent);
+        assertEquals(electronEvent.hasBank("REC::Event"), true);
+        assertEquals(electronEvent.hasBank("REC::Particle"), true);
+        //assertEquals(electronEvent.hasBank("REC::Tracks"), true);
+        //assertEquals(electronEvent.hasBank("REC::Cherenkov"), true);
+        //assertEquals(electronEvent.hasBank("REC::Scintillator"), true);
 
     }
 }
