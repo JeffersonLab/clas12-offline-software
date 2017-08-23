18242aeede453502f7739b865186a44be1fc51ab
Andrey Kim <drewkenjo@users.noreply.github.com>
2017-08-23 18:00:56 -0400

    modify coat-lib pom file to build clas-jcsg inside coat-lib jar file

--- build-coatjava.sh
+++ build-coatjava.sh
@@ -27,18 +27,8 @@ if [ $? != 0 ] ; then echo "common tools failure 2" ; exit 1 ; fi
 cd -
 cp common-tools/coat-lib/target/coat-libs*.jar coatjava/lib/clas/
 
-### jcsg ###
-export COATJAVA=$PWD/coatjava/
-cd common-tools/clas-jcsg
-./gradlew assemble
-if [ $? != 0 ] ; then echo "jcsg failure" ; exit 1 ; fi
-cd -
-cp common-tools/clas-jcsg/build/libs/jcsg-0.3.2.jar coatjava/lib/clas/
-
 ### create local mvn repo containing coat-libs and jcsg ##
 mvn deploy:deploy-file -Dfile=./common-tools/coat-lib/target/coat-libs-3.0-SNAPSHOT.jar -DgroupId=org.jlab.clas -DartifactId=common-tools -Dversion=0.0 -Dpackaging=jar -Durl=file:./myLocalMvnRepo/ -DrepositoryId=myLocalMvnRepo -DupdateReleaseInfo=true
-if [ $? != 0 ] ; then echo "failed to create local mvn repo" ; exit 1 ; fi
-mvn deploy:deploy-file -Dfile=./common-tools/clas-jcsg/build/libs/jcsg-0.3.2.jar -DgroupId=org.jlab.clas -DartifactId=clas-jcsg -Dversion=0.0 -Dpackaging=jar -Durl=file:./myLocalMvnRepo/ -DrepositoryId=myLocalMvnRepo -DupdateReleaseInfo=true
 if [ $? != 0 ] ; then echo "failed to create local mvn repo" ; exit 1 ; fi
  
 ### dc (depends on jcsg) ###
