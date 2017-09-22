336eb6949a9a142fc3826e5a6719203ce96316f2
baltzell <baltzell@gmx.com>
2017-09-22 19:08:25 -0400

    EventBuilder: neutrals
    only require one ECAL layer
    
    move neutrals matching to new class
    
    bugfix on FTOF photon veto
    
    disable FTOF photon veto for now
    
    photons require PCAL, neutrons do not

--- common-tools/clas-detector/src/main/java/org/jlab/detector/base/DetectorType.java
+++ common-tools/clas-detector/src/main/java/org/jlab/detector/base/DetectorType.java
@@ -35,7 +35,7 @@ public enum DetectorType {
 
     // FIXME add layer conventions here, e.g.:
     // (or maybe they are defined somewhere else?)
-    // FTOF2/1A/1B = 0/1/2
+    // FTOF1A/1B/2 = 1/2/3
     // PCAL/Inner/Outer = 1/4/7
     
     private final int detectorId;
