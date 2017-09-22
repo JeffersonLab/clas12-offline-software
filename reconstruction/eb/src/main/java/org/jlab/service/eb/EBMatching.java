f8a722cb8e043ab8a4044bc7d2df2a56abdfcc88
baltzell <baltzell@gmx.com>
2017-09-22 19:08:25 -0400

    EventBuilder: neutrals
    only require one ECAL layer
    
    move neutrals matching to new class
    
    bugfix on FTOF photon veto
    
    disable FTOF photon veto for now
    
    photons require PCAL, neutrons do not
    
    
    Former-commit-id: 55565092b88a6172c024b8b25c1178a8ac1f2fa8

---
+++ reconstruction/eb/src/main/java/org/jlab/service/eb/EBMatching.java
@@ -0,0 +1,143 @@
+package org.jlab.service.eb;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jlab.clas.detector.DetectorParticle;
+import org.jlab.clas.detector.DetectorResponse;
+import org.jlab.detector.base.DetectorType;
+
+/*
+ *
+ * Currently just used for matching for neutrals.
+ *
+ * @author baltzell
+ */
+public class EBMatching {
+
+    private EventBuilder eventBuilder=null;
+
+    public EBMatching(EventBuilder eb) {
+        this.eventBuilder=eb;
+    }
+
+    /*
+     * addResponsesFTOF
+     *
+     * Find and add all unmatched, matching FTOF responses to each particle.
+     *
+     */
+    public void addResponsesFTOF(List <DetectorParticle> parts) {
+
+        List<DetectorResponse> respFTOF1A = eventBuilder.getUnmatchedResponses(null, DetectorType.FTOF, 1);
+        List<DetectorResponse> respFTOF1B = eventBuilder.getUnmatchedResponses(null, DetectorType.FTOF, 2);
+        List<DetectorResponse>  respFTOF2 = eventBuilder.getUnmatchedResponses(null, DetectorType.FTOF, 3);
+
+        for (int ii=0; ii<parts.size(); ii++) {
+            
+            DetectorParticle part=parts.get(ii);
+
+            int index1A = part.getDetectorHit(respFTOF1A, DetectorType.FTOF, 1, EBConstants.FTOF_MATCHING_1A);
+            if (index1A >= 0) {
+                part.addResponse(respFTOF1A.get(index1A), true);
+                respFTOF1A.get(index1A).setAssociation(ii);
+            }
+            int index1B = part.getDetectorHit(respFTOF1B, DetectorType.FTOF, 2, EBConstants.FTOF_MATCHING_1B);
+            if (index1B >= 0) {
+                part.addResponse(respFTOF1B.get(index1B), true);
+                respFTOF1B.get(index1B).setAssociation(ii);
+            }
+            // only try to match with FTOF2 if not matched with 1A/1B:
+            if (index1A<0 && index1B<0) {
+                int index2 = part.getDetectorHit(respFTOF2, DetectorType.FTOF, 3, EBConstants.FTOF_MATCHING_2);
+                if (index2>=0) {
+                    part.addResponse(respFTOF2.get(index2), true);
+                    respFTOF2.get(index2).setAssociation(ii);
+                }
+            }
+        }
+    }
+
+    /*
+     * addResponsesECAL
+     *
+     * Find and add unmatched, matching ECAL responses to each particle,
+     * 
+     * But only for the given ECAL layers.
+     *
+     */
+    public void addResponsesECAL(List <DetectorParticle> parts, int[] layers) {
+
+        for (int ii=0; ii<parts.size(); ii++) {
+
+            DetectorParticle part=parts.get(ii);
+            
+            for (int layer : layers) {
+
+                double matching;
+                switch (layer) {
+                    case (1):
+                        matching=EBConstants.PCAL_MATCHING;
+                        break;
+                    case (4):
+                        matching=EBConstants.ECIN_MATCHING;
+                        break;
+                    case (7):
+                        matching=EBConstants.ECOUT_MATCHING;
+                        break;
+                    default:
+                        throw new RuntimeException("Invalid ECAL Layer:  "+layer);
+                }
+
+                List<DetectorResponse> respECAL = eventBuilder.getUnmatchedResponses(null, DetectorType.ECAL, layer);
+
+                int index = part.getDetectorHit(respECAL, DetectorType.ECAL, layer, matching);
+                if (index>=0) {
+                    part.addResponse(respECAL.get(index), true); 
+                    respECAL.get(index).setAssociation(ii);
+                }
+            }
+        }
+    }
+
+    /*
+     * findNeutrals
+     *
+     * Create neutral particles from unmatched responses from the given ECAL layer.
+     *
+     * Add unmatched, matching responses from FTOF and other ECAL layers. 
+     *
+     */
+    public List<DetectorParticle> findNeutrals(int ecalLayer) {
+
+        int[] otherEcalLayers;
+        switch (ecalLayer) {
+            case (1):
+                otherEcalLayers=new int[]{4,7};
+                break;
+            case (4):
+                otherEcalLayers=new int[]{1,7};
+                break;
+            case (7):
+                otherEcalLayers=new int[]{1,4};
+                break;
+            default:
+                throw new RuntimeException("Invalid ECAL Layer:  "+ecalLayer);
+        }
+
+        List<DetectorParticle> parts=new ArrayList<DetectorParticle>();
+
+        List<DetectorResponse> responsesECAL =
+            eventBuilder.getUnmatchedResponses(null, DetectorType.ECAL, ecalLayer);
+
+        for (DetectorResponse r : responsesECAL)
+            parts.add(DetectorParticle.createNeutral(r));
+        
+        // add other responses:
+        this.addResponsesECAL(parts,otherEcalLayers);
+        this.addResponsesFTOF(parts);
+
+        return parts;
+    }
+
+}
+
