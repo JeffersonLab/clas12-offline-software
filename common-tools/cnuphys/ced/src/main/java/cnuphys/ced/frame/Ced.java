a93e018439da09acac56b191ad681a204fa4ba83
David Heddle <heddle@Davids-iMac.local>
2017-09-28 18:10:29 -0400

    eliza
    Former-commit-id: c5021ab59c986a598833d9fdf25b8627edf7e3ba

--- common-tools/cnuphys/ced/src/main/java/cnuphys/ced/frame/Ced.java
+++ common-tools/cnuphys/ced/src/main/java/cnuphys/ced/frame/Ced.java
@@ -110,7 +110,7 @@ public class Ced extends BaseMDIApplication implements PropertyChangeListener,
 	// the singleton
 	private static Ced _instance;
 	
-	private static final String _release = "build 0.99.999.35";
+	private static final String _release = "build 0.99.999.36";
 
 	// used for one time inits
 	private int _firstTime = 0;
