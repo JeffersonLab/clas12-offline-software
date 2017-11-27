e1ab999fdf9ac53eb7c1312ba50f39ac96fc011f
raffaelladevita <devita@ge.infn.it>
2017-11-26 19:37:22 -0500

    reading FT constants with ConstantManager

--- reconstruction/ft/src/main/java/org/jlab/rec/ft/cal/FTCALHit.java
+++ reconstruction/ft/src/main/java/org/jlab/rec/ft/cal/FTCALHit.java
@@ -1,50 +1,54 @@
 package org.jlab.rec.ft.cal;
 
+import java.util.List;
+import org.jlab.utils.groups.IndexedTable;
+
 
 
 public class FTCALHit implements Comparable<FTCALHit>{
 	// class implements Comparable interface to allow for sorting a collection of hits by Edep values
 	
 	// constructor 
-	public FTCALHit(int i, int ICOMPONENT, int ADC, int TDC) {
+	public FTCALHit(int i, int ICOMPONENT, int ADC, int TDC, IndexedTable charge2Energy, IndexedTable timeOffsets, IndexedTable cluster) {
 		this._COMPONENT = ICOMPONENT;
 		this._IDY = ((int) ICOMPONENT/22) + 1;
 		this._IDX = ICOMPONENT + 1 - (this._IDY-1)*22;
 		this._ADC = ADC;
 		this._TDC = TDC;
 		
-		this.set_Edep(((double) this._ADC)*FTCALConstantsLoader.fadc_to_charge[0][0][ICOMPONENT-1]
-										  *FTCALConstantsLoader.mips_energy[0][0][ICOMPONENT-1]
-										  /FTCALConstantsLoader.mips_charge[0][0][ICOMPONENT-1]/1000.);
+		this.set_Edep(((double) this._ADC)*charge2Energy.getDoubleValue("fadc_to_charge", 1,1,ICOMPONENT)
+						  *charge2Energy.getDoubleValue("mips_energy", 1,1,ICOMPONENT)
+						  /charge2Energy.getDoubleValue("mips_charge", 1,1,ICOMPONENT)/1000.);
 		this.set_Time(((double) this._TDC)/FTCALConstantsLoader.TIMECONVFAC
-                                                                                  -(FTCALConstantsLoader.CRYS_LENGTH-FTCALConstantsLoader.depth_z)/FTCALConstantsLoader.VEFF
-										  -FTCALConstantsLoader.time_offset[0][0][ICOMPONENT-1]); 
+                                                 -(FTCALConstantsLoader.CRYS_LENGTH-cluster.getDoubleValue("depth_z", 1,1,0))/FTCALConstantsLoader.VEFF
+						 -timeOffsets.getDoubleValue("time_offset", 1,1,ICOMPONENT)); 
 //		if(this.get_Edep()>0.1) System.out.println(ICOMPONENT + " " + this._TDC + " " + 
 //				FTCALConstantsLoader.TIMECONVFAC + " " + FTCALConstantsLoader.time_offset[0][0][ICOMPONENT-1] + " " +
 //				this.get_Time());
 		this.set_Dx( (this._IDX-FTCALConstantsLoader.CRYS_DELTA )* FTCALConstantsLoader.CRYS_WIDTH);
 		this.set_Dy( (this._IDY-FTCALConstantsLoader.CRYS_DELTA )* FTCALConstantsLoader.CRYS_WIDTH);
-		this.set_Dz(FTCALConstantsLoader.CRYS_ZPOS);
+		this.set_Dz(FTCALConstantsLoader.CRYS_ZPOS+cluster.getDoubleValue("depth_z", 1,1,0));
 		this.set_DGTZIndex(i);
 		this.set_ClusIndex(0);
 	}
 
-	public FTCALHit(int i, int ICOMPONENT, int ADC, float time) {
+	public FTCALHit(int i, int ICOMPONENT, int ADC, float time, IndexedTable charge2Energy, IndexedTable timeOffsets, IndexedTable cluster) {
 		this._COMPONENT = ICOMPONENT;
 		this._IDY = ((int) ICOMPONENT/22) + 1;
 		this._IDX = ICOMPONENT + 1 - (this._IDY-1)*22;
 		this._ADC = ADC;
 		
-		this.set_Edep(((double) this._ADC)*FTCALConstantsLoader.fadc_to_charge[0][0][ICOMPONENT-1]
-										  *FTCALConstantsLoader.mips_energy[0][0][ICOMPONENT-1]
-										  /FTCALConstantsLoader.mips_charge[0][0][ICOMPONENT-1]/1000.);
-		this.set_Time(time -(FTCALConstantsLoader.CRYS_LENGTH-FTCALConstantsLoader.depth_z)/FTCALConstantsLoader.VEFF-FTCALConstantsLoader.time_offset[0][0][ICOMPONENT-1]); 
+		this.set_Edep(((double) this._ADC)*charge2Energy.getDoubleValue("fadc_to_charge", 1,1,ICOMPONENT)
+						  *charge2Energy.getDoubleValue("mips_energy", 1,1,ICOMPONENT)
+						  /charge2Energy.getDoubleValue("mips_charge", 1,1,ICOMPONENT)/1000.);
+		this.set_Time(time -(FTCALConstantsLoader.CRYS_LENGTH-cluster.getDoubleValue("depth_z", 1,1,0))/FTCALConstantsLoader.VEFF
+				   -timeOffsets.getDoubleValue("time_offset", 1,1,ICOMPONENT)); 
 //		if(this.get_Edep()>0.1) System.out.println(ICOMPONENT + " " + this._TDC + " " + 
 //				FTCALConstantsLoader.TIMECONVFAC + " " + FTCALConstantsLoader.time_offset[0][0][ICOMPONENT-1] + " " +
 //				this.get_Time());
 		this.set_Dx( (this._IDX-FTCALConstantsLoader.CRYS_DELTA )* FTCALConstantsLoader.CRYS_WIDTH);
 		this.set_Dy( (this._IDY-FTCALConstantsLoader.CRYS_DELTA )* FTCALConstantsLoader.CRYS_WIDTH);
-		this.set_Dz(FTCALConstantsLoader.CRYS_ZPOS);
+		this.set_Dz(FTCALConstantsLoader.CRYS_ZPOS+cluster.getDoubleValue("depth_z", 1,1,0));
 		this.set_DGTZIndex(i);
 		this.set_ClusIndex(0);
 	}
