4dd527d8aef926be8f3b8937a4ad8ecb2ecf9942
David Heddle <heddle@Davids-iMac.local>
2017-12-15 09:35:04 -0500

    magfield
    Former-commit-id: 9fe89714de5f36d680e1ca46b33faaec32be3335

--- common-tools/cnuphys/ced/src/main/java/cnuphys/ced/clasio/ConnectETDialog.java
+++ common-tools/cnuphys/ced/src/main/java/cnuphys/ced/clasio/ConnectETDialog.java
@@ -4,14 +4,20 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.FlowLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 import cnuphys.bCNU.dialog.DialogUtilities;
 import cnuphys.bCNU.dialog.SimpleDialog;
+import cnuphys.bCNU.dialog.VerticalFlowLayout;
 import cnuphys.bCNU.graphics.ImageManager;
+import cnuphys.bCNU.graphics.component.CommonBorder;
 
 public class ConnectETDialog extends SimpleDialog {
 	
@@ -24,6 +30,13 @@ public class ConnectETDialog extends SimpleDialog {
 	
 	// reason the dialog was closed
 	private int _reason = DialogUtilities.CANCEL_RESPONSE;
+	
+	//text fields
+	private JTextField _stationTF;
+	private JTextField _fileTF;
+	
+	//machine combobox
+	private JComboBox _machineCombo;
 
 	/**
 	 * Create the panel for selected 
@@ -49,7 +62,24 @@ public class ConnectETDialog extends SimpleDialog {
 			return null;
 		}
 		JLabel label = new JLabel(_etIcon);
+		
+		Border emptyBorder = BorderFactory
+				.createEmptyBorder(4, 4, 4, 4);
+
+		label.setBorder(emptyBorder);
 		return label;
+	}
+	
+	public String getFile() {
+		return _fileTF.getText();
+	}
+	
+	public String getStation() {
+		return _stationTF.getText();
+	}
+	
+	public String getMachine() {
+		return (String)(_machineCombo.getSelectedItem());
 	}
 	
 	/**
@@ -61,21 +91,38 @@ public class ConnectETDialog extends SimpleDialog {
 	@Override
 	protected Component createCenterComponent() {
 		JPanel panel = new JPanel();
-		panel.setLayout(new BorderLayout(8,8));
+		panel.setLayout(new VerticalFlowLayout());
 		
 		JPanel npanel = new JPanel();
 		npanel.setLayout(new FlowLayout(FlowLayout.LEFT, 8, 4));
-		
 		npanel.add(new JLabel("CLON cpu: "));
+		String[] machineStrings = { "clondaq2", "clondaq3", "clondaq4", "clondaq5", "clondaq6" };
 
+		//Create the combo box, select item at index 4.
+		//Indices start at 0, so 4 specifies the pig.
+		_machineCombo = new JComboBox(machineStrings);
+		_machineCombo.setSelectedIndex(2);
+		npanel.add(_machineCombo);
+		
+		JPanel cpanel = new JPanel();
+		cpanel.setLayout(new FlowLayout(FlowLayout.LEFT, 8, 4));
+		cpanel.add(new JLabel("Station: "));
+		_stationTF = new JTextField(14);
+		_stationTF.setText("ced_station");
+		cpanel.add(_stationTF);
 		
 		JPanel spanel = new JPanel();
-		spanel.setLayout(new FlowLayout(FlowLayout.LEFT, 8, 4));
-		
+		spanel.setLayout(new FlowLayout(FlowLayout.LEFT, 8, 4));		
 		spanel.add(new JLabel("File: "));
+		_fileTF = new JTextField(20);
+		_fileTF.setText("/tmp/et_sys_clasprod");
+		spanel.add(_fileTF);
 		
-		panel.add(npanel, BorderLayout.NORTH);
-		panel.add(spanel, BorderLayout.SOUTH);
+		panel.add(npanel);
+		panel.add(cpanel);
+		panel.add(spanel);
+		
+		panel.setBorder(CommonBorder.withEmptyBorder("Connection Parameters", 4));
 		return panel;
 	}
 	
