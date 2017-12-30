94f60ba1473c62788f9d937712c440f7a9270a8d
David Heddle <dheddle@circadence.com>
2017-12-30 15:24:02 -0500

    anneal

--- common-tools/cnuphys/bCNU/src/main/java/cnuphys/bCNU/simanneal/SimulationPanel.java
+++ common-tools/cnuphys/bCNU/src/main/java/cnuphys/bCNU/simanneal/SimulationPanel.java
@@ -1,5 +1,9 @@
 package cnuphys.bCNU.simanneal;
 
+import java.awt.BorderLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 /**
@@ -8,9 +12,57 @@ import javax.swing.JPanel;
  *
  */
 public class SimulationPanel extends JPanel implements IUpdateListener {
+	
+	private JComponent _plotComponent;
+	private JComponent _displayComponent;
+	private JComponent _controlPanel;
+	private Simulation _simulation;
+	
+	public SimulationPanel(Simulation simulation, JComponent plotComponent, JComponent displayComponent, JComponent controlPanel) {
+		_plotComponent = plotComponent;
+		_displayComponent = displayComponent;
+		_controlPanel = controlPanel;
+		
+		setLayout(new BorderLayout(4, 4));
+		
+		
+		if (plotComponent != null) {
+			plotComponent.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+			add(plotComponent, BorderLayout.NORTH);
+		}
+		
+		if (displayComponent != null) {
+			displayComponent.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+			add(displayComponent, BorderLayout.CENTER);
+		}
+		
+		if (controlPanel != null) {
+			controlPanel.setBorder(BorderFactory.createEmptyBorder(2, 2, 2, 2));
+			add(controlPanel, BorderLayout.EAST);
+		}
+		
+		setSimulation(simulation);
+		
+	}
+	
+	public void setSimulation(Simulation simulation) {
+		
+		if (_simulation == simulation) {
+			return;
+		}
+		
+		
+		if (_simulation != null) {
+			_simulation.removeUpdateListener(this);
+		}
+		
+		_simulation = simulation;
+		_simulation.addUpdateListener(this);
+	}
+	
 
 	@Override
-	public void updateSolution(Solution newSolution, Solution oldSolution, double temperature) {
+	public void updateSolution(Simulation simulation, Solution newSolution, Solution oldSolution) {
 	}
 
 }
