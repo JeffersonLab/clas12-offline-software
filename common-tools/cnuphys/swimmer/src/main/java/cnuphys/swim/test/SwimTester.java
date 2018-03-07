3c37c68edf36c08d3f937abae432f05140e92fff
heddle <david.heddle@cnu.edu>
2018-03-07 18:31:47 -0500

    swim test

--- common-tools/cnuphys/swimmer/src/main/java/cnuphys/swim/test/SwimTester.java
+++ common-tools/cnuphys/swimmer/src/main/java/cnuphys/swim/test/SwimTester.java
@@ -1,21 +1,26 @@
 package cnuphys.swim.test;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 /**
  * Used for testing the swimmer
  * @author heddle
  *
  */
-public class SwimTester extends JFrame {
+public class SwimTester extends JFrame implements ListSelectionListener {
 
-	private GenericList<? extends ASwimTest> _jList;
+	private GenericList<ASwimTest> _jList;
 	
 	public SwimTester() {
 		super("Swim Tester");
@@ -36,10 +41,29 @@ public class SwimTester extends JFrame {
 	//add the content to the frame
 	private void addContent() {
 		setLayout(new BorderLayout(4, 4));
+		addNorth();
+	}
+	
+	public void addNorth() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout(4, 4));
+		
 		_jList = new GenericList();
+		_jList.addListSelectionListener(this);
 		
 		JScrollPane spane = _jList.getScrollPane();
-		add(spane, BorderLayout.CENTER);
+
+		JLabel label = new JLabel("    Known Tests");
+		label.setForeground(Color.red);
+		
+		panel.add(label, BorderLayout.NORTH);
+		
+		panel.add(spane, BorderLayout.CENTER);
+		add(panel, BorderLayout.NORTH);
+	}
+	
+	public void addTest(ASwimTest test) {
+		_jList.getModel().addElement(test);
 	}
 	
 	/**
@@ -50,6 +74,9 @@ public class SwimTester extends JFrame {
 		
 		SwimTester tester = new SwimTester();
 		
+		//add the tests
+		tester.addTest(new SameTrackDifferentMaxStep());
+		
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
@@ -59,5 +86,12 @@ public class SwimTester extends JFrame {
 			}
 		});
 
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		if (!e.getValueIsAdjusting()) {
+			System.err.println("Selected [" + _jList.getSelectedValue() + "]");
+		}
 	}
 }
