74fa234a1cfa5cac9d7262e2fd82a5c6cd9e7c1d
David Heddle <dheddle@circadence.com>
2018-03-07 15:58:20 -0500

    preparing more swim tests
    Former-commit-id: 24995af71ebddcffe8345eafeadc757d61b60967

--- common-tools/cnuphys/swimmer/src/main/java/cnuphys/swim/test/SwimTester.java
+++ common-tools/cnuphys/swimmer/src/main/java/cnuphys/swim/test/SwimTester.java
@@ -1,5 +1,19 @@
 package cnuphys.swim.test;
 
+/**
+ * Used for testing the swimmer
+ * @author heddle
+ *
+ */
 public class SwimTester {
 
+	
+	
+	/**
+	 * Main program
+	 * @param arg command line arguments
+	 */
+	public static void main(String arg[]) {
+		
+	}
 }
