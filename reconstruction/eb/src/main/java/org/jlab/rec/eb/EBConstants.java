b07300e5aff09a4f6ef4b81f8d6026f7766656a8
baltzell <baltzell@gmx.com>
2018-03-10 20:39:16 -0500

    EBConstants:  comment out deprecaeted
    Former-commit-id: 3665d24625271cb709764a57553457b06d20f83a

--- reconstruction/eb/src/main/java/org/jlab/rec/eb/EBConstants.java
+++ reconstruction/eb/src/main/java/org/jlab/rec/eb/EBConstants.java
@@ -16,13 +16,13 @@ public class EBConstants {
     public static final double  FTOF_MATCHING_1B = 18.0; // matching for layer 1A of FTOF
     public static final double  FTOF_MATCHING_2 = 18.0; // matching for layer 1A of FTOF
     
-    public static final double CTOF_Matching = 15.0;
-    public static final double CND_Matching = 15.0;
+    //public static final double CTOF_Matching = 15.0;
+    //public static final double CND_Matching = 15.0;
     
     public static final double ECAL_SAMPLINGFRACTION = 0.24;
-    public static final double ECAL_SAMPLINGFRACTION_CUT = 0.218;
+    //public static final double ECAL_SAMPLINGFRACTION_CUT = 0.218;
     
-    public static final double TARGET_POSITION = -4.5;
+    //public static final double TARGET_POSITION = -4.5;
     
     public static final double      RF_BUCKET_LENGTH = 2.004;
     public static final double             RF_OFFSET = 0.0;
@@ -31,17 +31,17 @@ public class EBConstants {
     public static final double           RF_TDC2TIME = 0.0234358;
     public static final int         RF_LARGE_INTEGER = 800;
     
-    public static final Vector3D PCAL_hitRes = new Vector3D(0.0,0.0,0.0);
-    public static final Vector3D ECIN_hitRes = new Vector3D(0.0,0.0,0.0);
-    public static final Vector3D ECOUT_hitRes = new Vector3D(0.0,0.0,0.0);
-    public static final Vector3D FTOF1A_hitRes = new Vector3D(0.0,0.0,0.0);
-    public static final Vector3D FTOF1B_hitRes = new Vector3D(0.0,0.0,0.0);
+    //public static final Vector3D PCAL_hitRes = new Vector3D(0.0,0.0,0.0);
+    //public static final Vector3D ECIN_hitRes = new Vector3D(0.0,0.0,0.0);
+    //public static final Vector3D ECOUT_hitRes = new Vector3D(0.0,0.0,0.0);
+    //public static final Vector3D FTOF1A_hitRes = new Vector3D(0.0,0.0,0.0);
+    //public static final Vector3D FTOF1B_hitRes = new Vector3D(0.0,0.0,0.0);
    
-    public static final double PCAL_TimingRes = 0.0;
-    public static final double ECIN_TimingRes = 0.0;
-    public static final double ECOUT_TimingRes = 0.0;
-    public static final double FTOF1A_TimingRes = 0.0;
-    public static final double FTOF1B_TimingRes = 0.0;
+    //public static final double PCAL_TimingRes = 0.0;
+    //public static final double ECIN_TimingRes = 0.0;
+    //public static final double ECOUT_TimingRes = 0.0;
+    //public static final double FTOF1A_TimingRes = 0.0;
+    //public static final double FTOF1B_TimingRes = 0.0;
     
     public static final int HTCC_NPHE_CUT = 2;
     public static final int LTCC_NPHE_CUT = 2;
@@ -49,9 +49,9 @@ public class EBConstants {
     public static final double LTCC_LOWER_PION_THRESHOLD = 3.0;
     public static final double LTCC_UPPER_PION_THRESHOLD = 5.0;
     
-    public static final int TRIGGER_ID = 11;
+    //public static final int TRIGGER_ID = 11;
    
-    public static final double GEMC_STARTTIME = 124.25;
+    //public static final double GEMC_STARTTIME = 124.25;
     
 }
 
