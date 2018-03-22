fa74e289e53c02144c47e2ff9c32d05ddb73d259
baltzell <baltzell@gmx.com>
2018-03-22 09:21:58 -0400

    eb:  propagate ECAL::moments to REC::Calorimeter

--- common-tools/clas-reco/src/main/java/org/jlab/clas/detector/CalorimeterResponse.java
+++ common-tools/clas-reco/src/main/java/org/jlab/clas/detector/CalorimeterResponse.java
@@ -6,12 +6,43 @@ import org.jlab.detector.base.DetectorType;
 import org.jlab.io.base.DataBank;
 import org.jlab.io.base.DataEvent;
 
+import org.jlab.clas.physics.Vector3;
 
 /**
  *
  * @author jnewton
  */
 public class CalorimeterResponse extends DetectorResponse {
+
+    private Vector3 widthUVW = new Vector3(0,0,0);
+    private Vector3 coordUVW = new Vector3(0,0,0);
+    private Vector3 secondMomentUVW = new Vector3(0,0,0);
+    private Vector3 thirdMomentUVW = new Vector3(0,0,0);
+
+    public void setWidthUVW(float u,float v,float w) {
+        widthUVW.setXYZ(u,v,w);
+    }
+    public void setCoordUVW(float u,float v,float w) {
+        coordUVW.setXYZ(u,v,w);
+    }
+    public void setSecondMomentUVW(float u,float v,float w) {
+        secondMomentUVW.setXYZ(u,v,w);
+    }
+    public void setThirdMomentUVW(float u,float v,float w) {
+        thirdMomentUVW.setXYZ(u,v,w);
+    }
+    public Vector3 getWidthUVW() {
+        return widthUVW;
+    }
+    public Vector3 getCoordUVW() {
+        return coordUVW;
+    }
+    public Vector3 getSecondMomentUVW() {
+        return secondMomentUVW;
+    }
+    public Vector3 getThirdMomentUVW() {
+        return thirdMomentUVW;
+    }
 
     public CalorimeterResponse(){
         super();
@@ -22,24 +53,45 @@ public class CalorimeterResponse extends DetectorResponse {
     }
 
     public static List<DetectorResponse>  readHipoEvent(DataEvent event, 
-            String bankName, DetectorType type){        
+            String bankName, DetectorType type, String momentsBankName){        
         List<DetectorResponse> responseList = new ArrayList<DetectorResponse>();
         if(event.hasBank(bankName)==true){
             DataBank bank = event.getBank(bankName);
+            DataBank momentsBank=null;
+            if (momentsBankName!=null && event.hasBank(momentsBankName)) {
+                momentsBank=event.getBank(momentsBankName);
+                if (bank.rows() != momentsBank.rows()) {
+                    throw new RuntimeException("Bank length mismatch: "+bankName+" and "+momentsBankName);
+                }
+            }
             int nrows = bank.rows();
             for(int row = 0; row < nrows; row++){
                 int sector = bank.getByte("sector", row);
                 int layer = bank.getByte("layer", row);
-                DetectorResponse  response = new DetectorResponse(sector,layer,0);
+                CalorimeterResponse  response = new CalorimeterResponse(sector,layer,0);
                 response.setHitIndex(row);
                 response.getDescriptor().setType(type);
                 float x = bank.getFloat("x", row);
                 float y = bank.getFloat("y", row);
                 float z = bank.getFloat("z", row);
                 response.setPosition(x, y, z);
+                float u = bank.getFloat("widthU",row);
+                float v = bank.getFloat("widthV",row);
+                float w = bank.getFloat("widthW",row);
+                response.setWidthUVW(u,v,w);
+                if (momentsBank!=null) {
+                    u = momentsBank.getFloat("distU",row);
+                    v = momentsBank.getFloat("distV",row);
+                    w = momentsBank.getFloat("distW",row);
+                    response.setCoordUVW(u,v,w);
+                    u = momentsBank.getFloat("m2u",row);
+                    v = momentsBank.getFloat("m2v",row);
+                    w = momentsBank.getFloat("m2w",row);
+                    response.setSecondMomentUVW(u,v,w);
+                }
                 response.setEnergy(bank.getFloat("energy", row));
                 response.setTime(bank.getFloat("time", row));
-                responseList.add(response);
+                responseList.add((DetectorResponse)response);
             }
         }
         return responseList;
