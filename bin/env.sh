49f74b1e38d789a812e003017a91997fd9b60fda
baltzell <baltzell@gmx.com>
2018-07-23 14:52:27 -0400

    env.sh: donot override users fieldmap

--- bin/env.sh
+++ bin/env.sh
@@ -5,6 +5,12 @@ DISTRO_DIR=$SCRIPT_DIR/../ ; export DISTRO_DIR
 CLAS12DIR=$SCRIPT_DIR/../ ; export CLAS12DIR
 CLARA_SERVICES=$DISTRO_DIR/lib/services; export CLARA_SERVICES
 DATAMINING=$DISTRO_DIR ; export DATAMINING
-TORUSMAP=Symm_torus_r2501_phi16_z251_24Apr2018.dat ; export TORUSMAP
-SOLENOIDMAP=Symm_solenoid_r601_phi1_z1201_13June2018.dat ; export SOLENOIDMAP
+
+# Set default field maps (but do not override user's env):
+if [ -z "$TORUSMAP" ]; then
+    export TORUSMAP=Symm_torus_r2501_phi16_z251_24Apr2018.dat
+fi
+if [ -z "$SOLENOIDMAP" ]; then
+    export SOLENOIDMAP=Symm_solenoid_r601_phi1_z1201_13June2018.dat
+fi
 
