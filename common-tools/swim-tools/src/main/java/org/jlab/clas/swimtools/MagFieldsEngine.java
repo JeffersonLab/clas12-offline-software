1ede3fc68beb57f070f601b05ddee238371eca6c
Veronique Ziegler <ziegler@jlab.org>
2018-07-31 11:00:47 -0400

    New Package for Swimming in tracking

---
+++ common-tools/swim-tools/src/main/java/org/jlab/clas/swimtools/MagFieldsEngine.java
@@ -0,0 +1,81 @@
+package org.jlab.clas.swimtools;
+
+import cnuphys.magfield.MagneticFields;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.jlab.clas.reco.ReconstructionEngine;
+import org.jlab.io.base.DataBank;
+import org.jlab.io.base.DataEvent;
+import org.jlab.utils.CLASResources;
+
+public class MagFieldsEngine extends ReconstructionEngine {
+
+    public MagFieldsEngine() {
+        super("MagFields","ziegler","1.0");
+    }
+    AtomicInteger Run = new AtomicInteger(0);
+    /**
+     * 
+     * determine torus and solenoid map name from yaml, else env, else crash
+     */
+    public void initializeMagneticFields() {
+        String torusMap=this.getEngineConfigString("torusMap");
+        String solenoidMap=this.getEngineConfigString("solenoidMap");
+        if (torusMap!=null) {
+            System.out.println("["+this.getName()+"] Torus Map chosen based on yaml: "+torusMap);
+        }
+        else {
+            torusMap = System.getenv("TORUSMAP");
+            if (torusMap!=null) {
+                System.out.println("["+this.getName()+"] Torus Map chosen based on env: "+torusMap);
+            }
+        }
+        if (torusMap==null) {
+            throw new RuntimeException("["+this.getName()+"]  Failed to find torus map name in yaml or env.");
+        }
+        if (solenoidMap!=null) {
+            System.out.println("["+this.getName()+"] solenoid Map chosen based on yaml: "+solenoidMap);
+        }
+        else {
+            solenoidMap = System.getenv("SOLENOIDMAP");
+            if (solenoidMap!=null) {
+                System.out.println("["+this.getName()+"] solenoid Map chosen based on env: "+solenoidMap);
+            }
+        }
+        if (solenoidMap==null) {
+            throw new RuntimeException("["+this.getName()+"]  Failed to find solenoid map name in yaml or env.");
+        }
+        String mapDir = CLASResources.getResourcePath("etc")+"/data/magfield";
+        try {
+            MagneticFields.getInstance().initializeMagneticFields(mapDir,torusMap,solenoidMap);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean processDataEvent(DataEvent event) {
+        DataBank bank = event.getBank("RUN::config");
+        // Load the constants
+        //-------------------
+        int newRun = bank.getInt("run", 0);
+        if(newRun==0)
+            return true;
+        
+        double shift =0;
+        if(newRun>1890) {
+            shift = -1.9;
+        }
+        Swimmer.setMagneticFieldsScales(bank.getFloat("solenoid", 0), bank.getFloat("torus", 0), shift);
+        
+        return true;
+    }
+
+    @Override
+    public boolean init() {
+        this.initializeMagneticFields();
+        return true;
+    }
+
+   
+}
---
+++ common-tools/swim-tools/src/main/java/org/jlab/clas/swimtools/Swimmer.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jlab.clas.swimtools;
+import cnuphys.magfield.MagneticFields;
+import java.util.Hashtable;
+/**
+ *
+ * @author ziegler, heddle
+ */
+
+
+public class Swimmer {
+    
+    private static Hashtable<Thread, ProbeCollection> swimmers = new Hashtable<>();
+    
+    public static ProbeCollection getProbeCollection(Thread thr){
+        return swimmers.get(thr);
+    }
+    public static void put(Thread thr, ProbeCollection PC) {
+        swimmers.put(thr, PC);
+    }
+    
+    public static synchronized void setMagneticFieldsScales(double SolenoidScale, double TorusScale, double shift) {
+        if (FieldsLoaded) {
+            return;
+        }
+        MagneticFields.getInstance().getTorus().setScaleFactor(TorusScale);
+        MagneticFields.getInstance().getSolenoid().setScaleFactor(SolenoidScale);
+        MagneticFields.getInstance().setSolenoidShift(shift);
+        setSolScale(SolenoidScale);
+        setTorScale(TorusScale);
+        FieldsLoaded = true;
+        System.out.println(" TRACKING ***** ****** ****** THE TORUS IS BEING SCALED BY " + (TorusScale * 100) + "  %   *******  ****** **** ");
+        System.out.println(" TRACKING ***** ****** ****** THE SOLENOID IS BEING SCALED BY " + (SolenoidScale * 100) + "  %   *******  ****** **** ");
+    }
+
+    private static double SOLSCALE = -1;
+    private static double TORSCALE = -1;
+    
+    public static synchronized void setSolScale(double s) {
+        SOLSCALE = s;
+    }
+    public static synchronized void setTorScale(double s) {
+        TORSCALE = s;
+    }
+    public static double getSolScale() {
+        return SOLSCALE;
+    }
+    public static double getTorScale() {
+        return TORSCALE;
+    }
+    static boolean FieldsLoaded = false;
+    
+}
