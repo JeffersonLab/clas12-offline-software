2cbd397f5108b757ef6d836ae1b8deddc0dd93fd
baltzell <baltzell@gmx.com>
2018-08-16 21:28:11 -0400

    EBConstants: cleanup

--- reconstruction/eb/src/main/java/org/jlab/rec/eb/EBConstants.java
+++ reconstruction/eb/src/main/java/org/jlab/rec/eb/EBConstants.java
@@ -1,11 +1,10 @@
 package org.jlab.rec.eb;
-
-import org.jlab.geom.prim.Vector3D;
-
 /**
  *
  * @author gavalian
  * @author baltzell
+ *
+ * NOTE:  These should probably be in CCDB insated of here.
  */
 public class EBConstants {
    
@@ -19,43 +18,5 @@ public class EBConstants {
     public static final double LTCC_KAON_THRESHOLD = 5.0;
     
     public static final int RF_LARGE_INTEGER = 800;
-
-    //public static final int TRIGGER_ID = 11;
-   
-    //public static final double  PCAL_MATCHING = 15.0; // matching distance in PCAL
-    //public static final double  ECIN_MATCHING = 15.0; // matching distance in PCAL
-    //public static final double ECOUT_MATCHING = 15.0; // matching distance in PCAL
-    
-    //public static final double  FTOF_MATCHING_1A = 18.0; // matching for layer 1A of FTOF
-    //public static final double  FTOF_MATCHING_1B = 18.0; // matching for layer 1A of FTOF
-    //public static final double  FTOF_MATCHING_2 = 18.0; // matching for layer 1A of FTOF
-    
-    //public static final double CTOF_Matching = 15.0;
-    //public static final double CND_Matching = 15.0;
-    
-    //public static final double ECAL_SAMPLINGFRACTION = 0.24;
-    //public static final double ECAL_SAMPLINGFRACTION_CUT = 0.218;
-    
-    //public static final double TARGET_POSITION = 0;//-4.5;
-    
-    //public static final double      RF_BUCKET_LENGTH = 2.004;
-    //public static final double             RF_OFFSET = 0.0;
-    //public static final int                RF_CYCLES = 80;   
-    //public static final int                    RF_ID = 1;     // signal ID that will be used in reconstruction
-    //public static final double           RF_TDC2TIME = 0.0234358;
-    
-    //public static final Vector3D PCAL_hitRes = new Vector3D(0.0,0.0,0.0);
-    //public static final Vector3D ECIN_hitRes = new Vector3D(0.0,0.0,0.0);
-    //public static final Vector3D ECOUT_hitRes = new Vector3D(0.0,0.0,0.0);
-    //public static final Vector3D FTOF1A_hitRes = new Vector3D(0.0,0.0,0.0);
-    //public static final Vector3D FTOF1B_hitRes = new Vector3D(0.0,0.0,0.0);
-   
-    //public static final double PCAL_TimingRes = 0.0;
-    //public static final double ECIN_TimingRes = 0.0;
-    //public static final double ECOUT_TimingRes = 0.0;
-    //public static final double FTOF1A_TimingRes = 0.0;
-    //public static final double FTOF1B_TimingRes = 0.0;
-    
-    //public static final double GEMC_STARTTIME = 124.25;
 }
 
