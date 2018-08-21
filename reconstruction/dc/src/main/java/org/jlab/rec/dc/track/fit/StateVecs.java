1c8fecd92e91f7fc04fb6b580458be48d37ca928
Veronique Ziegler <ziegler@jlab.org>
2018-08-21 17:55:21 -0400

    RungeKutta 4 KF state vec and covMat transport

--- reconstruction/dc/src/main/java/org/jlab/rec/dc/track/fit/StateVecs.java
+++ reconstruction/dc/src/main/java/org/jlab/rec/dc/track/fit/StateVecs.java
@@ -27,6 +27,7 @@ public class StateVecs {
     private final float[] bf = new float[3];
     private final float[] lbf = new float[3];
     private Swim dcSwim;
+    private RungeKutta rk;
     /**
      * State vector representing the track in the sector coordinate system at the measurement layer
      */
@@ -34,6 +35,7 @@ public class StateVecs {
         //Max Field Location: (phi, rho, z) = (29.50000, 44.00000, 436.00000)
         // get the maximum value of the B field
         dcSwim = swimmer;
+        rk = new RungeKutta();
         double phi = Math.toRadians(29.5);
         double rho = 44.0;
         double z = 436.0;
@@ -41,56 +43,7 @@ public class StateVecs {
         Bmax = Math.sqrt(lbf[0]*lbf[0]+lbf[1]*lbf[1]+lbf[2]*lbf[2]) *(2.366498/4.322871999651699); // scales according to torus scale by reading the map and averaging the value
      }
     
-    /**
-     * 
-     * @param i initial state index
-     * @param f final state index
-     * @param iVec state vector at the initial state index
-     * @return state vector at the final state index
-     */
-    public StateVec f(int sector, int i, int f, StateVec iVec) {
-
-        double x = iVec.x;
-        double y = iVec.y;
-        double tx = iVec.tx;
-        double ty = iVec.ty;
-        double Q = iVec.Q;
-
-        int nSteps = (int) (Math.abs((Z[i] - Z[f]) / stepSize) + 1);
-
-        double s = Math.signum(Z[f] - Z[i]) * stepSize;
-
-        double z = Z[i];
-
-        for (int j = 0; j < nSteps; j++) {
-
-            if (j == nSteps - 1) {
-                s = Math.signum(Z[f] - Z[i]) * Math.abs(z - Z[f]);
-            }
-
-           // propagate the state vector a next step
-            dcSwim.Bfield(sector, x, y, z, bf);
-            A(tx, ty, bf[0], bf[1], bf[2], A);
-            // transport stateVec
-            x += tx * s + 0.5 * Q * speedLight * A[0] * s * s;
-            y += ty * s + 0.5 * Q * speedLight * A[1] * s * s;
-            tx += Q * speedLight * A[0] * s;
-            ty += Q * speedLight * A[1] * s;
-
-            z += s;
-        }
-
-        StateVec fVec = new StateVec(f);
-        fVec.z = Z[f];
-        fVec.x = x;
-        fVec.y = y;
-        fVec.tx = tx;
-        fVec.ty = ty;
-        fVec.Q = Q;
-
-        return fVec;
-
-    }
+    
     
     /**
      * 
@@ -102,193 +55,49 @@ public class StateVecs {
     public void transport(int sector, int i, int f, StateVec iVec, CovMat covMat) { // s = signed step-size
         if(iVec==null)
             return;
-        //StateVec iVec = trackTraj.get(i);
-        //bfieldPoints = new ArrayList<B>();
-       // CovMat covMat = icovMat;
-     //   double[] A = new double[5];
-      //  double[] dA = new double[5];
-        double[][] u = new double[5][5];       
-        double[][] C = new double[5][5];
-       // Matrix Cpropagated = null;
-        //double[][] transpStateJacobian = null;
-
-        double x = iVec.x;
-        double y = iVec.y;
-        double tx = iVec.tx;
-        double ty = iVec.ty;
-        double Q = iVec.Q;
+        double stepSize = 1;
+        
         double Bf = iVec.B;
-        // B-field components at state vector coordinates
-        dcSwim.Bfield(sector, x, y, Z[i], bf);
-        
-       // if (bfieldPoints.size() > 0) {
-        //    double B = new Vector3D(bfieldPoints.get(bfieldPoints.size() - 1).Bx, bfieldPoints.get(bfieldPoints.size() - 1).By, bfieldPoints.get(bfieldPoints.size() - 1).Bz).mag();
-        if (bf!=null) { // get the step size used in swimming as a function of the field intensity in the region traversed
-            double B = Math.sqrt(bf[0]*bf[0]+bf[1]*bf[1]+bf[2]*bf[2]); 
-            if (B / Bmax > 0.01) {
-                stepSize = 0.15*4;
-            }
-            if (B / Bmax > 0.02) {
-                stepSize = 0.1*3;
-            }
-            if (B / Bmax > 0.05) {
-                stepSize = 0.075*2;
-            }
-            if (B / Bmax > 0.1) {
-                stepSize = 0.05*2;
-            }
-            if (B / Bmax > 0.5) {
-                stepSize = 0.02;
-            }
-            if (B / Bmax > 0.75) {
-                stepSize = 0.01;
-            }
+        if (Bf / Bmax > 0.5) {
+            stepSize = 0.5;
         }
-        
+        StateVecs.StateVec fVec = new StateVec(f);
+        CovMat fCov = new CovMat(f);
+        fVec.x = iVec.x;
+        fVec.y = iVec.y;
+        fVec.z = iVec.z;
+        fVec.tx = iVec.tx;
+        fVec.ty = iVec.ty;
+        fVec.Q = iVec.Q;
+        fCov.covMat = covMat.covMat;
         int nSteps = (int) (Math.abs((Z[i] - Z[f]) / stepSize) + 1);
 
         double s  = (Z[f] - Z[i]) / (double) nSteps;
         double z = Z[i];
-        double dPath=0;
-       
+        
         for (int j = 0; j < nSteps; j++) {
             // get the sign of the step
             if (j == nSteps - 1) {
                 s = Math.signum(Z[f] - Z[i]) * Math.abs(z - Z[f]);
             }
-
-            //B bf = new B(i, z, x, y, tx, ty, s);
-            //bfieldPoints.add(bf);
-            dcSwim.Bfield(sector, x, y, z, bf);
+            //System.out.println(" RK step num "+(j+1)+" = "+(float)s+" nSteps = "+nSteps);
+            double x =  fVec.x;
+            double y =  fVec.y;
+            z = fVec.z;
+            double tx = fVec.tx;
+            double ty = fVec.ty;
+            double Q =  fVec.Q;
+            covMat.covMat = fCov.covMat; 
             
-            A(tx, ty, bf[0], bf[1], bf[2], A);
-            delA_delt(tx, ty, bf[0], bf[1], bf[2], dA);
-
-            // transport covMat
-            double delx_deltx0 = s;
-            double dely_deltx0 = 0.5 * Q * speedLight * s * s * dA[2];
-            double deltx_delty0 = Q * speedLight * s * dA[1];
-            double delx_delQ = 0.5 * speedLight * s * s * A[0];
-            double deltx_delQ = speedLight * s * A[0];
-            double delx_delty0 = 0.5 * Q * speedLight * s * s * dA[1];
-            double dely_delty0 = s;
-            double delty_deltx0 = Q * speedLight * s * dA[2];
-            double dely_delQ = 0.5 * speedLight * s * s * A[1];
-            double delty_delQ = speedLight * s * A[1];
-
+            rk.RK4transport( sector, Q, x, y, z, tx, ty, s, dcSwim,
+                        covMat, fVec, fCov, mass);
             
-            //double transpStateJacobian00=1; 
-            //double transpStateJacobian01=0; 
-            double transpStateJacobian02=delx_deltx0; 
-            double transpStateJacobian03=delx_delty0; 
-            double transpStateJacobian04=delx_delQ;
-            //double transpStateJacobian10=0; 
-            //double transpStateJacobian11=1; 
-            double transpStateJacobian12=dely_deltx0; 
-            double transpStateJacobian13=dely_delty0; 
-            double transpStateJacobian14=dely_delQ;
-            //double transpStateJacobian20=0; 
-            //double transpStateJacobian21=0; 
-            //double transpStateJacobian22=1; 
-            double transpStateJacobian23=deltx_delty0; 
-            double transpStateJacobian24=deltx_delQ;
-            //double transpStateJacobian30=0; 
-            //double transpStateJacobian31=0; 
-            double transpStateJacobian32=delty_deltx0; 
-            //double transpStateJacobian33=1; 
-            double transpStateJacobian34=delty_delQ;
-            //double transpStateJacobian40=0; 
-            //double transpStateJacobian41=0; 
-            //double transpStateJacobian42=0; 
-            //double transpStateJacobian43=0; 
-            //double transpStateJacobian44=1;
-            
-
-            //covMat = FCF^T; u = FC;
-            for (int j1 = 0; j1 < 5; j1++) {
-                u[0][j1] = covMat.covMat.get(0,j1) + covMat.covMat.get(2,j1) * transpStateJacobian02 + covMat.covMat.get(3,j1)* transpStateJacobian03 + covMat.covMat.get(4,j1) * transpStateJacobian04;
-                u[1][j1] = covMat.covMat.get(1,j1) + covMat.covMat.get(2,j1) * transpStateJacobian12 + covMat.covMat.get(3,j1) * transpStateJacobian13 + covMat.covMat.get(4,j1) * transpStateJacobian14;
-                u[2][j1] = covMat.covMat.get(2,j1) + covMat.covMat.get(3,j1) * transpStateJacobian23 + covMat.covMat.get(4,j1) * transpStateJacobian24;
-                u[3][j1] = covMat.covMat.get(2,j1) * transpStateJacobian32 + covMat.covMat.get(3,j1) + covMat.covMat.get(4,j1) * transpStateJacobian34;
-                u[4][j1] = covMat.covMat.get(4,j1);
-            }
-
-            for (int i1 = 0; i1 < 5; i1++) {
-                C[i1][0] = u[i1][0] + u[i1][2] * transpStateJacobian02 + u[i1][3] * transpStateJacobian03 + u[i1][4] * transpStateJacobian04;
-                C[i1][1] = u[i1][1] + u[i1][2] * transpStateJacobian12 + u[i1][3] * transpStateJacobian13 + u[i1][4] * transpStateJacobian14;
-                C[i1][2] = u[i1][2] + u[i1][3] * transpStateJacobian23 + u[i1][4] * transpStateJacobian24;
-                C[i1][3] = u[i1][2] * transpStateJacobian32 + u[i1][3] + u[i1][4] * transpStateJacobian34;
-                C[i1][4] = u[i1][4];
-            }
-
-            // Q  process noise matrix estimate	
-            double p = Math.abs(1. / Q);
-            double pz = p / Math.sqrt(1 + tx * tx + ty * ty);
-            double px = tx * pz;
-            double py = ty * pz;
-
-            double t_ov_X0 = Math.signum(Z[f] - Z[i]) * s / Constants.ARGONRADLEN; //path length in radiation length units = t/X0 [true path length/ X0] ; Ar radiation length = 14 cm
-
-            //double mass = this.MassHypothesis(this.massHypo); // assume given mass hypothesis
-            double mass = 0.000510998; // assume given mass hypothesis
-            if (Q > 0) {
-                mass = 0.938272029;
-            }
-
-            double beta = p / Math.sqrt(p * p + mass * mass); // use particle momentum
-            double cosEntranceAngle = Math.abs((x * px + y * py + z * pz) / (Math.sqrt(x * x + y * y + z * z) * p));
-            double pathLength = t_ov_X0 / cosEntranceAngle;
-
-            double sctRMS = (0.0136 / (beta * p)) * Math.sqrt(pathLength) * (1 + 0.038 * Math.log(pathLength)); // Highland-Lynch-Dahl formula
-
-            double cov_txtx = (1 + tx * tx) * (1 + tx * tx + ty * ty) * sctRMS * sctRMS;
-            double cov_tyty = (1 + ty * ty) * (1 + tx * tx + ty * ty) * sctRMS * sctRMS;
-            double cov_txty = tx * ty * (1 + tx * tx + ty * ty) * sctRMS * sctRMS;
-
-            if (s > 0) {
-                C[2][2] += cov_txtx;
-                C[2][3] += cov_txty;
-                C[3][2] += cov_txty;
-                C[3][3] += cov_tyty;
-            }
-
-           
-            covMat.covMat = new Matrix(C);
-            // transport stateVec
-            double dx = tx * s + 0.5 * Q * speedLight * A[0] * s * s;
-            x += dx;
-            double dy = ty * s + 0.5 * Q * speedLight * A[1] * s * s;
-            y +=dy;
-            tx += Q * speedLight * A[0] * s;
-            ty += Q * speedLight * A[1] * s;
-
-            z += s;
-            dPath+= Math.sqrt(dx*dx+dy*dy+s*s);
         }
-
-        StateVec fVec = new StateVec(f);
-        fVec.z = Z[f];
-        fVec.x = x;
-        fVec.y = y;
-        fVec.tx = tx;
-        fVec.ty = ty;
-        fVec.Q = Q;
-        fVec.B = Math.sqrt(bf[0]*bf[0]+bf[1]*bf[1]+bf[2]*bf[2]);
-        fVec.deltaPath = dPath;
-        //StateVec = fVec;
+        
         this.trackTraj.put(f, fVec);
-
-        //if(transpStateJacobian!=null) {
-        //	F = new Matrix(transpStateJacobian); 
-        //} 
-        if (covMat.covMat != null) {
-            CovMat fCov = new CovMat(f);
-            fCov.covMat = covMat.covMat;
-            //CovMat = fCov;
-            this.trackCov.put(f, fCov);
-        }
+        this.trackCov.put(f, fCov);
     }
-
+    
     
     private void A(double tx, double ty, double Bx, double By, double Bz, double[] a) {
 
@@ -311,14 +120,8 @@ public class StateVecs {
     }
 
     
-     /**
-        1 piMass = 0.13957018;
-        2 KMass = 0.493677;
-        3 muMass = 0.105658369;
-        0 eMass = 0.000510998;
-        4 pMass = 0.938272029;
-        **/
-    private void setMass(int hypo, double mass) {
+    private double mass = 0.13957018;
+    public void setMass(int hypo, double mass) {
           
         switch (hypo) {
             case 0:
@@ -338,7 +141,6 @@ public class StateVecs {
                 break;
         }
     }
-    
     
     
     /**
