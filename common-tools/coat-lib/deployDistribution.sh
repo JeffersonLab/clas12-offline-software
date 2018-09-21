d31629e8307bd1006c03cc251f1377dbbda29be1
raffaelladevita <devita@ge.infn.it>
2018-09-21 23:29:05 +0200

    ced: removed unused dependencies

--- common-tools/coat-lib/deployDistribution.sh
+++ common-tools/coat-lib/deployDistribution.sh
@@ -2,8 +2,8 @@
 # Script is exporting existing Jar files to repository
 #-------------------------------------------------------------------------------------------------
 #  JEVIO
-REPO="/Users/gavalian/Work/clas12-offline-software/myLocalMvnRepo"
-VERSION="5.1-SNAPSHOT"
+REPO="/Users/devita/clas12-offline-software/myLocalMvnRepo"
+VERSION="5.7.0-SNAPSHOT"
 
 mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file  -Dfile=target/coat-libs-5.7.0-SNAPSHOT.jar \
     -DgroupId=org.jlab.coat \
