65a2e4a1b0f4c9eff51013f801da5043329e9dee
David Heddle <dheddle@circadence.com>
2018-11-13 12:19:45 -0500

    start of direct db access for better or worse

--- common-tools/cnuphys/ced/src/main/java/cnuphys/ced/frame/Ced.java
+++ common-tools/cnuphys/ced/src/main/java/cnuphys/ced/frame/Ced.java
@@ -112,7 +112,7 @@ public class Ced extends BaseMDIApplication implements PropertyChangeListener,
 	// the singleton
 	private static Ced _instance;
 	
-	private static final String _release = "build 1.006";
+	private static final String _release = "build 1.006b";
 
 	// used for one time inits
 	private int _firstTime = 0;
