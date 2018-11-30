39cc3e616c290425eff95a8b81d82c0ce7704342
raffaelladevita <devita@ge.infn.it>
2018-11-29 19:35:16 -0500

    changed coat-libs snapshot to 5.7.2

--- common-tools/coat-lib/deployDistribution.sh
+++ common-tools/coat-lib/deployDistribution.sh
@@ -3,9 +3,9 @@
 #-------------------------------------------------------------------------------------------------
 #  JEVIO
 REPO="/Users/gavalian/Work/Software/project-5a.0.0/Distribution/clas12-offline-software/myLocalMvnRepo"
-VERSION="5.7.1-SNAPSHOT"
+VERSION="5.7.2-SNAPSHOT"
 
-mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file  -Dfile=target/coat-libs-5.7.1-SNAPSHOT.jar \
+mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file  -Dfile=target/coat-libs-5.7.2-SNAPSHOT.jar \
     -DgroupId=org.jlab.coat \
     -DartifactId=coat-libs \
     -Dversion=$VERSION \
