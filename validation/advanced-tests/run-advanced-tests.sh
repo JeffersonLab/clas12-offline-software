e3dcaa80169d2908092c57efcee97d06caf666a2
Raffaella De Vita <devita@ge.infn.it>
2019-05-03 18:58:21 -0400

    renamed hipo4 and hipo3 utils, EngineProcessor app (AKA notsouseful-util) now named recon-util

--- validation/advanced-tests/run-advanced-tests.sh
+++ validation/advanced-tests/run-advanced-tests.sh
@@ -51,7 +51,7 @@ if [ $? != 0 ] ; then echo "wget validation files failure" ; exit 1 ; fi
 tar -zxvf twoTrackEvents_809_raw.evio.tar.gz
 
 # run decoder
-$COAT/bin/decoder4 -t -0.5 -s 0.0 -i ./twoTrackEvents_809_raw.evio -o ./twoTrackEvents_809.hipo -c 2
+$COAT/bin/decoder -t -0.5 -s 0.0 -i ./twoTrackEvents_809_raw.evio -o ./twoTrackEvents_809.hipo -c 2
 
 # run reconstruction with clara
 echo "set inputDir $PWD/" > cook.clara
