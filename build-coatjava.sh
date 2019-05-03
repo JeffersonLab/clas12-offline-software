7dd04d044b7e93a8096a7953b4e74d46383f145e
Raffaella De Vita <devita@ge.infn.it>
2019-05-02 22:19:20 -0400

    updated versions of mon12 and kpp-plots

--- build-coatjava.sh
+++ build-coatjava.sh
@@ -65,8 +65,8 @@ cp external-dependencies/JEventViewer-1.1.jar coatjava/lib/clas/
 cp external-dependencies/vecmath-1.3.1-2.jar coatjava/lib/clas/
 mkdir -p coatjava/lib/utils
 cp external-dependencies/jclara-4.3-SNAPSHOT.jar coatjava/lib/utils
-cp external-dependencies/clas12mon-3.0.jar coatjava/lib/utils
-cp external-dependencies/KPP-Plots-2.0.jar coatjava/lib/utils
+cp external-dependencies/clas12mon-3.1.jar coatjava/lib/utils
+cp external-dependencies/KPP-Plots-3.0.jar coatjava/lib/utils
 #cp external-dependencies/jaw-1.0.jar coatjava/lib/utils
 mkdir -p coatjava/lib/services
 
