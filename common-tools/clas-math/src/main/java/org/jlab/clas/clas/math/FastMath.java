2e872e1eb749063ee39909006a88929a7f19a032
David Heddle <dheddle@circadence.com>
2019-05-24 16:46:34 -0400

    removed Slab class and fixed minor comflict in FastMath

--- common-tools/clas-math/src/main/java/org/jlab/clas/clas/math/FastMath.java
+++ common-tools/clas-math/src/main/java/org/jlab/clas/clas/math/FastMath.java
@@ -42,11 +42,7 @@ public class FastMath {
 		case FAST:
 			return org.apache.commons.math3.util.FastMath.atan2(y, x);
 		case SUPERFAST:
-<<<<<<< HEAD:common-tools/cnuphys/magfield/src/main/java/cnuphys/magfield/FastMath.java
-			return Icecore.atan2((float) y, (float) x);
-=======
 			return Icecore2.atan2((float)y, (float)x);
->>>>>>> development:common-tools/clas-math/src/main/java/org/jlab/clas/clas/math/FastMath.java
 		default:
 			return Math.atan2(y, x);
 		}
@@ -196,51 +192,5 @@ public class FastMath {
 	public static void setMathLib(MathLib lib) {
 		_mathLib = lib;
 	}
-
-<<<<<<< HEAD:common-tools/cnuphys/magfield/src/main/java/cnuphys/magfield/FastMath.java
-	// faster sin and cos from gamers
-	public static final class Riven {
-
-		private static final int SIN_BITS, SIN_MASK, SIN_COUNT;
-		private static final float radFull, radToIndex;
-		private static final float degFull, degToIndex;
-		private static final float[] sin, cos;
-
-		static {
-			SIN_BITS = 12;
-			SIN_MASK = ~(-1 << SIN_BITS);
-			SIN_COUNT = SIN_MASK + 1;
-
-			radFull = (float) (Math.PI * 2.0);
-			degFull = (float) (360.0);
-			radToIndex = SIN_COUNT / radFull;
-			degToIndex = SIN_COUNT / degFull;
-
-			sin = new float[SIN_COUNT];
-			cos = new float[SIN_COUNT];
-
-			for (int i = 0; i < SIN_COUNT; i++) {
-				sin[i] = (float) Math.sin((i + 0.5f) / SIN_COUNT * radFull);
-				cos[i] = (float) Math.cos((i + 0.5f) / SIN_COUNT * radFull);
-			}
-
-			// Four cardinal directions (credits: Nate)
-			for (int i = 0; i < 360; i += 90) {
-				sin[(int) (i * degToIndex) & SIN_MASK] = (float) Math.sin(i * Math.PI / 180.0);
-				cos[(int) (i * degToIndex) & SIN_MASK] = (float) Math.cos(i * Math.PI / 180.0);
-			}
-		}
-
-		public static final float sin(float rad) {
-			return sin[(int) (rad * radToIndex) & SIN_MASK];
-		}
-
-		public static final float cos(float rad) {
-			return cos[(int) (rad * radToIndex) & SIN_MASK];
-		}
-	}
-=======
-	
->>>>>>> development:common-tools/clas-math/src/main/java/org/jlab/clas/clas/math/FastMath.java
 
 }
