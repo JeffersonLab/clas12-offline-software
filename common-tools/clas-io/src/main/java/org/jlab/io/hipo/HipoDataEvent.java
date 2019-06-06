243e7bee0f88a1b31582bc320649bd2eec2287da
gavalian <gavalian@jlab.org>
2019-06-06 08:33:59 -0400

    added implementation for removeBank and removeBanks in HipoDataEvent.

--- common-tools/clas-io/src/main/java/org/jlab/io/hipo/HipoDataEvent.java
+++ common-tools/clas-io/src/main/java/org/jlab/io/hipo/HipoDataEvent.java
@@ -309,12 +309,17 @@ public class HipoDataEvent implements DataEvent {
 
     @Override
     public void removeBank(String bankName) {
+        if(schemaFactory.hasSchema(bankName)==true){
+            hipoEvent.remove(schemaFactory.getSchema(bankName));
+        }
         //this.hipoEvent.removeGroup(bankName);
     }
 
     @Override
     public void removeBanks(String... bankNames) {
-        
+        for(String bank : bankNames){
+            removeBank(bank);
+        }
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
