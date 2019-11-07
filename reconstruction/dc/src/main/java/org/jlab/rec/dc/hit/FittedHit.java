bb0de1f56b12c1a8d8e78e80913173c7ec0b4173
ziegler <ziegler@jlab.org>
2019-11-07 10:05:40 -0500

    Modified tBeta correction (off by default).

--- reconstruction/dc/src/main/java/org/jlab/rec/dc/hit/FittedHit.java
+++ reconstruction/dc/src/main/java/org/jlab/rec/dc/hit/FittedHit.java
@@ -368,18 +368,24 @@ public class FittedHit extends Hit implements Comparable<Hit> {
             double x = this.get_ClusFitDoca();
            
             double deltatime_beta = 0;
+            double deltatime_beta2 = 0;
             
             if (x != -1) {
                 if(Constants.useUSETIMETBETA()==true) {
                     deltatime_beta = calcDeltaTimeBetaTFCN(this.get_Time(), tab, beta);
+                    deltatime_beta2 = calcDeltaTimeBeta(this.get_Time(), tab, beta);
                 } else {
                     deltatime_beta = calcDeltaTimeBeta(x, tab, beta);
                 }
             }
             if(event.hasBank("MC::Particle")==false) {
-                distance = tde.interpolateOnGrid(B, Math.toDegrees(ralpha), this.getCorrectedTime(this.get_Time(), deltatime_beta), secIdx, slIdx) ;
+                distance = tde.interpolateOnGrid(B, Math.toDegrees(ralpha), 
+                        this.getCorrectedTime(this.get_Time(), deltatime_beta+deltatime_beta2), 
+                        secIdx, slIdx) ;
             } else {
-                distance = tde.interpolateOnGrid(B, Math.toDegrees(ralpha), this.getCorrectedTime(this.get_Time(), 0), secIdx, slIdx) ;
+                distance = tde.interpolateOnGrid(B, Math.toDegrees(ralpha), 
+                        this.getCorrectedTime(this.get_Time(), 0), 
+                        secIdx, slIdx) ;
             }
             //deltatime_beta = calcDeltaTimeBeta(distance, tab, beta);
             //deltatime_beta = calcDeltaTimeBeta(distance, this.get_Superlayer(), beta);
@@ -406,9 +412,9 @@ public class FittedHit extends Hit implements Comparable<Hit> {
     }
     
     public double calcDeltaTimeBetaTFCN(double t,IndexedTable tab, double beta){
-        double db = tab.getDoubleValue("distbeta", this.get_Sector(), 
+        double delt = tab.getDoubleValue("c3", this.get_Sector(), 
                         this.get_Superlayer(),0);
-        double delt = db/Constants.AVEDRIFTVEL; 
+        
         //see [CLAS-Note 96-008]
         double tBeta = (0.5 *delt*delt*delt*t)/(delt*delt*delt+t*t*t);
         return tBeta*beta*beta;
