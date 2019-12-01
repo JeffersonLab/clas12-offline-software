4234ad19bcb4e3883f1d6fecb995a62c645591c6
baltzell <baltzell@gmx.com>
2019-12-01 18:21:08 -0500

    auto-update deploy script

--- version-bump.sh
+++ version-bump.sh
@@ -11,8 +11,10 @@ new=$2
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   find . -type f -name pom.xml -exec sed -i '' -e "s/$old-SNAPSHOT/$new-SNAPSHOT/" "{}" \;
+  find . -type f -name deployDistribution.sh -exec sed -i '' -e "s/$old/$new/" "{}" \;
 else
   find . -type f -name pom.xml -exec sed -i -e "s/$old-SNAPSHOT/$new-SNAPSHOT/" "{}" \;
+  find . -type f -name deployDistribution.sh -exec sed -i -e "s/$old/$new/" "{}" \;
 fi
 
 
