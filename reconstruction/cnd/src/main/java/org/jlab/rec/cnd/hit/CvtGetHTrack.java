1fc0a144cfd1d393644969ba93a4eea5ec348a2e
Pierre Chatagnon <33055626+PChatagnon@users.noreply.github.com>
2020-03-03 11:40:58 +0100

    Use swimmer for track matching, pathlegth and path in bar

--- reconstruction/cnd/src/main/java/org/jlab/rec/cnd/hit/CvtGetHTrack.java
+++ reconstruction/cnd/src/main/java/org/jlab/rec/cnd/hit/CvtGetHTrack.java
@@ -3,42 +3,118 @@ package org.jlab.rec.cnd.hit;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jlab.geom.prim.Arc3D;
+import org.jlab.geom.prim.Cylindrical3D;
 import org.jlab.geom.prim.Point3D;
+import org.jlab.geometry.prim.Line3d;
 import org.jlab.io.base.DataBank;
 import org.jlab.io.base.DataEvent;
 import org.jlab.rec.cnd.constants.CalibrationConstantsLoader;
 import org.jlab.rec.cnd.constants.Parameters;
 import org.jlab.rec.cvt.trajectory.Helix;
 
+import eu.mihosoft.vrl.v3d.Vector3d;
+
 public class CvtGetHTrack { // this class is used to extract helical tracks from the cvt event bank. 
 
-    public CvtGetHTrack() {
-        helices = new ArrayList<CVTTrack>();
-    }
+	public CvtGetHTrack() {
+		helices = new ArrayList<CVTTrack>();
+	}
 
-    private List<CVTTrack> helices;
+	private List<CVTTrack> helices;
 
-    public List<CVTTrack> getHelices() {
-        return helices;
-    }
+	public List<CVTTrack> getHelices() {
+		return helices;
+	}
 
-    public void setHelices(List<CVTTrack> helices) {
-        this.helices = helices;
-    }
+	public void setHelices(List<CVTTrack> helices) {
+		this.helices = helices;
+	}
 
-    public void getCvtHTrack(DataEvent event, CalibrationConstantsLoader ccdb) {
+	public void getCvtHTrack(DataEvent event, CalibrationConstantsLoader ccdb) {
 
-        helices.clear();
+		helices.clear();
 
-        if (event == null) { // check if there is an event
-            //System.out.println(" no event");
-        }
+		if (event == null) { // check if there is an event
+			//System.out.println(" no event");
+		}
 
-        if (event.hasBank("CVTRec::Tracks") == false) {
+		if (event.hasBank("CVTRec::Trajectory") == false) {
+			// check if there are some cvt tracks in the event
+			//System.out.println(" no cvt tracks");
+		}
+		
+		System.out.println();
+		
+		if (event.hasBank("CVTRec::Trajectory") == true) {
+
+			DataBank bank = event.getBank("CVTRec::Trajectory");
+
+			int nt = bank.rows();   // number of tracks in the cvt event
+
+			for (int i = 0; i < nt; i++) {
+
+				
+				if(bank.getByte("detector", i)==3 && bank.getByte("layer", i)==1) { //make use of the fact that the 3 cnd layer are in order so find layer 1 and loop through the 2 next entries
+					CVTTrack trk = new CVTTrack();
+					for(int j=0; j<3;j++) {
+
+						int id       = (bank.getShort("id", i+j))-1;
+						int layer	  = bank.getByte("layer", i+j);
+						double x     = bank.getFloat("x", i+j);
+						double y     = bank.getFloat("y", i+j);
+						double z     = bank.getFloat("z", i+j);
+						double theta = bank.getFloat("theta", i+j);
+						double phi   = bank.getFloat("phi", i+j);                
+						double ux    = Math.sin(theta)*Math.cos(phi);
+						double uy    = Math.sin(theta)*Math.sin(phi);
+						double uz    = Math.cos(theta);
+						double path  = bank.getFloat("path", i+j);
+
+
+						trk.set_Id(id);
+
+						double entryradius  = (ccdb.INNERRADIUS[0] + (layer - 1) * ccdb.THICKNESS[0] + (layer - 1) * Parameters.LayerGap)/10.;
+						double escaperadius = (ccdb.INNERRADIUS[0] + (layer) * ccdb.THICKNESS[0] + (layer - 1) * Parameters.LayerGap)/10.;
+
+						//find intercept of line defined by interaction point + director cosines with cylinder of radius defined above
+
+						double b = 2*(ux*x+uy*y);
+						double a = ux*ux+uy*uy;
+						double co = x*x+y*y-((escaperadius*escaperadius));
+						double ci = x*x+y*y-((entryradius*entryradius));
+
+						double uo1 = (-b+Math.sqrt(b*b-4*a*co))/(2*a); 
+						double ui1 = (-b+Math.sqrt(b*b-4*a*ci))/(2*a); 
+						
+
+						Point3D entryPoint = new Point3D((x+ui1*ux)*10,(y+ui1*uy)*10,(z+ui1*uz)*10);
+						Point3D midPoint = new Point3D(x*10,y*10,z*10);
+						Point3D exitPoint = new Point3D((x+uo1*ux)*10,(y+uo1*uy)*10,(z+uo1*uz)*10);
+
+						trk._TrkInters.get(layer - 1).add(entryPoint);
+						trk._TrkInters.get(layer - 1).add(midPoint);
+						trk._TrkInters.get(layer - 1).add(exitPoint);
+
+						trk._TrkLengths.add(path*10);
+
+						helices.add(trk);
+	    				//System.out.println(layer + " "+id +"path in paddle new "+entryPoint.distance(exitPoint)+ " pathlength "+ trk._TrkInters.get(layer-1)+ " id "+id);
+
+					} 
+				}
+
+			}	
+
+		}
+
+
+			//old code (kept only for quick reference)
+		
+     /*   if (event.hasBank("CVTRec::Tracks") == false) {
 			// check if there are some cvt tracks in the event
             //System.out.println(" no cvt tracks");
         }
-
         if (event.hasBank("CVTRec::Tracks") == true) {
 
             //System.out.println(" cvt tracks present");
@@ -62,7 +138,7 @@ public class CvtGetHTrack { // this class is used to extract helical tracks from
                 int q         = bank.getInt("q", i);
                 double pt     = bank.getFloat("pt", i);
                 // use the formula R=pt/qB to get the curvature (see cvt reconstruction class Track)/Pt is in Gev
-                double rho = (0.000299792458 * q * 5. * config.getFloat("solenoid", 0)) / pt;
+                double rho = (0.000299792458 * q * 5. * -1.) / pt;
 				// the max value of B is set to 5.Tesla 
                 //double p=bank.getFloat("p", i);
 
@@ -113,71 +189,78 @@ public class CvtGetHTrack { // this class is used to extract helical tracks from
                     double r = Math.sqrt(Xm.x() * Xm.x() + Xm.y() * Xm.y());
                     double par = 1. - ((r * r - dca * dca) * rho * rho) / (2. * (1. + dca * Math.abs(rho)));
                     double pathLengthXY = Math.abs(Math.acos(par) / rho);
-                    
+
                     double pathLength = Math.sqrt((Xm.z() - z0 )*(Xm.z() - z0 )+pathLengthXY*pathLengthXY);
                     trk._TrkLengths.add(pathLength);
+                    
+    				System.out.println(lay + " "+trkID +"path in paddle old "+trk.get_Helix().getPointAtRadius(entryradius).distance(trk.get_Helix().getPointAtRadius(escaperadius))+ " pathlength "+ trk._TrkInters.get(lay-1)+ " id "+trkID);
+
                 }
-                helices.add(trk);
+                
+
+               // helices.add(trk);
             }
 
-        }
+        }//end
+        
+        */
 
-    }
+	}
 
-    /**
-     * A class to hold the CVT track information relevant for CND analysis
-     *
-     * @author ziegler
-     *
-     */
-    public class CVTTrack {
+	/**
+	 * A class to hold the CVT track information relevant for CND analysis
+	 *
+	 * @author ziegler
+	 *
+	 */
+	public class CVTTrack {
 
-        private Helix _Helix;
-        private int _Id;
+		private Helix _Helix; // not in use since swimmer point is used 
+		private int _Id;
 
 		// The position of the tracks at the relevant radii is accessed as an ordered List of List:
-        // first index 0...2 is layer index
-        // second index 0...2 corresponds to entrance, middle and exit of track wrt the counter
-        private List<ArrayList<Point3D>> _TrkInters = new ArrayList<ArrayList<Point3D>>(); // intersection point wrt front middle and end of counter
-        private List<Double>             _TrkLengths = new ArrayList<Double>();  //the pathlength of the track from the doca to the beam line to the middle of the CND counter
+		// first index 0...2 is layer index
+		// second index 0...2 corresponds to entrance, middle and exit of track wrt the counter
+		private List<ArrayList<Point3D>> _TrkInters = new ArrayList<ArrayList<Point3D>>(); // intersection point wrt front middle and end of counter
+		private List<Double>             _TrkLengths = new ArrayList<Double>();  //the pathlength of the track from the doca to the beam line to the middle of the CND counter
 
-        public CVTTrack() {
-            for (int i = 0; i < 3; i++) {
-                _TrkInters.add(new ArrayList<Point3D>());
-            }
-        }
+		public CVTTrack() {
+			for (int i = 0; i < 3; i++) {
+				_TrkInters.add(new ArrayList<Point3D>());
+			}
+		}
 
-        public Helix get_Helix() {
-            return _Helix;
-        }
+		public Helix get_Helix() {
+			return _Helix;
+		}
 
-        public void set_Helix(Helix _Helix) {
-            this._Helix = _Helix;
-        }
+		public void set_Helix(Helix _Helix) {
+			this._Helix = _Helix;
+		}
 
-        public int get_Id() {
-            return _Id;
-        }
+		public int get_Id() {
+			return _Id;
+		}
 
-        public void set_Id(int _Id) {
-            this._Id = _Id;
-        }
+		public void set_Id(int _Id) {
+			this._Id = _Id;
+		}
 
-        public List<ArrayList<Point3D>> get_TrkInters() {
-            return _TrkInters;
-        }
+		public List<ArrayList<Point3D>> get_TrkInters() {
+			return _TrkInters;
+		}
 
-        public void set_TrkInters(List<ArrayList<Point3D>> _TrkInters) {
-            this._TrkInters = _TrkInters;
-        }
+		public void set_TrkInters(List<ArrayList<Point3D>> _TrkInters) {
+			this._TrkInters = _TrkInters;
+		}
 
-        public List<Double> get_TrkLengths() {
-            return _TrkLengths;
-        }
+		public List<Double> get_TrkLengths() {
+			return _TrkLengths;
+		}
 
-        public void set_TrkLengths(List<Double> _PathLengths) {
-            this._TrkLengths = _PathLengths;
-        }
+		public void set_TrkLengths(List<Double> _PathLengths) {
+			this._TrkLengths = _PathLengths;
+		}
 
-    }
+	}
 }
