e6615521aeb6a77dc7fbcd82fd2444d7ffac6b07
Pierre Chatagnon <33055626+PChatagnon@users.noreply.github.com>
2020-04-14 15:12:06 +0200

    Add status and sorted tdc

--- reconstruction/cnd/src/main/java/org/jlab/rec/cnd/banks/HitReader.java
+++ reconstruction/cnd/src/main/java/org/jlab/rec/cnd/banks/HitReader.java
@@ -44,14 +44,46 @@ public class HitReader {
 		int tdc=0;
 		int adc=0;
 		
+		
+		//Sort the TDC bank using index. This allows to reproduce double hits in simulation.
+		// The TDCs are sorted in descending order.
+		
+		int sortedIndTDC[];
+		sortedIndTDC = new int[ntdc]; 
+		int AlreadysortedIndTDC[];
+		AlreadysortedIndTDC = new int[ntdc]; 
+		for(int i=0; i<ntdc; i++){//index in the sorted array
+			int index=-1000;
+			int TDC= -50000;
+			for(int j=0; j<ntdc; j++){//index in the bank
+				int tdcTemp = bankTDC.getInt("TDC",j);
+				if(tdcTemp>TDC & AlreadysortedIndTDC[j]==0) {
+					TDC=tdcTemp;
+					index=j;
+				}
+			}
+			sortedIndTDC[i]=index;
+			AlreadysortedIndTDC[index]=1;
+		}
+		
+		/*System.out.println("Sorted TDC");
+		bankTDC.show();
+		System.out.println("Sorted TDC");
+		for(int i=0; i<ntdc; i++){//index in the sorted array
+			System.out.println(i+"   "+sortedIndTDC[i]+" "+bankTDC.getInt("TDC",sortedIndTDC[i]));
+		}*/
+		
 		// starts looking at the adc bank
 		for(int i = 0; i<nadc; i++)
 		{ 
+			
+			
 			int sector    = bankADC.getByte("sector",i);  // one of the 24 "blocks"
 			int layer     = bankADC.getByte("layer",i);  
 			int order     = bankADC.getByte("order",i);
 			int component = order + 1; // get the component 1 is left 2 is right
 			int indextdc = -1;
+			
 			
 			adc = bankADC.getInt("ADC",i); 
 			boolean ignorePaddle = false; // whether to ignore or not the processing paddle (as there is already a half it in the paddle)
@@ -64,17 +96,17 @@ public class HitReader {
 				}
 			}
 			
-			//know look for a tdc that is matching the adc. Starts from the beggining of the tdc list so only the first tdc is taken.
+			//now look for a tdc that is matching the adc. Starts from the beggining of the tdc list so only the first tdc is taken.
 			if(ignorePaddle==false){
 				for(int j=0; j<ntdc; j++){
-					int s = bankTDC.getByte("sector", j);
-                   			int l = bankTDC.getByte("layer", j);
-                 			int o = bankTDC.getByte("order", j);
+					int s = bankTDC.getByte("sector", sortedIndTDC[j]);
+                   			int l = bankTDC.getByte("layer", sortedIndTDC[j]);
+                 			int o = bankTDC.getByte("order", sortedIndTDC[j]);
 
 					if(s==sector && l == layer  && o == order+2 ){
 					//System.out.println("s "+ s+" sector "+sector+" l "+l+" layer "+layer+" o "+o+" order "+order);
-					tdc = bankTDC.getInt("TDC",j);
-					indextdc=j;
+					tdc = bankTDC.getInt("TDC",sortedIndTDC[j]);
+					indextdc=sortedIndTDC[j];
 					break;
 					}
 				}
@@ -85,7 +117,9 @@ public class HitReader {
 
 			// First, carry out checks on the quality of the signals:	    	  
 			if (adc == 0 || tdc == 0 || tdc == Parameters.NullTDC || indextdc==-1) continue; // require good ADC and TDC values
+			if(ccdb.Status_LR[sector-1][layer-1][component-1]==1)continue; //if status is one then ignore halfhit and pass to the next one in the list
 
+			
 			newhit = new HalfHit(sector, layer, component, triggerPhase, adc, tdc, i,indextdc, ccdb); 
 
 			halfhits.add(newhit);
