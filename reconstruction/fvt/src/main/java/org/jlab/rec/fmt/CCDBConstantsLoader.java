1b9d951ad722df7dc3177918413c13130256c7c3
bleaktwig <bleak.twig@gmail.com>
2020-07-29 16:26:57 -0400

    marked code locations for shifts in CCDB loader

--- reconstruction/fvt/src/main/java/org/jlab/rec/fmt/CCDBConstantsLoader.java
+++ reconstruction/fvt/src/main/java/org/jlab/rec/fmt/CCDBConstantsLoader.java
@@ -9,20 +9,21 @@ import org.jlab.detector.calib.utils.DatabaseConstantProvider;
 public class CCDBConstantsLoader {
 
     static boolean CSTLOADED = false;
-    public static final synchronized void Load(int runNb, String variation) {
+
+    public static final synchronized void Load(int runNb, String variation, double[][] shArr) {
         if (CSTLOADED) return;
 
         double[] EFF_Z_OVER_A;
-        double[] EFFX0  ;
+        double[] EFFX0;
         double[] REL_POS;
 
         // Load the tables
         DatabaseConstantProvider dbprovider = new DatabaseConstantProvider(10, variation);
 
-        //load material budget:
+        // Load material budget:
         dbprovider.loadTable("/test/mvt/fmt_mat");
 
-        // material budget
+        // Material budget
         // ===============
         EFF_Z_OVER_A = new double[dbprovider.length("/test/mvt/fmt_mat/thickness")];
         EFFX0 = new double[dbprovider.length("/test/mvt/fmt_mat/thickness")];
@@ -40,7 +41,7 @@ public class CCDBConstantsLoader {
         Constants.set_X0(EFFX0);
         Constants.set_RELPOS(REL_POS);
 
-        // Common variables for all disks
+        // Get common variables for all disks and apply shifts
         // ===============
 	    dbprovider.loadTable("/geometry/fmt/fmt_global");
 	    Constants.hDrift            = dbprovider.getDouble("/geometry/fmt/fmt_global/hDrift", 0)/10.;
@@ -49,24 +50,28 @@ public class CCDBConstantsLoader {
         Constants.FVT_Beamhole      = dbprovider.getDouble("/geometry/fmt/fmt_global/R_min", 0)/10.;
 	    Constants.FVT_Nstrips       = dbprovider.getInteger("/geometry/fmt/fmt_global/N_strip", 0);
 	    Constants.FVT_Halfstrips    = dbprovider.getInteger("/geometry/fmt/fmt_global/N_halfstr", 0);
+        // TODO: Apply x and y shifts in lines 54 to 59! ===========================================
 	    Constants.FVT_stripsXloc    = new double[Constants.FVT_Nstrips][2]; // Give the local end-points x-coordinates of the strip segment
         Constants.FVT_stripsYloc    = new double[Constants.FVT_Nstrips][2]; // Give the local end-points y-coordinates of the strip segment
         Constants.FVT_stripsXlocref = new double[Constants.FVT_Nstrips]; // Give the local ref-points x-coordinates of the strip segment
         Constants.FVT_stripsYlocref = new double[Constants.FVT_Nstrips]; // Give the local ref-points y-coordinates of the strip segment
         Constants.FVT_stripsX       = new double[Constants.FVT_Nlayers][Constants.FVT_Nstrips][2]; // Give the  end-points x-coordinates of the strip segment rotated in the correct frame for the layer
         Constants.FVT_stripsY       = new double[Constants.FVT_Nlayers][Constants.FVT_Nstrips][2]; // Give the  end-points y-coordinates of the strip segment
+        // =========================================================================================
         Constants.FVT_stripslength  = new double[Constants.FVT_Nstrips]; // Give the strip length
 
         // Position and strip orientation of each disks
         // ===============
         dbprovider.loadTable("/geometry/fmt/fmt_layer_noshim");
 
+        // TODO: Apply z shifts and rotations from lines 68 to 73! =================================
         Constants.FVT_Zlayer = new double[dbprovider.length("/geometry/fmt/fmt_layer_noshim/Z")];
         Constants.FVT_Alpha = new double[dbprovider.length("/geometry/fmt/fmt_layer_noshim/Z")];
         for (int i = 0; i < dbprovider.length("/geometry/fmt/fmt_layer_noshim/Z"); i++) {
             Constants.FVT_Zlayer[i]=dbprovider.getDouble("/geometry/fmt/fmt_layer_noshim/Z", i)/10.;
             Constants.FVT_Alpha[i]=Math.toRadians(dbprovider.getDouble("/geometry/fmt/fmt_layer_noshim/Angle", i));
         }
+        // =========================================================================================
 
         dbprovider.disconnect();
 
