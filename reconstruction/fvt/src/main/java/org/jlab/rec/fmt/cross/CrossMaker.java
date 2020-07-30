63289401fedd69065e8bad41bff104642bdd7d5f
bleaktwig <bleak.twig@gmail.com>
2020-07-30 11:15:09 -0400

    no changes are needed in cluster or cross packages

--- reconstruction/fvt/src/main/java/org/jlab/rec/fmt/cross/CrossMaker.java
+++ reconstruction/fvt/src/main/java/org/jlab/rec/fmt/cross/CrossMaker.java
@@ -13,11 +13,9 @@ import org.jlab.rec.fmt.cluster.Cluster;
  */
 
 public class CrossMaker {
-	
-	public CrossMaker() {
-		
-	}
-	
+
+	public CrossMaker() {}
+
 	public ArrayList<Cross> findCrosses(List<Cluster> clusters) {
 
 		// first separate the segments according to layers
@@ -26,44 +24,32 @@ public class CrossMaker {
 
 		// Sorting by layer first:
 		for (Cluster theclus : clusters){
-			if(theclus.get_Layer()%2==0) { 
-				allouterlayrclus.add(theclus); 
-			} 
-			if(theclus.get_Layer()%2==1) { 
-				allinnerlayrclus.add(theclus);
-			}
+			if (theclus.get_Layer()%2 == 0) allouterlayrclus.add(theclus);
+			if (theclus.get_Layer()%2 == 1) allinnerlayrclus.add(theclus);
 		}
 
 		ArrayList<Cross> crosses = new ArrayList<Cross>();
 
 		int rid =0;
-		for(Cluster inlayerclus : allinnerlayrclus){
-			for(Cluster outlayerclus : allouterlayrclus){
-				if(outlayerclus.get_Layer()-inlayerclus.get_Layer()!=1)
-					continue;
-				if(outlayerclus.get_Sector()!=inlayerclus.get_Sector())
-					continue;
-				if( (inlayerclus.get_MinStrip()+outlayerclus.get_MinStrip() > 1) 
-						&& (inlayerclus.get_MaxStrip()+outlayerclus.get_MaxStrip() < Constants.FVT_Nstrips*2) ) { // put correct numbers to make sure the intersection is valid
+		for (Cluster inlayerclus : allinnerlayrclus) {
+			for (Cluster outlayerclus : allouterlayrclus) {
+				if (outlayerclus.get_Layer() - inlayerclus.get_Layer() != 1) continue;
+				if (outlayerclus.get_Sector() != inlayerclus.get_Sector()) continue;
+                // put correct numbers to make sure the intersection is valid
+				if ((inlayerclus.get_MinStrip() + outlayerclus.get_MinStrip() > 1)
+						&& (inlayerclus.get_MaxStrip() + outlayerclus.get_MaxStrip() < Constants.FVT_Nstrips*2) ) {
 
-					// define new cross 
+					// define new cross
 					Cross this_cross = new Cross(inlayerclus.get_Sector(), inlayerclus.get_Region(),rid++);
 					this_cross.set_Cluster1(inlayerclus);
 					this_cross.set_Cluster2(outlayerclus);
-					
-					this_cross.set_CrossParams();
-					//make arraylist
-					crosses.add(this_cross);
 
+					this_cross.set_CrossParams();
+					// make arraylist
+					crosses.add(this_cross);
 				}
 			}
 		}
 		return crosses;
 	}
-	
-	
-	
 }
-
-
-
