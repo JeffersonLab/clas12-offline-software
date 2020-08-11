0158735545cff02431b596c0d9073b2c0d5200c9
raffaelladevita <devita@ge.infn.it>
2020-08-10 23:52:51 -0400

    Background merging tools (#562)
    * moved event merger to clas-analysis, finalized merging code and created random trigger filtering tools
    
    * dropped DC::doca in bg merging to avoid inconsistency with number of entries in DC::tdc
    
    * fixed bg merging tools usage instructions
    
    * added file splitting to create trigger files with fixed number of events
    
    * added file splitting to create trigger files with fixed number of events
    
    * evio2hipo: added conversion of CND fADC time
    
    * added statistics analysis to triggerBitFilter, bgMerger now handles all detectors based in command line list, added comments
    
    * bug fix in beam current threshold for random trigger selection
    
    * trigger bit filter: moved charge calculation to DaqScalersSequence class
    
    * DaqScalerSequence: added Interval class to handle single scaler readouts
    
    * added tag-2 bank and saving/transfer of non-zero tag events
    
    * bg merging: changed file prefix for file splitter and removed dependency on environment variable
    
    * BG merging: modied trigger bit filtering (only selected bit should be set) and added doublee-hit suppression  on/off option to merging tool
    
    * BG merging: modied trigger bit filtering (only selected bit should be set) and added doublee-hit suppression  on/off option to merging tool
    
    * BG merging added correction for TDC jitter
    
    * BG Merging: more realistic emulation of DC readout, class renaming
    
    * EventMerger: reading constants from CCDB
    
    * EventMerger: fixed problem with CCDB constants
    
    * Event Merger: added missing java docs
    
    * clas-analysis: removed unnecessary dependency in pom file
    
    * restored accidentally dropped RUN::scaler, modify bg merging script to remove bashisms
    
    * renamed bg merging scripts

---
+++ common-tools/clas-analysis/src/main/java/org/jlab/analysis/eventmerger/EventMergerEnum.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jlab.analysis.eventmerger;
+
+/**
+ * Event merger constants enum
+ * 
+ * @author devita
+ */
+public enum EventMergerEnum {
+    TIME_JITTER,
+    READOUT_PAR,
+    TDC_CONV,
+    JITTER_PHASE,
+    JITTER_CYCLES,
+    JITTER_PERIOD,
+    READOUT_WINDOW,
+    READOUT_HOLDOFF
+}
