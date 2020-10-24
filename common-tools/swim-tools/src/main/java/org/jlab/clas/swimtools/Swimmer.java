6e7ca72d7e6527420ab8a4734eed64b9c745ec65
Veronique Ziegler <ziegler@jlab.org>
2020-10-23 22:14:08 -0400

    Ability to track in zero field.

--- common-tools/swim-tools/src/main/java/org/jlab/clas/swimtools/Swimmer.java
+++ common-tools/swim-tools/src/main/java/org/jlab/clas/swimtools/Swimmer.java
@@ -65,6 +65,8 @@ public class Swimmer {
         }
         if(TorusScale==0)
             TorusScale=0.000001;
+        if(SolenoidScale==0)
+            SolenoidScale=0.000001;
         MagneticFields.getInstance().getTorus().setScaleFactor(TorusScale);
         MagneticFields.getInstance().getSolenoid().setScaleFactor(SolenoidScale);
         MagneticFields.getInstance().setSolenoidShift(shift);
