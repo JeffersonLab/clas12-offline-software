b9f6497d2185cef1a3b44efc51bf8154e8926cfd
Rafayel Paremuzyan <rafopar@jlab.org>
2021-01-28 10:03:21 -0500

    A commented line was removed, for the code to look more clean

--- common-tools/clas-reco/src/main/java/org/jlab/clas/reco/EngineProcessor.java
+++ common-tools/clas-reco/src/main/java/org/jlab/clas/reco/EngineProcessor.java
@@ -101,7 +101,6 @@ public class EngineProcessor {
             "org.jlab.service.eb.EBTBEngine",
             "org.jlab.rec.rich.RICHEBEngine",
             "org.jlab.service.rtpc.RTPCEngine",
-	    //"org.jlab.service.mc.TruthMatching"
 	    "org.jlab.service.mc.TruthMatch"
         };
 
