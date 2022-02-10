fd53b463f718100a8fd2aa2eac93bc5da7896cd7
Alessandra Filippi <filippi@to.infn.it>
2022-02-10 15:25:16 +0100

    tuning of TRK constants for reco

--- reconstruction/ft/src/main/java/org/jlab/rec/ft/FTConstants.java
+++ reconstruction/ft/src/main/java/org/jlab/rec/ft/FTConstants.java
@@ -16,28 +16,29 @@ public class FTConstants {
     public static final double  CAL_HODO_DISTANCE_MATCHING = 3.0;   // matching distance in cm
     public static final double  CAL_HODO_TIME_MATCHING     = 8;    // matching time in ns (it was 8)
     
-//    public static final double  CAL_TRK_DISTANCE_MATCHING = 1.5;   // matching distance in cm
-//    public static final double  CAL_TRK_TIME_MATCHING     = 30;   // matching time in ns TOBECORRECTED
-      public static final double  CAL_TRK_DISTANCE_MATCHING = 3.;   // matching distance in cm (it was 3)
-      public static final double  CAL_TRK_TIME_MATCHING     = 100;   // matching time in ns TOBECORRECTED (it was 300)
+    public static final double  CAL_TRK_DISTANCE_MATCHING = 3.;    // matching distance in cm (it was 1.5)
+    public static final double  CAL_TRK_TIME_MATCHING     = 200;   // matching time in ns TOBECORRECTED (it was 30 - 300 - 100 - 150)
 
-
+    public static final double  TRK_STRIP_MIN_TIME = 50;                 // minimum time value associated to one strip
+    public static final double  TRK_STRIP_MAX_TIME = 350;                 // minimum time value associated to one strip
     
     public static final double  TRK_MIN_CROSS_NUMBER = 1;           // minimum number of crosses to find a line in the tracker
 //    public static final double  TRK0_TRK1_DISTANCE_MATCHING = 0.25;  // matching distance between FTTRK points in cms
 //    public static final double  TOLERANCE_ON_CROSSES_TWO_DETECTORS = 0.; // 1. cm radius tolerance, tune it up
-//    public static final double  TRK0_TRK1_RADTOL = 2.5;              // max tolerance for TRK0/TRK1 distance (3D)
+    public static final double  TRK0_TRK1_RADTOL = 1.;              // max tolerance for TRK0/TRK1 distance (3D)
 ///    public static final double  TRK0_TRK1_RADTOL = 0.5;             // max tolerance for TRK0/TRK1 radial distance (2D)
 ///    public static final double  TRK0_TRK1_PHITOL = 0.07;             // max tolerance for TRK0/TRK1 phi angular deviation
 ///    public static final double  TRK0_TRK1_THETATOL = 0.07;           // max tolerance for TRK0/TRK1 theta angulare deviation
-    public static final double  TRK0_TRK1_RADTOL = 10.;             // max tolerance for TRK0/TRK1 radial distance (2D)
-    public static final double  TRK0_TRK1_PHITOL = 1.;             // max tolerance for TRK0/TRK1 phi angular deviation
-    public static final double  TRK0_TRK1_THETATOL = 1.;           // max tolerance for TRK0/TRK1 theta angulare deviation
+//    public static final double  TRK0_TRK1_RADTOL = 10.;             // max tolerance for TRK0/TRK1 radial distance (2D)
+//    public static final double  TRK0_TRK1_PHITOL = 1.;             // max tolerance for TRK0/TRK1 phi angular deviation
+//    public static final double  TRK0_TRK1_THETATOL = 1.;           // max tolerance for TRK0/TRK1 theta angulare deviation
+      public static final double  TRK0_TRK1_PHITOL = 0.7;             // max tolerance for TRK0/TRK1 phi angular deviation
+      public static final double  TRK0_TRK1_THETATOL = 0.7;
     
     public static final double  FTTRKMinAdcThreshold = 18.;             // check min threshold for FTTTRK ADCs (18)
-    public static final double  FTTRKMaxAdcThreshold = 1000.;            // check max threshold for FTTTRK ADCs (18)
+    public static final double  FTTRKMaxAdcThreshold = 2000.;           // check max threshold for FTTTRK ADCs (18) was 1000 170122
+    public static final int     TRK_MAX_StripsForTruncatedMean = 20;          // threshold for maxnumber of strips included in the truncated mean algo.
     public static final double  TRK_MIN_CLUS_ENERGY = 20.;              // minimum cluster energy to accept a signal cluster (20)
     public static final int     TRK_MIN_CLUS_SIZE = 0;                  // minimum cluster size to form crosses (0: no limit on cluster size) // 2 is very strict
-
-
+    public static final int     TRK_MAXNUMBEROFHITS = 100;                // maximum number of accepted hits in the tracker banks
 }
