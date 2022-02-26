83597981e4ee1996dbdd68206b254d03aea8a23a
Alessandra Filippi <filippi@to.infn.it>
2022-02-26 14:43:39 +0100

    tuning of parameters, rename TRK0->TRK1, TRK1->TRK2

--- reconstruction/ft/src/main/java/org/jlab/rec/ft/FTConstants.java
+++ reconstruction/ft/src/main/java/org/jlab/rec/ft/FTConstants.java
@@ -24,13 +24,14 @@ public class FTConstants {
     public static final double  TRK_STRIP_MAX_TIME = 350;                 // minimum time value associated to one strip
     
     public static final double  TRK_MIN_CROSS_NUMBER = 1;           // minimum number of crosses to find a line in the tracker
-    public static final double  TRK0_TRK1_RADTOL = 0.05;              // max tolerance for TRK0/TRK1 distance (3D) (was 1.-0.5)
-    public static final double  TRK0_TRK1_PHITOL = 0.07;             // max tolerance for TRK0/TRK1 phi angular deviation (was 1.-0.7)
-    public static final double  TRK0_TRK1_THETATOL = 0.07;           // max tolerance for TRK0/TRK1 theta angulare deviation (was 1.-0.7)
+    public static final double  TRK1_TRK2_RADTOL = 0.05;              // max tolerance for TRK0/TRK1 distance (3D) (was 1.-0.5)
+    public static final double  TRK1_TRK2_PHITOL = 0.07;             // max tolerance for TRK0/TRK1 phi angular deviation (was 1.-0.7)
+    public static final double  TRK1_TRK2_THETATOL = 0.07;           // max tolerance for TRK0/TRK1 theta angulare deviation (was 1.-0.7)
     
     public static final double  FTTRKMinAdcThreshold = 18.;             // check min threshold for FTTTRK ADCs (18)
     public static final double  FTTRKMaxAdcThreshold = 2000.;           // check max threshold for FTTTRK ADCs (18) was 1000 170122
     public static final int     TRK_MAX_StripsForTruncatedMean = 20;          // threshold for maxnumber of strips included in the truncated mean algo.
+    public static final int     TRK_MIN_ClusterSizeForCentroid = 3;     // minimum size of the cluster to provide a reliable location by its centroid
     public static final double  TRK_MIN_CLUS_ENERGY = 20.;              // minimum cluster energy to accept a signal cluster (20)
     public static final int     TRK_MIN_CLUS_SIZE = 0;                  // minimum cluster size to form crosses (0: no limit on cluster size) // 2 is very strict
     public static final int     TRK_MAXNUMBEROFHITS = 100;                // maximum number of accepted hits in the tracker banks
