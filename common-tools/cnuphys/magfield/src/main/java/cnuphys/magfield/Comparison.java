062b4e1b8412a992e640cc84957b701a07c15fce
heddle <David.Heddle@cnu.edu>
2022-03-27 16:00:30 -0400

    About 6% speed increase in tilted mag field from streamlining transformations

--- common-tools/cnuphys/magfield/src/main/java/cnuphys/magfield/Comparison.java
+++ common-tools/cnuphys/magfield/src/main/java/cnuphys/magfield/Comparison.java
@@ -9,14 +9,22 @@ public class Comparison {
 	public float x;
 	public float y;
 	public float z;
+	int sect = 1;
 	
-	public float[] tradResult = {Float.NaN, Float.NaN, Float.NaN};
+	public float[] oldResult = {Float.NaN, Float.NaN, Float.NaN};
 	public float[] newResult = {Float.NaN, Float.NaN, Float.NaN};
 	
+	public float maxCompDiff() {
+		float cmax = 0;
+		for (int i = 0; i < 3; i++) {
+			cmax = Math.max(cmax, Math.abs(oldResult[i] - newResult[i]));
+		}
+		return cmax;
+	}
 	
 	public String toString() {
-		return String.format("(%9.5e, %9.5e, %9.5e), old: (%9.5e, %9.5e, %9.5e), new: (%9.5e, %9.5e, %9.5e)", x, y, z, 
-				tradResult[0], tradResult[1], tradResult[2], 
-				newResult[0], newResult[1], newResult[2]);
+		return String.format("[%d] (%8.4e, %8.4e, %8.4e), old: (%8.4e, %8.4e, %8.4e), new: (%8.4e, %8.4e, %8.4e) [%10.6e]", sect, x, y, z, 
+				oldResult[0], oldResult[1], oldResult[2], 
+				newResult[0], newResult[1], newResult[2], maxCompDiff());
 	}
 }
--- common-tools/cnuphys/magfield/src/main/java/cnuphys/magfield/RotatedCompositeProbe.java
+++ common-tools/cnuphys/magfield/src/main/java/cnuphys/magfield/RotatedCompositeProbe.java
@@ -7,60 +7,163 @@ public class RotatedCompositeProbe extends CompositeProbe {
 	private final double _sin25 = Math.sin(Math.toRadians(_angle));
 	private final double _cos25 = Math.cos(Math.toRadians(_angle));
 	
-	// for the use of maps rotated to the tiled system. These are used to speed up by using
-	// maps rotated to each sector tilted system, rather than rotating on each query
-	private TiltedMapManager _tiltedManager;
+	public static boolean TestMode;
 
 	public RotatedCompositeProbe(RotatedCompositeField field) {
 		super(field);
-		_tiltedManager = TiltedMapManager.getInstance();
 	}
 
+	
+	private static final float m11 = 0.90630779f;
+	private static final float m12 = 0.42261826f;
+	private static final float m21 = 0.45315389f;
+	private static final float m22 = 0.86602540f;
+	private static final float m23 = 0.21130913f;
+	private static final float m31 = 0.78488557f;
+	private static final float m32 = 0.5f;
+	private static final float m33 = 0.36599815f;
+	
+	
 	/**
 	 * Obtain the magnetic field at a given location expressed in Cartesian
 	 * coordinates of the tilted sector system. The field is returned as a Cartesian
 	 * vector in kiloGauss.
 	 * 
 	 * @param sector the sector [1..6]
-	 * @param xs     the tilted x coordinate in cm
-	 * @param ys     the tilted y coordinate in cm
-	 * @param zs     the tilted z coordinate in cm
+	 * @param xtilt     the tilted x coordinate in cm
+	 * @param ytilt     the tilted y coordinate in cm
+	 * @param ztilt     the tilted z coordinate in cm
 	 * @param result the result is a float array holding the retrieved field in
 	 *               kiloGauss. The 0,1 and 2 indices correspond to x, y, and z
 	 *               components.
 	 */
 	@Override
-	public void field(int sector, float xs, float ys, float zs, float[] result) {
+	public void field(int sector, float xtilt, float ytilt, float ztilt, float[] result) {
 
-		// first rotate location to get to the sector coordinate system
-		float x = (float) (xs * _cos25 - zs * _sin25);
-		float y = (ys);
-		float z = (float) (zs * _cos25 + xs * _sin25);
+		float x, y, z;
+		float bxl = 0, byl = 0, bzl = 0;
+		
+		
+		if (TestMode) {
+			z = -m12 * xtilt + m11 * ztilt;
+			
+			switch(sector) {
+			case 1:
+				x = m11 * xtilt + m12 *ztilt;
+				y = ytilt;
+				break;
+				
+			case 2:
+				x = m21 * xtilt - m22 * ytilt + m23 * ztilt;
+				y = m31 * xtilt + m32 * ytilt + m33 * ztilt;
+				break;
+				
+			case 3:
+				x = -m21 * xtilt - m22 * ytilt - m23 * ztilt;
+				y = m31 * xtilt - m32 * ytilt + m33 * ztilt;
+				break;
+				
+			case 4:
+				x = -m11 * xtilt - m12 *ztilt;
+				y = -ytilt;
+				break;
+				
+			case 5:
+				x = -m21 * xtilt + m22 * ytilt - m23 * ztilt;
+				y = -m31 * xtilt - m32 * ytilt - m33 * ztilt;
+				break;
+				
+			case 6:
+				x = m21 * xtilt + m22 * ytilt + m23 * ztilt;
+				y = -m31 * xtilt + m32 * ytilt- m33 * ztilt;
+				break;
+				
+			default:
+				x = Float.NaN;
+				y = Float.NaN;
+				System.err.println("Bad sector in RotatedCompositeProbe, sector = " + sector);
+				break;
+			}
 
-		// now rotate to the correct sector to get the lab coordinates. We can use the
-		// result array!
-		MagneticFields.sectorToLab(sector, result, x, y, z);
-		x = result[0];
-		y = result[1];
-		z = result[2];
+		} else {
+			// first rotate location to get to the sector coordinate system
+			x = (float) (xtilt * _cos25 - ztilt * _sin25);
+			y = (ytilt);
+			z = (float) (ztilt * _cos25 + xtilt * _sin25);
 
-		float bx = 0, by = 0, bz = 0;
-		for (IField probe : probes) {
-			probe.field(x, y, z, result);
-			bx += result[0];
-			by += result[1];
-			bz += result[2];
+			// now rotate to the correct sector to get the lab coordinates. We can use the
+			// result array!
+			MagneticFields.sectorToLab(sector, result, x, y, z);
+			x = result[0];
+			y = result[1];
+			z = result[2];
 		}
 
-		MagneticFields.labToSector(sector, result, bx, by, bz);
-		bx = result[0];
-		by = result[1];
-		bz = result[2];
+		
+		for (IField probe : probes) {
+			probe.field(x, y, z, result);
+			bxl += result[0];
+			byl += result[1];
+			bzl += result[2];
+		}
+		
+		if (TestMode) {
+			
+			switch(sector) {
+			case 1:
+				result[0] = m11 * bxl - m12 * bzl;
+				result[1] = byl;
+				result[2] = m12 * bxl + m11 * bzl;
+				break;
+				
+			case 2:
+				result[0] = m21 * bxl + m31 * byl - m12 * bzl;
+				result[1] = -m22 * bxl + m32 * byl;
+				result[2] = m23 * bxl + m33 * byl + m11 * bzl;			
+				break;
+				
+			case 3:
+				result[0] = -m21 * bxl + m31 * byl - m12 * bzl;
+				result[1] = -m22 * bxl - m32 * byl;
+				result[2] = -m23 * bxl + m33 * byl + m11 * bzl;			
+				break;
+				
+			case 4:
+				result[0] = -m11 * bxl - m12 * bzl;
+				result[1] = -byl;
+				result[2] = -m12 * bxl + m11 * bzl;
+				break;
+				
+			case 5:
+				result[0] = -m21 * bxl - m31 * byl - m12 * bzl;
+				result[1] = m22 * bxl - m32 * byl;
+				result[2] = -m23 * bxl - m33 * byl + m11 * bzl;			
+				break;
+				
+			case 6:
+				result[0] = m21 * bxl - m31 * byl - m12 * bzl;
+				result[1] = m22 * bxl + m32 * byl;
+				result[2] = m23 * bxl - m33 * byl + m11 * bzl;			
+				break;
+				
+			default:
+				result[0] = Float.NaN;
+				result[1] = Float.NaN;
+				result[2] = Float.NaN;
+				System.err.println("Bad sector in RotatedCompositeProbe, sector = " + sector);
+				break;
+			}
+		}
+		else {
 
-		// now rotate the field in the opposite sense
-		result[0] = (float) (bx * _cos25 + bz * _sin25);
-		result[1] = (by);
-		result[2] = (float) (bz * _cos25 - bx * _sin25);
+			MagneticFields.labToSector(sector, result, bxl, byl, bzl);
+			float bx = result[0];
+			float bz = result[2];
+
+			// now rotate the field in the opposite sense
+			result[0] = (float) (bx * _cos25 + bz * _sin25);
+			result[2] = (float) (bz * _cos25 - bx * _sin25);
+		}
 	}
 
 	/**
