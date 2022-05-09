2e67539b9d4858de217e203827575e79efd5aca2
Bruno Benkel <bruno.benkel@gmail.com>
2022-05-09 16:00:48 -0400

    Switched arrays to a new class for clarity and speed

--- reconstruction/dc/src/main/java/org/jlab/rec/dc/track/fit/RungeKuttaDoca.java
+++ reconstruction/dc/src/main/java/org/jlab/rec/dc/track/fit/RungeKuttaDoca.java
@@ -16,23 +16,23 @@ public class RungeKuttaDoca {
     public RungeKuttaDoca() {}
 
     /** Swim to Z position without updating the covariance matrix. */
-    public void SwimToZ(int sector, StateVecsDoca.StateVec fVec, Swim swim, double z0, float[] bf) {
+    public void SwimToZ(int sector, StateVecsDoca.StateVec vec, Swim swim, double z0, float[] bf) {
         double stepSize = 1.0;
-        swim.Bfield(sector, fVec.x, fVec.y, fVec.z, bf);
+        swim.Bfield(sector, vec.x, vec.y, vec.z, bf);
 
-        fVec.B = Math.sqrt(bf[0]*bf[0] + bf[1]*bf[1] + bf[2]*bf[2]);
-        double s = fVec.B;
-        final double travelSign = Math.signum(z0 - fVec.z);
+        vec.B = Math.sqrt(bf[0]*bf[0] + bf[1]*bf[1] + bf[2]*bf[2]);
+        double s = vec.B;
+        final double travelSign = Math.signum(z0 - vec.z);
         double BatMeas = 0;
 
-        while(travelSign * fVec.z < travelSign * z0) {
+        while(travelSign * vec.z < travelSign * z0) {
             s = travelSign * stepSize;
-            if (travelSign*(fVec.z+s) > travelSign*z0) s = travelSign*Math.abs(z0-fVec.z);
+            if (travelSign*(vec.z+s) > travelSign*z0) s = travelSign*Math.abs(z0-vec.z);
 
-            this.RK4transport(sector, s, swim, fVec);
+            this.RK4transport(sector, s, swim, vec);
 
-            if (Math.abs(fVec.B - BatMeas) < 0.0001) stepSize *= 2;
-            BatMeas = fVec.B;
+            if (Math.abs(vec.B - BatMeas) < 0.0001) stepSize *= 2;
+            BatMeas = vec.B;
         }
     }
 
@@ -40,27 +40,27 @@ public class RungeKuttaDoca {
      * Transport using Runge Kutta 4 without updating the covariance matrix. Lab system = 1, TSC = 0.
      * Internal state array (sN) is defined as {x, y, tx, ty}, since q doesn't change.
      */
-    private void RK4transport(int sector, double h, Swim swim, StateVecsDoca.StateVec fVec) {
+    private void RK4transport(int sector, double h, Swim swim, StateVecsDoca.StateVec vec) {
         // Set initial state.
-        double qv = fVec.Q*v;
-        double[] s0 = {fVec.x, fVec.y, fVec.tx, fVec.ty};
+        double qv = vec.Q*v;
+        double[] s0 = {vec.x, vec.y, vec.tx, vec.ty};
         double[] sNull = {0, 0, 0, 0};
 
         // Transport.
-        double[] s1 = RK4step(sector, fVec.z, 0,     swim, s0, sNull, qv);
-        double[] s2 = RK4step(sector, fVec.z, 0.5*h, swim, s0, s1,    qv);
-        double[] s3 = RK4step(sector, fVec.z, 0.5*h, swim, s0, s2,    qv);
-        double[] s4 = RK4step(sector, fVec.z, h,     swim, s0, s3,    qv);
+        double[] s1 = RK4step(sector, vec.z, 0,     swim, s0, sNull, qv);
+        double[] s2 = RK4step(sector, vec.z, 0.5*h, swim, s0, s1,    qv);
+        double[] s3 = RK4step(sector, vec.z, 0.5*h, swim, s0, s2,    qv);
+        double[] s4 = RK4step(sector, vec.z, h,     swim, s0, s3,    qv);
 
         // Set final state.
-        fVec.z  += h;
-        fVec.x  += this.RK4(s1[0], s2[0], s3[0], s4[0], h);
-        fVec.y  += this.RK4(s1[1], s2[1], s3[1], s4[1], h);
-        fVec.tx += this.RK4(s1[2], s2[2], s3[2], s4[2], h);
-        fVec.ty += this.RK4(s1[3], s2[3], s3[3], s4[3], h);
+        vec.z  += h;
+        vec.x  += this.RK4(s1[0], s2[0], s3[0], s4[0], h);
+        vec.y  += this.RK4(s1[1], s2[1], s3[1], s4[1], h);
+        vec.tx += this.RK4(s1[2], s2[2], s3[2], s4[2], h);
+        vec.ty += this.RK4(s1[3], s2[3], s3[3], s4[3], h);
 
-        fVec.B = Math.sqrt(_b[0]*_b[0] + _b[1]*_b[1] + _b[2]*_b[2]);
-        fVec.deltaPath += Math.sqrt((s0[0]-fVec.x)*(s0[0]-fVec.x)+(s0[1]-fVec.y)*(s0[1]-fVec.y)+h*h);
+        vec.B = Math.sqrt(_b[0]*_b[0] + _b[1]*_b[1] + _b[2]*_b[2]);
+        vec.deltaPath += Math.sqrt((s0[0]-vec.x)*(s0[0]-vec.x)+(s0[1]-vec.y)*(s0[1]-vec.y)+h*h);
     }
 
     /** Perform a single RK4 step without updating the covariance matrix. */
@@ -76,132 +76,122 @@ public class RungeKuttaDoca {
         return sNext;
     }
 
-    /**
-     * Transport using Runge Kutta 4, updating the covariance matrix.
-     * Internal state matrix (sN) is defined as:
-     *     sN = {{00: x,  01: dx/dtx0,  02: dx/dty0,  03: dx/dq0},
-     *           {10: y,  11: dy/dtx0,  12: dy/dty0,  13: dy/dq0},
-     *           {20: tx, 21: dtx/dtx0, 22: dtx/dty0, 23: dtx/dq0},
-     *           {30: ty, 31: dty/dtx0, 32: dty/dty0, 33: dty/dq0}}
-     */
+    /** Transport using Runge Kutta 4, updating the covariance matrix. */
     void RK4transport(int sector, double h, Swim swim, StateVecsDoca.CovMat covMat,
-                      StateVecsDoca.StateVec fVec, StateVecsDoca.CovMat fCov) {
+                      StateVecsDoca.StateVec vec, StateVecsDoca.CovMat fCov) {
         // Set initial state and Jacobian.
-        double qv = fVec.Q*v;
-        double[][] s0 = {{fVec.x,0,0,0}, {fVec.y,0,0,0}, {fVec.tx,1,0,0}, {fVec.ty,0,1,0}};
-        double[][] sNull = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
+        double qv = vec.Q*v;
+        RK4Vec s0 = new RK4Vec(vec);
+        RK4Vec sNull = new RK4Vec();
 
         // Perform steps.
-        double[][] s1 = RK4step(sector, fVec.z, 0,     swim, s0, sNull, qv);
-        double[][] s2 = RK4step(sector, fVec.z, 0.5*h, swim, s0, s1,    qv);
-        double[][] s3 = RK4step(sector, fVec.z, 0.5*h, swim, s0, s2,    qv);
-        double[][] s4 = RK4step(sector, fVec.z, h,     swim, s0, s3,    qv);
+        RK4Vec s1 = RK4step(sector, vec.z, 0,     swim, s0, sNull, qv);
+        RK4Vec s2 = RK4step(sector, vec.z, 0.5*h, swim, s0, s1,    qv);
+        RK4Vec s3 = RK4step(sector, vec.z, 0.5*h, swim, s0, s2,    qv);
+        RK4Vec s4 = RK4step(sector, vec.z, h,     swim, s0, s3,    qv);
 
         // Compute and set final state and covariance matrix.
-        double[][] sFinal = computeFinalState(h, s0, s1, s2, s3, s4);
+        RK4Vec sF = computeFinalState(h, s0, s1, s2, s3, s4);
 
-        fVec.x   = sFinal[0][0];
-        fVec.y   = sFinal[1][0];
-        fVec.tx  = sFinal[2][0];
-        fVec.ty  = sFinal[3][0];
-        fVec.z  += h;
-        fVec.B   = Math.sqrt(_b[0]*_b[0]+_b[1]*_b[1]+_b[2]*_b[2]);
-        fVec.deltaPath += Math.sqrt((s0[0][0]-fVec.x)*(s0[0][0]-fVec.x)
-                                    + (s0[1][0]-fVec.y)*(s0[1][0]-fVec.y) + h*h);
-        fCov.covMat.set(computeCovMat(covMat.covMat, sFinal));
+        vec.x   = sF.x;
+        vec.y   = sF.y;
+        vec.tx  = sF.tx;
+        vec.ty  = sF.ty;
+        vec.z  += h;
+        vec.B   = Math.sqrt(_b[0]*_b[0]+_b[1]*_b[1]+_b[2]*_b[2]);
+        vec.deltaPath += Math.sqrt((s0.x-vec.x)*(s0.x-vec.x) + (s0.y-vec.y)*(s0.y-vec.y) + h*h);
+        fCov.covMat.set(computeCovMat(covMat.covMat, sF));
     }
 
     /** Perform one RK4 step, updating the covariance matrix. */
-    private double[][] RK4step(int sector, double z0, double h, Swim swim,
-                               double[][] sInit, double[][] sPrev, double qv) {
-        double[][] sNext = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
-        swim.Bfield(sector, sInit[0][0] + h*sPrev[0][0], sInit[1][0] + h*sPrev[1][0], z0 + h, _b);
+    private RK4Vec RK4step(int sector, double z0, double h, Swim swim,
+                               RK4Vec sInit, RK4Vec sPrev, double qv) {
+        RK4Vec sNext = new RK4Vec();
+        swim.Bfield(sector, sInit.x + h*sPrev.x, sInit.y + h*sPrev.y, z0 + h, _b);
 
         // State.
-        sNext[0][0] = sInit[2][0] + h*sPrev[2][0];
-        sNext[1][0] = sInit[3][0] + h*sPrev[3][0];
+        sNext.x = sInit.tx + h*sPrev.tx;
+        sNext.y = sInit.ty + h*sPrev.ty;
 
-        double Csq  = Csq(sNext[0][0], sNext[1][0]);
+        double Csq  = Csq(sNext.x, sNext.y);
         double C    = C(Csq);
-        double Ax   = Ax(C, sNext[0][0], sNext[1][0]);
-        double Ay   = Ay(C, sNext[0][0], sNext[1][0]);
+        double Ax   = Ax(C, sNext.x, sNext.y);
+        double Ay   = Ay(C, sNext.x, sNext.y);
 
-        sNext[2][0] = qv * Ax;
-        sNext[3][0] = qv * Ay;
+        sNext.tx = qv * Ax;
+        sNext.ty = qv * Ay;
 
         // Jacobian.
-        sNext[0][1] = sInit[2][1] + h*sPrev[2][1];
-        sNext[0][2] = sInit[2][2] + h*sPrev[2][2];
-        sNext[0][3] = sInit[2][3] + h*sPrev[2][3];
-        sNext[1][1] = sInit[3][1] + h*sPrev[3][1];
-        sNext[1][2] = sInit[3][2] + h*sPrev[3][2];
-        sNext[1][3] = sInit[3][3] + h*sPrev[3][3];
+        sNext.dxdtx0 = sInit.dtxdtx0 + h*sPrev.dtxdtx0;
+        sNext.dxdty0 = sInit.dtxdty0 + h*sPrev.dtxdty0;
+        sNext.dxdq0  = sInit.dtxdq0  + h*sPrev.dtxdq0;
+        sNext.dydtx0 = sInit.dtydtx0 + h*sPrev.dtydtx0;
+        sNext.dydty0 = sInit.dtydty0 + h*sPrev.dtydty0;
+        sNext.dydq0  = sInit.dtydq0  + h*sPrev.dtydq0;
 
-        double dAx_dtx = dAx_dtx(C, Csq, Ax, sNext[0][0], sNext[1][0]);
-        double dAx_dty = dAx_dty(C, Csq, Ax, sNext[0][0], sNext[1][0]);
-        double dAy_dtx = dAy_dtx(C, Csq, Ay, sNext[0][0], sNext[1][0]);
-        double dAy_dty = dAy_dty(C, Csq, Ay, sNext[0][0], sNext[1][0]);
+        double dAx_dtx = dAx_dtx(C, Csq, Ax, sNext.x, sNext.y);
+        double dAx_dty = dAx_dty(C, Csq, Ax, sNext.x, sNext.y);
+        double dAy_dtx = dAy_dtx(C, Csq, Ay, sNext.x, sNext.y);
+        double dAy_dty = dAy_dty(C, Csq, Ay, sNext.x, sNext.y);
 
-        sNext[2][1] = this.dtx_dtx0(qv, dAx_dtx, dAx_dty, sNext[0][1], sNext[1][1]);
-        sNext[2][2] = this.dtx_dty0(qv, dAx_dty, sNext[0][2], sNext[1][2]);
-        sNext[2][3] = this.dtx_dq0( qv, Ax, dAx_dtx, dAx_dty, sNext[0][3], sNext[1][3]);
-        sNext[3][1] = this.dty_dtx0(qv, dAy_dtx, dAy_dty, sNext[0][1], sNext[1][1]);
-        sNext[3][2] = this.dty_dty0(qv, dAy_dty, sNext[0][2], sNext[1][2]);
-        sNext[3][3] = this.dty_dq0( qv, Ay, dAy_dtx, dAy_dty, sNext[0][3], sNext[1][3]);
+        sNext.dtxdtx0 = this.dtx_dtx0(qv,     dAx_dtx, dAx_dty, sNext.dxdtx0, sNext.dydtx0);
+        sNext.dtxdty0 = this.dtx_dty0(qv,              dAx_dty, sNext.dxdty0, sNext.dydty0);
+        sNext.dtxdq0  = this.dtx_dq0( qv, Ax, dAx_dtx, dAx_dty, sNext.dxdq0,  sNext.dydq0);
+        sNext.dtydtx0 = this.dty_dtx0(qv,     dAy_dtx, dAy_dty, sNext.dxdtx0, sNext.dydtx0);
+        sNext.dtydty0 = this.dty_dty0(qv,              dAy_dty, sNext.dxdty0, sNext.dydty0);
+        sNext.dtydq0  = this.dty_dq0( qv, Ay, dAy_dtx, dAy_dty, sNext.dxdq0,  sNext.dydq0);
 
         return sNext;
     }
 
     /** Compute the final state for each entry in the internal state matrix. */
-    private double[][] computeFinalState(double h, double[][] s0, double[][] s1, double[][] s2,
-                                         double[][] s3, double[][] s4) {
-        double[][] sFinal = {{0,0,0,0}, {0,0,0,0}, {0,0,0,0}, {0,0,0,0}};
-        for (int i = 0; i < 4; ++i) {
-            for (int j = 0; j < 4; ++j) {
-                if (j == 0) sFinal[i][j] += s0[i][j];
-                if (i == 2 && j == 1) sFinal[i][j] += 1;
-                if (i == 3 && j == 2) sFinal[i][j] += 1;
-                sFinal[i][j] += this.RK4(s1[i][j], s2[i][j], s3[i][j], s4[i][j], h);
-            }
-        }
-        return sFinal;
+    private RK4Vec computeFinalState(double h, RK4Vec s0, RK4Vec s1, RK4Vec s2, RK4Vec s3, RK4Vec s4) {
+        RK4Vec sF = new RK4Vec();
+
+        sF.x      = s0.x  + this.RK4(s1.x,      s2.x,      s3.x,      s4.x,      h);
+        sF.dxdtx0 =         this.RK4(s1.dxdtx0, s2.dxdtx0, s3.dxdtx0, s4.dxdtx0, h);
+        sF.dxdty0 =         this.RK4(s1.dxdty0, s2.dxdty0, s3.dxdty0, s4.dxdty0, h);
+        sF.dxdq0  =         this.RK4(s1.dxdq0,  s2.dxdq0,  s3.dxdq0,  s4.dxdq0,  h);
+
+        sF.y      = s0.y  + this.RK4(s1.y,      s2.y,      s3.y,      s4.y,      h);
+        sF.dydtx0 =         this.RK4(s1.dydtx0, s2.dydtx0, s3.dydtx0, s4.dydtx0, h);
+        sF.dydty0 =         this.RK4(s1.dydty0, s2.dydty0, s3.dydty0, s4.dydty0, h);
+        sF.dydq0  =         this.RK4(s1.dydq0,  s2.dydq0,  s3.dydq0,  s4.dydq0,  h);
+
+        sF.tx      = s0.tx + this.RK4(s1.tx,      s2.tx,      s3.tx,      s4.tx,      h);
+        sF.dtxdtx0 = 1     + this.RK4(s1.dtxdtx0, s2.dtxdtx0, s3.dtxdtx0, s4.dtxdtx0, h);
+        sF.dtxdty0 =         this.RK4(s1.dtxdty0, s2.dtxdty0, s3.dtxdty0, s4.dtxdty0, h);
+        sF.dtxdq0  =         this.RK4(s1.dtxdq0,  s2.dtxdq0,  s3.dtxdq0,  s4.dtxdq0,  h);
+
+        sF.ty      = s0.ty + this.RK4(s1.ty,      s2.ty,      s3.ty,      s4.ty,      h);
+        sF.dtydtx0 =         this.RK4(s1.dtydtx0, s2.dtydtx0, s3.dtydtx0, s4.dtydtx0, h);
+        sF.dtydty0 = 1     + this.RK4(s1.dtydty0, s2.dtydty0, s3.dtydty0, s4.dtydty0, h);
+        sF.dtydq0  =         this.RK4(s1.dtydq0,  s2.dtydq0,  s3.dtydq0,  s4.dtydq0,  h);
+
+        return sF;
     }
 
     /** Compute the final covariance matrix. covMat = FCF^T; u = FC. */
-    private double[][] computeCovMat(Matrix cPrev, double[][] sFinal) {
-        double dxdtx0 = sFinal[0][1];
-        double dxdty0 = sFinal[0][2];
-        double dxdq0  = sFinal[0][3];
-
-        double dydtx0 = sFinal[1][1];
-        double dydty0 = sFinal[1][2];
-        double dydq0  = sFinal[1][3];
-
-        double dtxdtx0 = sFinal[2][1];
-        double dtxdty0 = sFinal[2][2];
-        double dtxdq0  = sFinal[2][3];
-
-        double dtydtx0 = sFinal[3][1];
-        double dtydty0 = sFinal[3][2];
-        double dtydq0  = sFinal[3][3];
-
+    private double[][] computeCovMat(Matrix cPrev, RK4Vec sF) {
         double[][] u = new double[5][5];
         for (int j = 0; j < 5; j++) {
-            u[0][j] = cPrev.get(0,j) + cPrev.get(2,j) * dxdtx0 + cPrev.get(3,j) * dxdty0
-                    + cPrev.get(4,j) * dxdq0;
-            u[1][j] = cPrev.get(1,j) + cPrev.get(2,j) * dydtx0 + cPrev.get(3,j) * dydty0
-                    + cPrev.get(4,j) * dydq0;
-            u[2][j] = cPrev.get(2,j) * dtxdtx0 + cPrev.get(3,j) * dtxdty0 + cPrev.get(4,j) * dtxdq0;
-            u[3][j] = cPrev.get(2,j) * dtydtx0 + cPrev.get(3,j) * dtydty0 + cPrev.get(4,j) * dtydq0;
+            u[0][j] = cPrev.get(0,j) + cPrev.get(2,j) * sF.dxdtx0 + cPrev.get(3,j) * sF.dxdty0
+                    + cPrev.get(4,j) * sF.dxdq0;
+            u[1][j] = cPrev.get(1,j) + cPrev.get(2,j) * sF.dydtx0 + cPrev.get(3,j) * sF.dydty0
+                    + cPrev.get(4,j) * sF.dydq0;
+            u[2][j] = cPrev.get(2,j) * sF.dtxdtx0 + cPrev.get(3,j) * sF.dtxdty0
+                    + cPrev.get(4,j) * sF.dtxdq0;
+            u[3][j] = cPrev.get(2,j) * sF.dtydtx0 + cPrev.get(3,j) * sF.dtydty0
+                    + cPrev.get(4,j) * sF.dtydq0;
             u[4][j] = cPrev.get(4,j);
         }
 
         double[][] cNext = new double[5][5];
         for (int i = 0; i < 5; i++) {
-            cNext[i][0] = u[i][0] + u[i][2] * dxdtx0 + u[i][3] * dxdty0 + u[i][4] * dxdq0;
-            cNext[i][1] = u[i][1] + u[i][2] * dydtx0 + u[i][3] * dydty0 + u[i][4] * dydq0;
-            cNext[i][2] = u[i][2] * dtxdtx0 + u[i][3] * dtxdty0 + u[i][4] * dtxdq0;
-            cNext[i][3] = u[i][2] * dtydtx0 + u[i][3] * dtydty0 + u[i][4] * dtydq0;
+            cNext[i][0] = u[i][0] + u[i][2] * sF.dxdtx0 + u[i][3] * sF.dxdty0 + u[i][4] * sF.dxdq0;
+            cNext[i][1] = u[i][1] + u[i][2] * sF.dydtx0 + u[i][3] * sF.dydty0 + u[i][4] * sF.dydq0;
+            cNext[i][2] = u[i][2] * sF.dtxdtx0 + u[i][3] * sF.dtxdty0 + u[i][4] * sF.dtxdq0;
+            cNext[i][3] = u[i][2] * sF.dtydtx0 + u[i][3] * sF.dtydty0 + u[i][4] * sF.dtydq0;
             cNext[i][4] = u[i][4];
         }
 
@@ -266,5 +256,39 @@ public class RungeKuttaDoca {
     private double dty_dq0(double qv, double Ay, double dAy_dtx, double dAy_dty,
                            double dtx_dq0, double dty_dq0) {
         return v*Ay + qv * (dAy_dtx*dtx_dq0 + dAy_dty*dty_dq0);
+    }
+
+    /** State vector and its derivatives used internally. */
+    private class RK4Vec {
+        public double x      = 0;
+        public double dxdtx0 = 0;
+        public double dxdty0 = 0;
+        public double dxdq0  = 0;
+
+        public double y      = 0;
+        public double dydtx0 = 0;
+        public double dydty0 = 0;
+        public double dydq0  = 0;
+
+        public double tx      = 0;
+        public double dtxdtx0 = 0;
+        public double dtxdty0 = 0;
+        public double dtxdq0  = 0;
+
+        public double ty      = 0;
+        public double dtydtx0 = 0;
+        public double dtydty0 = 0;
+        public double dtydq0  = 0;
+
+        RK4Vec() {}
+        RK4Vec(StateVecsDoca.StateVec vec) {
+            this.x  = vec.x;
+            this.y  = vec.y;
+            this.tx = vec.tx;
+            this.ty = vec.ty;
+
+            this.dtxdtx0 = 1;
+            this.dtydty0 = 1;
+        }
     }
 }
