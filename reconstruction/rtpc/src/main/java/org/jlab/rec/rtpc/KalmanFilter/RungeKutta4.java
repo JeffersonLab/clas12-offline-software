f0ae5e0671ee3b634aaf4919406a79fe5081b955
Mathieu Ouillon <mathieu.ouillon@ijclab.in2p3.fr>
2022-06-29 17:48:30 +0200

    Kalman Filter for BONuS12

---
+++ reconstruction/rtpc/src/main/java/org/jlab/rec/rtpc/KalmanFilter/RungeKutta4.java
@@ -0,0 +1,143 @@
+package org.jlab.rec.rtpc.KalmanFilter;
+
+import org.jlab.rec.rtpc.KalmanFilter.EnergyLoss.*;
+
+public class RungeKutta4 {
+  private final int numberOfVariables;
+  private final Particle particle;
+  private final BetheBlochModel model;
+  private final double[] B;
+
+  private Stepper stepper;
+
+  private final double[] k1;
+  private final double[] k2;
+  private final double[] k3;
+  private final double[] k4;
+
+  private final double[] yInTemp;
+  private final double[] yTemp;
+
+  public RungeKutta4(
+      Particle particle, BetheBlochModel betheBlochModel, int numberOfVariables, double[] B) {
+
+    this.numberOfVariables = numberOfVariables;
+    this.particle = particle;
+    this.model = betheBlochModel;
+    this.B = B;
+
+    this.k1 = new double[numberOfVariables];
+    this.k2 = new double[numberOfVariables];
+    this.k3 = new double[numberOfVariables];
+    this.k4 = new double[numberOfVariables];
+
+    this.yInTemp = new double[numberOfVariables];
+    this.yTemp = new double[numberOfVariables];
+  }
+
+  public void doOneStep(Stepper stepper) {
+
+    this.stepper = stepper;
+
+    double[] yIn = stepper.y;
+    double h = stepper.h;
+    Material material = stepper.material;
+
+    stepper.s += h;
+    if (stepper.is_in_drift) stepper.s_drift += h;
+
+    if (stepper.direction) stepper.sTot += h;
+    else stepper.sTot -= h;
+
+    System.arraycopy(yIn, 0, yInTemp, 0, numberOfVariables);
+
+    double[] dydt = f(yInTemp);
+    for (int i = 0; i < numberOfVariables; ++i) {
+      k1[i] = h * dydt[i];
+    }
+
+    for (int i = 0; i < numberOfVariables; ++i) {
+      yTemp[i] = yInTemp[i] + 0.5 * k1[i];
+    }
+    dydt = f(yTemp);
+    for (int i = 0; i < numberOfVariables; ++i) {
+      k2[i] = h * dydt[i];
+    }
+
+    for (int i = 0; i < numberOfVariables; ++i) {
+      yTemp[i] = yInTemp[i] + 0.5 * k2[i];
+    }
+    dydt = f(yTemp);
+    for (int i = 0; i < numberOfVariables; ++i) {
+      k3[i] = h * dydt[i];
+    }
+
+    for (int i = 0; i < numberOfVariables; ++i) {
+      yTemp[i] = yInTemp[i] + k3[i];
+    }
+    dydt = f(yTemp);
+    for (int i = 0; i < numberOfVariables; ++i) {
+      k4[i] = h * dydt[i];
+    }
+
+    for (int i = 0; i < numberOfVariables; ++i) {
+      yIn[i] =
+          yInTemp[i]
+              + 1.0 / 6.0 * k1[i]
+              + 1.0 / 3.0 * k2[i]
+              + 1.0 / 3.0 * k3[i]
+              + 1.0 / 6.0 * k4[i];
+    }
+
+    energyLoss(yIn, h, material);
+  }
+
+  private double[] f(double[] y) {
+    double charge = 1.0;
+    double pModuleInverse = 1.0 / Math.sqrt(y[3] * y[3] + y[4] * y[4] + y[5] * y[5]);
+    double k = SystemOfUnits.eplus * charge * PhysicalConstants.c_light * pModuleInverse;
+
+    if (this.stepper.direction) {
+      return new double[] {
+        y[3] * pModuleInverse,
+        y[4] * pModuleInverse,
+        y[5] * pModuleInverse,
+        k * (y[4] * B[2] - y[5] * B[1]),
+        k * (y[5] * B[0] - y[3] * B[2]),
+        k * (y[3] * B[1] - y[4] * B[0])
+      };
+    } else {
+      return new double[] {
+        -y[3] * pModuleInverse,
+        -y[4] * pModuleInverse,
+        -y[5] * pModuleInverse,
+        -k * (y[4] * B[2] - y[5] * B[1]),
+        -k * (y[5] * B[0] - y[3] * B[2]),
+        -k * (y[3] * B[1] - y[4] * B[0])
+      };
+    }
+  }
+
+  private void energyLoss(double[] yIn, double h, Material material) {
+    double mass = particle.GetMass();
+
+    h /= 10; // cm
+    double mom = Math.sqrt(yIn[3] * yIn[3] + yIn[4] * yIn[4] + yIn[5] * yIn[5]);
+    double kineticEnergy = Math.sqrt(mass * mass + mom * mom) - mass;
+    double E = Math.sqrt(mom * mom + mass * mass);
+
+    double dedx =
+        model.ComputeDEDXPerVolume(particle, material, kineticEnergy, false) * 1000; // MeV/cm
+    double DeltaE = dedx * h;
+
+    stepper.dEdx += DeltaE;
+
+    double mom_prim;
+    if (this.stepper.direction) mom_prim = Math.sqrt((E - DeltaE) * (E - DeltaE) - mass * mass);
+    else mom_prim = Math.sqrt((E + DeltaE) * (E + DeltaE) - mass * mass);
+
+    yIn[3] *= mom_prim / mom;
+    yIn[4] *= mom_prim / mom;
+    yIn[5] *= mom_prim / mom;
+  }
+}
--- reconstruction/rtpc/src/main/java/org/jlab/service/rtpc/RTPCEngine.java
+++ reconstruction/rtpc/src/main/java/org/jlab/service/rtpc/RTPCEngine.java
@@ -1,204 +1,202 @@
 package org.jlab.service.rtpc;
 
-import java.io.File;
-
-
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import org.jlab.clas.reco.EngineProcessor;
-
 import org.jlab.clas.reco.ReconstructionEngine;
 import org.jlab.io.base.DataBank;
 import org.jlab.io.base.DataEvent;
+import org.jlab.rec.rtpc.KalmanFilter.KalmanFitter;
+import org.jlab.rec.rtpc.KalmanFilter.KalmanFitterInfo;
 import org.jlab.rec.rtpc.banks.HitReader;
 import org.jlab.rec.rtpc.banks.RecoBankWriter;
-import org.jlab.rec.rtpc.hit.Hit;
-import org.jlab.rec.rtpc.hit.HitParameters;
-import org.jlab.rec.rtpc.hit.SignalSimulation;
-import org.jlab.rec.rtpc.hit.TimeAverage;
-import org.jlab.rec.rtpc.hit.TrackDisentangler;
-import org.jlab.rec.rtpc.hit.TrackFinder;
-import org.jlab.rec.rtpc.hit.TrackHitReco;
-import org.jlab.rec.rtpc.hit.HelixFitTest;
-import org.jlab.detector.calib.utils.ConstantsManager;
+import org.jlab.rec.rtpc.hit.*;
 import org.jlab.utils.groups.IndexedTable;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 
-
-public class RTPCEngine extends ReconstructionEngine{
-
-
-    public RTPCEngine() {
-        super("RTPC","davidp","3.0");
-    }
-
-    private boolean simulation = false;
-    private boolean cosmic = false;
-    private int fitToBeamline = 1;
-    private boolean disentangle = true;
-    private boolean chi2culling = true; 
-
-    @Override
-    public boolean init() {
-        String sim = this.getEngineConfigString("rtpcSimulation");
-        String cosm = this.getEngineConfigString("rtpcCosmic");
-        String beamfit = this.getEngineConfigString("rtpcBeamlineFit");
-        String disentangler = this.getEngineConfigString("rtpcDisentangler");
-	String chi2cull = this.getEngineConfigString("rtpcChi2Cull");
-        //System.out.println(sim + " " + cosm + " " + beamfit);
-
-        if(sim != null){
-            simulation = Boolean.valueOf(sim);
-        }
-
-        if(cosm != null){
-            cosmic = Boolean.valueOf(cosm);
-        }
-
-        if(beamfit != null){
-           fitToBeamline = Boolean.valueOf(beamfit)?1:0;
-        }
-
-        if(disentangler != null){
-            disentangle = Boolean.valueOf(disentangler);
-        }
-
-        if(chi2cull != null){
-           chi2culling = Boolean.valueOf(chi2cull);
-        }
-
-        String[] rtpcTables = new String[]{
-            "/calibration/rtpc/time_offsets",
-            "/calibration/rtpc/gain_balance",
-            "/calibration/rtpc/time_parms",
-            "/calibration/rtpc/recon_parms",
-            "/calibration/rtpc/global_parms",
-            "/geometry/rtpc/alignment"
-        };
-
-        requireConstants(Arrays.asList(rtpcTables));
-
-        return true;
-    }
+public class RTPCEngine extends ReconstructionEngine {
 
 
+	public RTPCEngine() {
+		super("RTPC", "davidp", "3.0");
+	}
 
-    @Override
-    public boolean processDataEvent(DataEvent event) {
+	private boolean simulation = false;
+	private boolean cosmic = false;
+	private int fitToBeamline = 1;
+	private boolean disentangle = true;
+	private boolean chi2culling = true;
+	private boolean kfStatus = true;
 
-        HitParameters params = new HitParameters();
+	@Override
+	public boolean init() {
+		String sim = this.getEngineConfigString("rtpcSimulation");
+		String cosm = this.getEngineConfigString("rtpcCosmic");
+		String beamfit = this.getEngineConfigString("rtpcBeamlineFit");
+		String disentangler = this.getEngineConfigString("rtpcDisentangler");
+		String chi2cull = this.getEngineConfigString("rtpcChi2Cull");
+		String kfstatus = this.getEngineConfigString("rtpcKF");
 
-        HitReader hitRead = new HitReader();
-        hitRead.fetch_RTPCHits(event,simulation,cosmic);//boolean is for simulation
+		if (sim != null) {
+			simulation = Boolean.valueOf(sim);
+		}
 
-        List<Hit> hits = new ArrayList<>();
+		if (cosm != null) {
+			cosmic = Boolean.valueOf(cosm);
+		}
 
-        hits = hitRead.get_RTPCHits();
+		if (beamfit != null) {
+			fitToBeamline = Boolean.valueOf(beamfit) ? 1 : 0;
+		}
 
-        if(hits==null || hits.size()==0){
-            return true;
-        }
+		if (disentangler != null) {
+			disentangle = Boolean.valueOf(disentangler);
+		}
 
-        int runNo = 10;
-        int eventNo = 777;
-        double magfield = 50.0;
-        double magfieldfactor = 1;
+		if (chi2cull != null) {
+			chi2culling = Boolean.valueOf(chi2cull);
+		}
+		if (kfstatus != null) {
+			kfStatus = Boolean.parseBoolean(kfstatus);
+		}
 
-        if(event.hasBank("RUN::config")==true){
-            DataBank bank = event.getBank("RUN::config");
-            runNo = bank.getInt("run", 0);
-            eventNo = bank.getInt("event",0);
-            magfieldfactor = bank.getFloat("solenoid",0);
-            if (runNo<=0) {
-                System.err.println("RTPCEngine:  got run <= 0 in RUN::config, skipping event.");
-                return false;
-            }
-        }
-	
-        magfield = 50 * magfieldfactor;
-        IndexedTable global_parms = this.getConstantsManager().getConstants(runNo, "/calibration/rtpc/global_parms");
-        int hitsbound;
-        hitsbound = (int) global_parms.getDoubleValue("MaxHitsEvent",0,0,0);
-        if(hits.size() > hitsbound) return true;
+		String[] rtpcTables = new String[]{
+				"/calibration/rtpc/time_offsets",
+				"/calibration/rtpc/gain_balance",
+				"/calibration/rtpc/time_parms",
+				"/calibration/rtpc/recon_parms",
+				"/calibration/rtpc/global_parms",
+				"/geometry/rtpc/alignment"
+		};
 
-       // reading the central detectors z shift with respect to the FD 
-       IndexedTable rtpc_alignment = this.getConstantsManager().getConstants(runNo, "/geometry/rtpc/alignment");
-       float rtpc_vz_shift;
-       rtpc_vz_shift = (float) rtpc_alignment.getDoubleValue("deltaZ",0,0,0);
+		requireConstants(Arrays.asList(rtpcTables));
 
-        if(event.hasBank("RTPC::adc")){
-            params.init(this.getConstantsManager(), runNo);
-
-            SignalSimulation SS = new SignalSimulation(hits,params,simulation); //boolean is for simulation
-
-            //Sort Hits into Tracks at the Readout Pads
-            TrackFinder TF = new TrackFinder(params,cosmic);
-            //Calculate Average Time of Hit Signals
-            TimeAverage TA = new TimeAverage(this.getConstantsManager(),params,runNo);
-            //Disentangle Crossed Tracks
-            TrackDisentangler TD = new TrackDisentangler(params,disentangle,eventNo);
-            //Reconstruct Hits in Drift Region
-            TrackHitReco TR = new TrackHitReco(params,hits,cosmic,magfield);
-            //Helix Fit Tracks to calculate Track Parameters
-            HelixFitTest HF = new HelixFitTest(params,fitToBeamline,Math.abs(magfield),cosmic,chi2culling);
-
-            RecoBankWriter writer = new RecoBankWriter();
-            DataBank recoBank = writer.fillRTPCHitsBank(event,params);
-            DataBank trackBank = writer.fillRTPCTrackBank(event,params,rtpc_vz_shift);
-
-            event.appendBank(recoBank);
-            event.appendBank(trackBank);
+		return true;
+	}
 
 
-        }
-        else{
-            return true;
-        }
-        return true;
-    }
+	@Override
+	public boolean processDataEvent(DataEvent event) {
 
-    public static void main(String[] args){
+		HitParameters params = new HitParameters();
 
-        System.setProperty("CLAS12DIR", "/Users/davidpayette/Desktop/newrtpcbranch/clas12-offline-software");
-        double starttime = System.nanoTime();
+		HitReader hitRead = new HitReader();
+		hitRead.fetch_RTPCHits(event, simulation, cosmic);//boolean is for simulation
 
-        File f = new File("/Users/davidpayette/Desktop/SignalStudies/sig.txt");
-        f.delete();
-        f = new File("/Users/davidpayette/Desktop/SignalStudies/trackenergy.txt");
-        f.delete();
-        f = new File("/Users/davidpayette/Desktop/SignalStudies/timespectra.txt");
-        f.delete();
-        f = new File("/Users/davidpayette/Desktop/SignalStudies/sigafter.txt");
-        f.delete();
-        f = new File("/Users/davidpayette/Desktop/SignalStudies/sigTF.txt");
-        f.delete();
-        f = new File("/Users/davidpayette/Desktop/SignalStudies/timeenergy.txt");
-        f.delete();
-        f = new File("/Users/davidpayette/Desktop/SignalStudies/signalbins.txt");
-        f.delete();
+		List<Hit> hits = new ArrayList<>();
+
+		hits = hitRead.get_RTPCHits();
+
+		if (hits == null || hits.size() == 0) {
+			return true;
+		}
+
+		int runNo = 10;
+		int eventNo = 777;
+		double magfield = 50.0;
+		double magfieldfactor = 1;
+
+		if (event.hasBank("RUN::config") == true) {
+			DataBank bank = event.getBank("RUN::config");
+			runNo = bank.getInt("run", 0);
+			eventNo = bank.getInt("event", 0);
+			magfieldfactor = bank.getFloat("solenoid", 0);
+			if (runNo <= 0) {
+				System.err.println("RTPCEngine:  got run <= 0 in RUN::config, skipping event.");
+				return false;
+			}
+		}
+
+		magfield = 50 * magfieldfactor;
+		IndexedTable global_parms = this.getConstantsManager().getConstants(runNo, "/calibration/rtpc/global_parms");
+		int hitsbound;
+		hitsbound = (int) global_parms.getDoubleValue("MaxHitsEvent", 0, 0, 0);
+		if (hits.size() > hitsbound) return true;
+
+		// reading the central detectors z shift with respect to the FD
+		IndexedTable rtpc_alignment = this.getConstantsManager().getConstants(runNo, "/geometry/rtpc/alignment");
+		float rtpc_vz_shift;
+		rtpc_vz_shift = (float) rtpc_alignment.getDoubleValue("deltaZ", 0, 0, 0);
+
+		if (event.hasBank("RTPC::adc")) {
+			params.init(this.getConstantsManager(), runNo);
+
+			SignalSimulation SS = new SignalSimulation(hits, params, simulation); //boolean is for simulation
+
+			//Sort Hits into Tracks at the Readout Pads
+			TrackFinder TF = new TrackFinder(params, cosmic);
+			//Calculate Average Time of Hit Signals
+			TimeAverage TA = new TimeAverage(this.getConstantsManager(), params, runNo);
+			//Disentangle Crossed Tracks
+			TrackDisentangler TD = new TrackDisentangler(params, disentangle, eventNo);
+			//Reconstruct Hits in Drift Region
+			TrackHitReco TR = new TrackHitReco(params, hits, cosmic, magfield);
+			//Helix Fit Tracks to calculate Track Parameters
+			HelixFitTest HF = new HelixFitTest(params, fitToBeamline, Math.abs(magfield), cosmic, chi2culling);
+
+			RecoBankWriter writer = new RecoBankWriter();
+			DataBank recoBank = writer.fillRTPCHitsBank(event, params);
+			DataBank trackBank = writer.fillRTPCTrackBank(event, params, rtpc_vz_shift);
+
+			event.appendBank(recoBank);
+			event.appendBank(trackBank);
+
+			if (kfStatus) {
+				HashMap<Integer, KalmanFitterInfo> KFTrackMap = new HashMap<>();
+				new KalmanFitter(params, KFTrackMap, magfield);
+				DataBank KFBank = writer.fillRTPCKFBank(event, KFTrackMap);
+				event.appendBank(KFBank);
+			}
+
+		} else {
+			return true;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+		System.setProperty("CLAS12DIR", "/Users/davidpayette/Desktop/newrtpcbranch/clas12-offline-software");
+		double starttime = System.nanoTime();
+
+		File f = new File("/Users/davidpayette/Desktop/SignalStudies/sig.txt");
+		f.delete();
+		f = new File("/Users/davidpayette/Desktop/SignalStudies/trackenergy.txt");
+		f.delete();
+		f = new File("/Users/davidpayette/Desktop/SignalStudies/timespectra.txt");
+		f.delete();
+		f = new File("/Users/davidpayette/Desktop/SignalStudies/sigafter.txt");
+		f.delete();
+		f = new File("/Users/davidpayette/Desktop/SignalStudies/sigTF.txt");
+		f.delete();
+		f = new File("/Users/davidpayette/Desktop/SignalStudies/timeenergy.txt");
+		f.delete();
+		f = new File("/Users/davidpayette/Desktop/SignalStudies/signalbins.txt");
+		f.delete();
 
 
-        //String inputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/good.hipo";
-        //String inputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/cosmics.hipo";
-        //String inputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/ctest.hipo";
-        //String inputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/new40p.hipo";
-        //String inputFile = "/Users/davidpayette/Desktop/rtpcbranch/1ep.hipo";
-        //String inputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/plugins/clas12/340_40p.hipo";
-        String inputFile = "/Users/davidpayette/Desktop/newrtpcbranch/input.hipo";
-        String outputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/out_cosmic.hipo";
+		//String inputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/good.hipo";
+		//String inputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/cosmics.hipo";
+		//String inputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/ctest.hipo";
+		//String inputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/new40p.hipo";
+		//String inputFile = "/Users/davidpayette/Desktop/rtpcbranch/1ep.hipo";
+		//String inputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/plugins/clas12/340_40p.hipo";
+		String inputFile = "/Users/davidpayette/Desktop/newrtpcbranch/input.hipo";
+		String outputFile = "/Users/davidpayette/Desktop/6b.2.0/myClara/out_cosmic.hipo";
 
-        System.err.println(" \n[PROCESSING FILE] : " + inputFile);
+		System.err.println(" \n[PROCESSING FILE] : " + inputFile);
 
-        RTPCEngine en = new RTPCEngine();
-        en.init();
+		RTPCEngine en = new RTPCEngine();
+		en.init();
 
 
-        EngineProcessor processor = new EngineProcessor();
-        processor.addEngine("RTPC", en);
-        processor.processFile(inputFile, outputFile);
+		EngineProcessor processor = new EngineProcessor();
+		processor.addEngine("RTPC", en);
+		processor.processFile(inputFile, outputFile);
 
         /*
         HipoDataSource reader = new HipoDataSource();
@@ -220,6 +218,6 @@ public class RTPCEngine extends ReconstructionEngine{
 
         writer.close();
         */
-        System.out.println("finished " + (System.nanoTime() - starttime)*Math.pow(10,-9));
-    }
+		System.out.println("finished " + (System.nanoTime() - starttime) * Math.pow(10, -9));
+	}
 }
