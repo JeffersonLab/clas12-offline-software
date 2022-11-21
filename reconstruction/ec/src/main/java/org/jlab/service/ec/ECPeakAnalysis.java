fae2af52a04cc8c5885337c26de847d5465a17ac
colesmith <colesmith@ls-imac.lan>
2022-11-20 21:57:45 -0500

    ECPeakAnalysis.java
    o Bug/typo fixes. Reproduces results from version with static globals.

--- reconstruction/ec/src/main/java/org/jlab/service/ec/ECPeakAnalysis.java
+++ reconstruction/ec/src/main/java/org/jlab/service/ec/ECPeakAnalysis.java
@@ -11,26 +11,26 @@ import java.util.List;
 public class ECPeakAnalysis {
     
     public static int[] getPeakSplitIndex(List<ECPeak> peaks){ 
-    	int [] split = {-1,-1}; 
+    	int [] split = {-1,-1,-1}; int[] zero = {-1,-1,-1}; 
         for(int i = 0; i < peaks.size(); i++){
             split[0] = peaks.get(i).getSplitIndex(ECCommon.splitMethod); //index of strip used to split peak
             split[1] = peaks.get(i).getSplitStrip(); //strip used to split peak
+            split[2] = i;
             if(split[0]>=0) return split; //index of peak tagged to be split
         }
-        return split;
+        return zero;
     }
     
     public static void splitPeaks(List<ECPeak> peaks){
-        int [] split;
         while(true){ //repeat processing all peaks until no split found
         	if(ECCommon.debugSplit) System.out.println(" ");
-            split = getPeakSplitIndex(peaks);
+            int[] split = getPeakSplitIndex(peaks);
         	if(ECCommon.debugSplit) System.out.println("New Iteration "+split[0]+" "+split[1]);
-            if(split[0]<0){
+            if(split[2]<0){
                 return; // no split was found in any peak.  Exit.
             } else {
-                ECPeak  peak = peaks.get(split[0]); //retrieve tagged peak with split candidate
-                peaks.remove(split[0]); //tagged peak removed from list 
+                ECPeak  peak = peaks.get(split[2]); //retrieve tagged peak with split candidate
+                peaks.remove(split[2]); //tagged peak removed from list 
                 peaks.addAll(peak.splitPeak(split[0])); //two split peaks returned to list
             }
         }
