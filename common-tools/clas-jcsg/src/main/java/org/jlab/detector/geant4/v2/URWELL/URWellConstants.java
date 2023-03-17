a9f74ecf02d3e4565064a8a28d594e92dc021626
mariangela-bondi <mariangela.bondi@ct.infn.it>
2023-03-17 20:23:45 +0100

    Iss977 urwell proto (#1092)
    * new geometry for Prototype and for Nregion>1
    
    * new geometry proto and Nregion>1 fix bug on URWellStripFactory.java
    
    ---------
    
    Co-authored-by: raffaelladevita <devita@ge.infn.it>

--- common-tools/clas-jcsg/src/main/java/org/jlab/detector/geant4/v2/URWELL/URWellConstants.java
+++ common-tools/clas-jcsg/src/main/java/org/jlab/detector/geant4/v2/URWELL/URWellConstants.java
@@ -2,13 +2,18 @@ package org.jlab.detector.geant4.v2.URWELL;
 
 
 import org.jlab.detector.calib.utils.DatabaseConstantProvider; 
-
+import eu.mihosoft.vrl.v3d.Vector3d;
+import org.jlab.geom.prim.Point3D;
+import org.jlab.geom.prim.Line3D;
+import org.jlab.geom.prim.Point3D;
+import org.jlab.geom.prim.Vector3D;
 
 
 public class URWellConstants {
 
     private final static String CCDBPATH = "/geometry/urwell/";
     
+    public final static int NMAXREGIONS  = 2;    //number of regions 
     public final static int NSECTORS  = 6;    //number of sectors
     public final static int NLAYERS   = 2;    //number of layers
     public final static int NCHAMBERS = 3;    //number of chambers in a sector
@@ -42,15 +47,41 @@ public class URWellConstants {
 
     // URWELL position in the CLAS12 frame 
     public final static double TGT2DC0    = 228.078; // cm            
-    public final static double URWELL2DC0 = 2;       // cm
-    public final static double DIST2TGT   = (TGT2DC0-URWELL2DC0);
-    public final static double W2TGT = DIST2TGT/Math.cos(Math.toRadians(THTILT-THMIN));
-    public final static double YMIN = W2TGT*Math.sin(Math.toRadians(THMIN)); // distance from the base chamber1 and beamline
-    public final static double ZMIN = W2TGT*Math.cos(Math.toRadians(THMIN)); 
-
+   // public final static double URWELL2DC0 = 2;       // cm
+    public final static double URWELL2DC0[] = new double[NMAXREGIONS];
+    public final static double DIST2TGT[] = new double[NMAXREGIONS];
+    public final static double W2TGT[] = new double[NMAXREGIONS];; 
+    public final static double YMIN[] = new double[NMAXREGIONS];
+    public final static double ZMIN[] = new double[NMAXREGIONS];
     
+  //  public final static double DIST2TGT   = (TGT2DC0-URWELL2DC0);
+   // public final static double W2TGT = DIST2TGT/Math.cos(Math.toRadians(THTILT-THMIN));
+  //  public final static double YMIN = W2TGT*Math.sin(Math.toRadians(THMIN)); // distance from the base chamber1 and beamline
+  //  public final static double ZMIN = W2TGT*Math.cos(Math.toRadians(THMIN));   
     public final static double PITCH = 0.1 ;       // cm
     public final static double STEREOANGLE = 10;   // deg
+    
+    
+    
+        // URWELL - PROTO information // 
+    /*
+    public final static double DX0_PROTO = 101.2; //cm
+    public final static double DX1_PROTO = 146.2; //cm
+    public final static double HEIGHT_PROTO = 50; //cm
+   */
+    // public final static int THILT_PROTO;            // theta tilt (deg)
+    public final static int NREGIONS_PROTO  = 1;    //number of regions 
+    public final static int NSECTORS_PROTO  = 1;    //number of sectors
+    public final static int NCHAMBERS_PROTO = 1;    //number of chambers in a sector
+    
+    
+        // URWELL-PROTO position in CLAS12 frame
+    
+    // 4 URWELL-PROTO vertex - sensitive plane
+    public static Point3D Apoint = new Point3D(22.9545, -337.005, 465.9); // {X,Y,Z}
+    public static Point3D Bpoint = new Point3D(148.906, -264.288, 465.9); // {X,Y,Z}
+    public static Point3D Cpoint = new Point3D(152.047, -314.276, 445.0); // {X,Y,Z}
+    public static Point3D Dpoint = new Point3D(64.676, -364.719, 445.0); // {X,Y,Z}
     
     /*
      * @return String a path to a directory in CCDB of the format {@code "/geometry/detector/"}
@@ -71,7 +102,7 @@ public class URWellConstants {
     {
           //  cp.loadTable( CCDBPATH +"RWELL");
 
-            load( cp );
+            load(cp  );
             return cp;
     }
 
@@ -80,10 +111,24 @@ public class URWellConstants {
      * Please use a DatabaseConstantProvider to access CCDB and load the following tables:
      * @param cp a ConstantProvider that has loaded the necessary tables
      */
+    
     public static synchronized void load( DatabaseConstantProvider cp )
     {
             // read constants from svt table
-      //      NREGIONS = cp.getInteger( CCDBPATH+"svt/nRegions", 0 );
+//            NREGIONS = cp.getInteger( CCDBPATH+"svt/nRegions", 0 );
 
+             for (int i=0; i<NMAXREGIONS; i++){
+                 
+                URWELL2DC0[NMAXREGIONS-i-1] =  2+i*2;
+                DIST2TGT[NMAXREGIONS-i-1]   = (TGT2DC0-URWELL2DC0[NMAXREGIONS-i-1]);
+                W2TGT[NMAXREGIONS-i-1] = DIST2TGT[NMAXREGIONS-i-1]/Math.cos(Math.toRadians(THTILT-THMIN));
+                YMIN[NMAXREGIONS-i-1]= W2TGT[NMAXREGIONS-i-1]*Math.sin(Math.toRadians(THMIN)); // distance from the base chamber1 and beamline
+                ZMIN[NMAXREGIONS-i-1] = W2TGT[NMAXREGIONS-i-1]*Math.cos(Math.toRadians(THMIN));  
+
+                }
+
+    
     }
+
+
 }
--- common-tools/clas-jcsg/src/main/java/org/jlab/detector/geant4/v2/URWELL/URWellStripFactory.java
+++ common-tools/clas-jcsg/src/main/java/org/jlab/detector/geant4/v2/URWELL/URWellStripFactory.java
@@ -7,7 +7,9 @@ import org.jlab.detector.calib.utils.DatabaseConstantProvider;
 import org.jlab.detector.hits.DetHit;
 import org.jlab.detector.volume.Geant4Basic;
 import org.jlab.geom.prim.Line3D;
+import org.jlab.geom.prim.Plane3D;
 import org.jlab.geom.prim.Point3D;
+import org.jlab.geom.prim.Vector3D;
 import org.jlab.geometry.prim.Line3d;
 import org.jlab.geometry.prim.Straight;
 import org.jlab.utils.groups.IndexedList;
@@ -22,8 +24,13 @@ public final class URWellStripFactory {
     URWellGeant4Factory factory;
     IndexedList<Line3D> globalStrips = new IndexedList(3);
     IndexedList<Line3D> localStrips  = new IndexedList(3);
+    IndexedList<Plane3D> planeStrips  = new IndexedList(3);
+    int NRegions;
+    int NSectors;
+    int NChambers;
+    int NLayers;
+    boolean Proto;
     
-
     public URWellStripFactory() {
     }
     
@@ -42,8 +49,41 @@ public final class URWellStripFactory {
      * @param cp
      */
     public void init(DatabaseConstantProvider cp) {
-        factory = new URWellGeant4Factory(cp);
+        this.init(cp, false, 1);
+    }
+   
+        /**
+     * Create the strip factory based on constants from CCDB.
+     * Currently constants are defined in the URWellConstants class. 
+     * They will be moved to CCDB when finalized).
+     * @param cp database provide
+     */
+    public URWellStripFactory(DatabaseConstantProvider cp, boolean isProto, int Nregions) {
+        this.init(cp, isProto, Nregions);
+    }
+    
+    /**
+     * Initialize the factory by the strip maps
+     * @param cp
+     */
+    public void init(DatabaseConstantProvider cp, boolean isProto, int Nregions) {
+        factory = new URWellGeant4Factory(cp, isProto, Nregions);
+        Proto = isProto;
+        if(isProto == false){
+         //   NRegions = URWellConstants.NREGIONS;
+            NSectors = URWellConstants.NSECTORS;
+            NChambers= URWellConstants.NCHAMBERS;
+            NLayers = URWellConstants.NLAYERS;
+
+        }else{
+            NRegions = URWellConstants.NREGIONS_PROTO;
+            NSectors = URWellConstants.NSECTORS_PROTO;
+            NChambers= URWellConstants.NCHAMBERS_PROTO;
+            NLayers = URWellConstants.NLAYERS;
+        }
+        
         this.fillStripLists();
+        this.fillPlaneLists();
     }
 
     /**
@@ -53,7 +93,7 @@ public final class URWellStripFactory {
      */
     public int getNStripSector() {
         int nStrips = 0;
-        for (int i = 0; i < URWellConstants.NCHAMBERS; i++) {
+        for (int i = 0; i < NChambers; i++) {
             nStrips += getNStripChamber(i);
         }
         return nStrips;
@@ -67,11 +107,11 @@ public final class URWellStripFactory {
      */
     public int getNStripChamber(int ichamber) {
 
-        double[] dim = factory.getChamberDimensions(ichamber);
+        double[] dim = factory.getChamber_daughter_Dimensions(ichamber);
 
-        double xHalfSmallBase = dim[0];
-        double xHalfLargeBase = dim[1];
-        double yHalf          = dim[2];
+        double yHalf          = dim[0];
+        double xHalfSmallBase = dim[1];
+        double xHalfLargeBase = dim[2];
 
         // C-------------D //
         //  -------------  //
@@ -88,7 +128,7 @@ public final class URWellStripFactory {
         int nAC = (int) (AC / (URWellConstants.PITCH
                 / Math.cos(theta - Math.toRadians(URWellConstants.STEREOANGLE))));
 
-        int nStrips = nAB + nAC + 1;
+        int nStrips = nAB + nAC +1 ;
 
         return nStrips;
     }
@@ -101,10 +141,14 @@ public final class URWellStripFactory {
      */
     public int getChamberIndex(int strip) {
         int nStripTotal = 0;
-        for(int i=0; i<URWellConstants.NCHAMBERS; i++) {
+
+        for(int i=0; i<NChambers; i++) {
             nStripTotal += this.getNStripChamber(i);
-            if(strip <= nStripTotal)
+            
+            if(strip <= nStripTotal){
                 return i;
+            }
+                
         }
         return -1;
     }
@@ -137,51 +181,67 @@ public final class URWellStripFactory {
     private Line3d createStrip(int sector, int layer, int strip) {
 
         int chamberIndex = getChamberIndex(strip);
-                
+          
         int cStrip = this.getLocalStripId(strip);
 
+       
         // CHAMBER reference frame
         // new numeration with stri ID_strip=0 crossing (0,0,0) of chamber
-        double[] dim = factory.getChamberDimensions(chamberIndex);
-
+        double[] dim = factory.getChamber_daughter_Dimensions(chamberIndex);
+        
+        double yHalf          = dim[0];
+        double xHalfSmallBase = dim[1];
+        double xHalfLargeBase = dim[2];
+        
+        
+        
         // Y coordinate of the intersection point between the x=0 and the strip line crossing for B
-        double DY = -dim[2] - Math.tan(Math.toRadians(URWellConstants.STEREOANGLE)) * dim[0];
 
+        double DY = -yHalf - Math.tan(Math.toRadians(URWellConstants.STEREOANGLE)) *xHalfSmallBase;
+       
         // ID of the strip 
         int nS = (int) (DY * Math.cos(Math.toRadians(URWellConstants.STEREOANGLE)) / URWellConstants.PITCH);
         int nCStrip = nS + (cStrip - 1);
-
+     
         //strip straight line chamber reference frame -> y = mx +c; 
         double stereoAngle = URWellConstants.STEREOANGLE;
-        if (layer % 2 != 0) {
+        if (layer % 2 == 0) {
             stereoAngle = -URWellConstants.STEREOANGLE;
         }
         double m = Math.tan(Math.toRadians(stereoAngle));
         double c = nCStrip * URWellConstants.PITCH / Math.cos(Math.toRadians(stereoAngle));
-
+   
         // Take 2 points in the strip straight line. They needs to define Line object 
-        double oX = -dim[1];
-        double oY = -dim[1] * m + c;
+        double oX = -xHalfLargeBase;
+        double oY = -xHalfLargeBase * m + c;
         double oZ = 0;
         Vector3d origin = new Vector3d(oX, oY, oZ);
 
-        double eX = dim[1];
-        double eY = dim[1] * m + c;
+        double eX = xHalfLargeBase;
+        double eY = xHalfLargeBase * m + c;
         double eZ = 0;
         Vector3d end = new Vector3d(eX, eY, eZ);
 
         // Get Chamber Volume
-        Geant4Basic chamberVolume = factory.getChamberVolume(sector, chamberIndex+1);
+        Geant4Basic chamberVolume = factory.getChamberVolume(sector, chamberIndex+1, layer, Proto);
+            
         // 2 point defined before wrt the GLOBAL frame     
         Vector3d globalOrigin = chamberVolume.getGlobalTransform().transform(origin);
+   
         Vector3d globalEnd    = chamberVolume.getGlobalTransform().transform(end);
 
-        Straight line = new Line3d(globalOrigin, globalEnd);
 
+        Straight line = new Line3d(globalOrigin, globalEnd);
+         
         // CHECK intersections between line and volume
         chamberVolume.makeSensitive();
         List<DetHit> Hits = chamberVolume.getIntersections(line);
+            
         if (Hits.size() >= 1) {
+      
+                Vector3d TestOrigin = Hits.get(0).origin();
+                Vector3d TestEnd = Hits.get(0).end();
+
             return new Line3d(Hits.get(0).origin(), Hits.get(0).end());
 
         } else {
@@ -196,10 +256,10 @@ public final class URWellStripFactory {
      * @param strip (1-N)
      * @return the 3D strip line as a Line3d
      */
-    private Line3d getLocalStrip(int sector, int layer, int strip) {
+    private Line3d getLocalStrip(int region, int sector, int layer, int strip) {
 
         Line3d globalStrip = createStrip(sector, layer, strip);
-        Geant4Basic sVolume = factory.getSectorVolume(sector);
+        Geant4Basic sVolume = factory.getSectorVolume(region, sector);
 
         Vector3d origin = sVolume.getGlobalTransform().invert().transform(globalStrip.origin());
         Vector3d end    = sVolume.getGlobalTransform().invert().transform(globalStrip.end());
@@ -207,6 +267,37 @@ public final class URWellStripFactory {
         Line3d localStrip = new Line3d(origin, end);
 
         return localStrip;
+    }
+    
+    
+    private void fillStripLists() {
+        
+        for(int ir=0; ir<NRegions; ir++) {
+            int region = ir+1;
+            for(int is=0; is<NSectors; is++) {
+                int sector = is+1;
+                if(Proto==true) sector =6;
+                for(int il=0; il<NLayers; il++) {
+                    int layer = (2*region-1) + il;
+
+                    for(int ic=0; ic<this.getNStripSector(); ic++) {
+                         int strip = ic+1;
+                       
+                        Line3d line = this.createStrip(sector, layer, strip);
+                                       
+                        Point3D origin = new Point3D(line.origin().x, line.origin().y, line.origin().z);
+
+                        Point3D end    = new Point3D(line.end().x,    line.end().y,    line.end().z);
+                     
+                        Line3D global = new Line3D(origin, end);
+                        Line3D local = this.toLocal(sector, global);
+                    
+                        this.globalStrips.add(global, sector, layer, strip);
+                        this.localStrips.add(local, sector, layer, strip);
+                    }
+                }
+            }
+        }
     }
     
     /**
@@ -218,30 +309,43 @@ public final class URWellStripFactory {
     public Line3D toLocal(int sector, Line3D global) {
         Line3D local = new Line3D();
         local.copy(global);
+  
         local.rotateZ(Math.toRadians(-60*(sector-1)));
         local.rotateY(Math.toRadians(-URWellConstants.THTILT));
+
+        
         return local;
     }
     
-    private void fillStripLists() {
+    private void fillPlaneLists() {
 
-        for(int is=0; is<URWellConstants.NSECTORS; is++) {
-            int sector = is+1;
-            for(int il=0; il<URWellConstants.NLAYERS; il++) {
-                int layer = il+1;
-                for(int ic=0; ic<this.getNStripSector(); ic++) {
-                    int strip = ic+1;
-                    Line3d line = this.createStrip(sector, layer, strip);
-                    Point3D origin = new Point3D(line.origin().x, line.origin().y, line.origin().z);
-                    Point3D end    = new Point3D(line.end().x,    line.end().y,    line.end().z);
-                    Line3D global = new Line3D(origin, end);
-                    Line3D local = this.toLocal(sector, global);
-                    this.globalStrips.add(global, is+1, il+1, ic+1);
-                    this.localStrips.add(local, is+1, il+1, ic+1);
+        for(int ir=0; ir<NRegions; ir++) {
+            int region = ir+1;
+            for(int is=0; is<NSectors; is++) {
+                int sector = is+1;
+                if(Proto==true) sector =6;
+                for(int il=0; il<NLayers; il++) {
+ 
+                    int layer = (2*region-1) + il;
+
+                    for(int ic=0; ic<this.getNStripSector(); ic++) {
+                         int strip = ic+1;
+                       
+                        Plane3D plane = this.createPLane(sector, layer, strip);
+                        this.planeStrips.add(plane, sector, layer, strip);
+
+                    }
                 }
             }
         }
     }
+        
+    
+    public Plane3D getPlane(int sector, int layer, int strip){
+        
+        return planeStrips.getItem(sector, layer, strip);
+    }
+    
     
     /**
      * Provides the 3D line for the given strip in the CLAS12 frame
@@ -251,6 +355,7 @@ public final class URWellStripFactory {
      * @return the 3D strip line in the CLAS12 frame as a Line3D
      */
     public Line3D getStrip(int sector, int layer, int strip) {
+
         return globalStrips.getItem(sector, layer, strip);
     }
     
@@ -265,17 +370,49 @@ public final class URWellStripFactory {
         return localStrips.getItem(sector, layer, strip);
     }
     
+    
+
+    
+    private Plane3D createPLane(int sector, int layer, int strip){
+     
+        int chamber = this.getChamberIndex(strip);
+
+        int LastStripID = this.getNStripChamber(chamber);
+        Line3D Last_strip = this.getStrip(sector, layer, LastStripID);
+       
+        Line3D First_strip = this.getStrip(sector, layer, 1);
+
+        Line3D test_strip = this.getStrip(sector, layer, 1);
+
+         
+        Vector3D Dir_strip_test = First_strip.originDir();
+    
+        /* Line orthogonal to the 2 strip */
+        Line3D line = First_strip.distance(Last_strip);
+        
+        Vector3D Dir_line = line.originDir();
+
+        Vector3D normal_plane = Dir_strip_test.cross(Dir_line);
+        if(Proto ==true) normal_plane = Dir_line.cross(Dir_strip_test);
+        
+        Plane3D plane = new Plane3D(First_strip.origin(), normal_plane);
+
+        return plane;
+    }
+    
+    
     public static void main(String[] args) {
         DatabaseConstantProvider cp = new DatabaseConstantProvider(11, "default");
 
         URWellConstants.connect(cp);
 
-        URWellGeant4Factory factory = new URWellGeant4Factory(cp);
-        URWellStripFactory factory2 = new URWellStripFactory(cp);
+        URWellGeant4Factory factory = new URWellGeant4Factory(cp,true,2);
 
-        Line3d strip_line = factory2.getLocalStrip(1, 0, 1600);
-        System.out.println(strip_line.toString());
+        URWellStripFactory factory2 = new URWellStripFactory(cp,true,2);
+  
+        Plane3D plane = factory2.getPlane(6, 1, 200);
 
+        
     }
 
 }
