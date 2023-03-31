4661c356c205881c51011416169253ae0e068379
Nathan Baltzell <baltzell@lappi4.mynetworksettings.com>
2023-03-31 16:24:17 -0400

    add yet another embedded string

--- common-tools/clas-detector/src/main/java/org/jlab/detector/decode/CLASDecoder4.java
+++ common-tools/clas-detector/src/main/java/org/jlab/detector/decode/CLASDecoder4.java
@@ -391,10 +391,10 @@ public class CLASDecoder4 {
 
         String[]        adcBankNames = new String[]{"FTOF::adc","ECAL::adc","FTCAL::adc","FTHODO::adc","FTTRK::adc",
                                                     "HTCC::adc","BST::adc","CTOF::adc","CND::adc","LTCC::adc","BMT::adc",
-                                                    "FMT::adc","HEL::adc","RF::adc","BAND::adc","RASTER::adc"};
+                                                    "FMT::adc","HEL::adc","RF::adc","BAND::adc","RASTER::adc","ECALTEST::adc"};
         DetectorType[]  adcBankTypes = new DetectorType[]{DetectorType.FTOF,DetectorType.ECAL,DetectorType.FTCAL,DetectorType.FTHODO,DetectorType.FTTRK,
                                                           DetectorType.HTCC,DetectorType.BST,DetectorType.CTOF,DetectorType.CND,DetectorType.LTCC,DetectorType.BMT,
-                                                          DetectorType.FMT,DetectorType.HEL,DetectorType.RF,DetectorType.BAND, DetectorType.RASTER};
+                                                          DetectorType.FMT,DetectorType.HEL,DetectorType.RF,DetectorType.BAND, DetectorType.RASTER, DetectorType.ECALTEST};
 
         String[]        tdcBankNames = new String[]{"FTOF::tdc","ECAL::tdc","DC::tdc","HTCC::tdc","LTCC::tdc","CTOF::tdc","CND::tdc","RF::tdc","RICH::tdc","BAND::tdc"};
         DetectorType[]  tdcBankTypes = new DetectorType[]{DetectorType.FTOF,DetectorType.ECAL,
