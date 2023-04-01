cac26f9e9e82d253fcd0ba328af02e7437af21f7
Nathan Baltzell <baltzell@lappi4.mynetworksettings.com>
2023-04-01 16:42:24 -0400

    cleanup

--- common-tools/clas-detector/src/main/java/org/jlab/detector/decode/ExtendedFADCFitter.java
+++ common-tools/clas-detector/src/main/java/org/jlab/detector/decode/ExtendedFADCFitter.java
@@ -3,163 +3,133 @@ package org.jlab.detector.decode;
 /**
  *
  * @author gavalian
+ * @author baltzell
  */
 public class ExtendedFADCFitter implements IFADCFitter {
 
-    private int pedistalMinBin = 1;
-    private int pedistalMaxBin = 5;
+    private int pedestalMinBin = 1;
+    private int pedestalMaxBin = 15;
+    public int t0;
+    public int adc;
+    public int ped;
+    public int thresholdCrossingBin;
+    public int pulsePeakValue;
+    public int pulsePeakBin;
+    public int pulseWidth;
+    public double rms;
+    private int tcoarse, tfine;
 
-    int p1=1,p2=15;
-    int mmsum,summing_in_progress;
-
-    public int     t0,adc,ped,pedsum;
-    public int     thresholdCrossing,pulsePeakValue, pulsePeakPosition, pulseWidth;
-    public double  baseline, rms;
-    private int    tcourse, tfine;
-
-    public ExtendedFADCFitter(){
-        
+    public ExtendedFADCFitter() {}
+    
+    public ExtendedFADCFitter(int pedMaxBin) {
+        this.pedestalMaxBin = pedMaxBin;
     }
 
-    public void fitCourseTime(int nsa, int nsb, int tet, int pedr, short[] pulse) {
-        pedsum=0;adc=0;mmsum=0;summing_in_progress=0;
-        for (int mm=0; mm<pulse.length; mm++) {
-            if(mm>p1 && mm<=p2)  pedsum+=pulse[mm];
-            if(mm==p2)           pedsum=pedsum/(p2-p1);				
-            if(mm>p2) {
-                if (pedr==0) ped=pedsum;
-                if (pedr!=0) ped=pedr;
-                if ((summing_in_progress==0) && pulse[mm]>ped+tet) {
-                    summing_in_progress=1;
-                    tcourse = mm;
-                    t0 = (tcourse << 6);
-                    for (int ii=1; ii<nsb+1;ii++) adc+=(pulse[mm-ii]-ped);
-                    mmsum=nsb;
-                }
-                if(summing_in_progress>0 && mmsum>(nsa+nsb)) summing_in_progress=-1;
-                if(summing_in_progress>0) {adc+=(pulse[mm]-ped); mmsum++;}
-            }
-        }
+    public ExtendedFADCFitter(int pedMinBin, int pedMaxBin) {
+        this.pedestalMinBin = pedMinBin;
+        this.pedestalMaxBin = pedMaxBin;
     }
 
-    public void fit(int nsa, int nsb, int tet, int pedr, short[] pulse) {
-        t0=0; adc=0; ped=0; pedsum=0; baseline=0; rms=0; 
-        thresholdCrossing=0; pulsePeakValue=0; pulsePeakPosition=0; pulseWidth=0;
-        tcourse=0; tfine=0;
-        double noise  = 0;
-        int    tstart = pedistalMaxBin+1;
-        int    tcross = 0;
-        int    pmax   = 0;
-        int    ppos   = 0;
-        // calculate pedestal means and noise
-        if (pedr!=0) ped=pedr;        // use default mode 7 pedestal range (1-4)
-        if(pulse.length<p2+1 && pedr==0) {
-            for (int bin = 0; bin < pulse.length; bin++) {
-                pedsum += pulse[bin];
-            }
-            ped=pedsum/pulse.length;
-            return;
+    private void calculatePedestal(int minBin, int maxBin, short[] waveform) {
+        int sum = 0;
+        int noise = 0;
+        for (int bin = minBin; bin <= maxBin; bin++) {
+            sum += waveform[bin];
+            noise  += waveform[bin] * waveform[bin];
         }
-        if (pedr==0) {
-            tstart = p2+1;
-            for (int bin = p1+1; bin < p2+1; bin++) {
-                pedsum += pulse[bin];
-                noise  += pulse[bin] * pulse[bin];
-            }
-            baseline = ((double) pedsum)/ (p2 - p1);
-            ped = pedsum=pedsum/(p2-p1);	//(int) baseline;
-            rms = Math.sqrt(noise / (p2 - p1) - baseline * baseline);
+        ped = sum / (maxBin - minBin + 1);	
+        rms = Math.sqrt(noise / (maxBin - minBin + 1) - ped*ped);
+    }
+
+    public boolean fit(int nsa, int nsb, int tet, int pedr, short[] waveform) {
+
+        t0=0;
+        adc=0;
+        thresholdCrossingBin=0;
+        pulsePeakValue=0;
+        pulsePeakBin=0;
+        pulseWidth=0;
+        tcoarse=0;
+        tfine=0;
+
+        // if user-supplied pedestal is nonzero, use it:
+        if (pedr!=0) ped = pedr;
+
+        // not enough samples, just use the whole window for pedestal and abort:
+        else if (waveform.length < pedestalMaxBin+1) {
+            calculatePedestal(0, waveform.length, waveform);
+            return false;
         }
+
+        // otherwise use the specified sample range:
+        else {
+            calculatePedestal(pedestalMinBin, pedestalMaxBin, waveform);
+        }
+
         // find threshold crossing
-        for (int bin=tstart; bin<pulse.length; bin++) {
-            if(pulse[bin]>ped+tet) {
-                tcross=bin;
-                thresholdCrossing=tcross;
+        for (int bin=pedestalMaxBin+1; bin<waveform.length; bin++) {
+            if(waveform[bin] > ped+tet) {
+                thresholdCrossingBin = bin;
                 break;
             }
         }
-        if(tcross>0) {
-            // calculate integral and find maximum
-            for (int bin=Math.max(0,tcross-nsb); bin<Math.min(pulse.length,tcross+nsa+1); bin++) { // sum should be up to tcross+nsa (without +1), this was added to match the old fit method
-                adc+=pulse[bin]-ped;
-                if(bin>=tcross && pulse[bin]>pmax) {
-                    pmax=pulse[bin];
-                    ppos=bin;
-                }
+
+        // if no threshold crossing, abort:
+        if(thresholdCrossingBin <= 0) {
+            return false;
+        }
+            
+        // calculate integral and find maximum:
+        final int firstBin = Math.max(0, thresholdCrossingBin - nsb);
+        final int lastBin = Math.min(waveform.length, thresholdCrossingBin + nsa);
+        for (int bin = firstBin; bin <= lastBin; bin++) {
+            adc += waveform[bin]-ped;
+            if(bin >= thresholdCrossingBin && waveform[bin] > pulsePeakValue) {
+                pulsePeakValue = waveform[bin];
+                pulsePeakBin = bin;
             }
-            pulsePeakPosition=ppos;
-            pulsePeakValue=pmax;
-            // calculating mode 7 pulse time
-            double halfMax = (pmax+baseline)/2;
-            int s0 = -1;
-            int s1 = -1;
-            for (int bin=tcross-1; bin<Math.min(pulse.length-1,ppos+1); bin++) {
-                if (pulse[bin]<=halfMax && pulse[bin+1]>halfMax) {
-                    s0 = bin;
-                    break;
-                }
+        }
+
+        // calculating mode 7 pulse time:
+        final double halfMax = (pulsePeakValue+ped)/2;
+        int s0 = -1;
+        int s1 = -1;
+
+        // find the leading-edge bin at half-height: 
+        for (int bin=thresholdCrossingBin-1; bin<Math.min(waveform.length-1,pulsePeakBin+1); bin++) {
+            if (waveform[bin]<=halfMax && waveform[bin+1]>halfMax) {
+                s0 = bin;
+                break;
             }
-            for (int bin=ppos; bin<Math.min(pulse.length-1,tcross+nsa); bin++) {
-                if (pulse[bin]>halfMax && pulse[bin+1]<=halfMax) {
-                    s1 = bin;
-                    break;
-                }
+        }
+
+        // find the trailing-edge bin at half-height:
+        for (int bin=pulsePeakBin; bin<Math.min(waveform.length-1,thresholdCrossingBin+nsa); bin++) {
+            if (waveform[bin]>halfMax && waveform[bin+1]<=halfMax) {
+                s1 = bin;
+                break;
             }
-            if(s0>-1) {
-                int a0 = pulse[s0];
-                int a1 = pulse[s0+1];
-                // set course time to be the sample before the 50% crossing
-                tcourse = s0;
-                // set the fine time from interpolation between the two samples before and after the 50% crossing (6 bits resolution)
-                tfine   = ((int) ((halfMax - a0)/(a1-a0) * 64));
-                t0      = (tcourse << 6) + tfine;
-            }
-            if(s1>-1 && s0>-1) {
-                int a0 = pulse[s1];
-                int a1 = pulse[s1+1];
+        }
+
+        // set the leading-edge times: 
+        if(s0 > 0) {
+            // set coarse time to be the sample before the 50% crossing
+            tcoarse = s0;
+            // set the fine time from interpolation between the two samples before and after the 50% crossing (6 bits resolution)
+            tfine   = ((int) ((halfMax - waveform[s0])/(waveform[s0+1]-waveform[s0]) * 64));
+            t0      = (tcoarse << 6) + tfine;
+            // set the width based on trailing-edge time:
+            if (s1 > 0) {
                 pulseWidth  = s1 - s0;
             }
         }
-    }
-    
-    private double findPedestal(DetectorDataDgtz.ADCData data){
-        int ped = 0;
-        for(int i = this.pedistalMinBin; i < this.pedistalMaxBin; i++){
-            ped += data.getPulseValue(i);
-        }
-        double ped_norm =   ((double) ped) / (Math.abs(this.pedistalMaxBin-this.pedistalMinBin));
-        return ped_norm;
-    }
-    
-    private int findMaximumADC(DetectorDataDgtz.ADCData data){
-        int bin = this.pedistalMaxBin ;
-        int max = data.getPulseValue(pedistalMaxBin);
-        
-        for(int i = this.pedistalMaxBin; i < data.getPulseSize(); i++){
-            if(data.getPulseValue(i)>max){
-                max = data.getPulseValue(i);
-                bin = i;
-            }
-        }
-        return bin;
-    }
-    
-    private int findPulseIntegral(DetectorDataDgtz.ADCData data, int maxValueBin, int nsa, int nsb){
-        int min_bin = maxValueBin - nsb;
-        int max_bin = maxValueBin + nsa;
-        if(min_bin>=0&&max_bin<data.getPulseSize()){
-            
-        } else {
-            System.out.println("MIN _ MAX out of range....");            
-        }
-        return 0;
+ 
+        return true;
     }
 
     @Override
     public void fit(DetectorDataDgtz.ADCData data) {
-        if(data.getPulseSize()>this.pedistalMaxBin){
-            double ped = this.findPedestal(data);
-        }
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
 }
