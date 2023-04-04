d18f37fb843fc82c944ccd2948ba724504e1821d
Nathan Baltzell <baltzell@baltzellmac.jlab.org>
2023-04-04 11:17:38 -0400

    restore sector matching for DC tracks

--- common-tools/clas-reco/src/main/java/org/jlab/clas/detector/matching/MatchTrajDistance.java
+++ common-tools/clas-reco/src/main/java/org/jlab/clas/detector/matching/MatchTrajDistance.java
@@ -2,6 +2,7 @@ package org.jlab.clas.detector.matching;
 
 import org.jlab.clas.detector.DetectorParticle;
 import org.jlab.clas.detector.DetectorResponse;
+import org.jlab.detector.base.DetectorType;
 import org.jlab.geom.prim.Point3D;
 
 /**
@@ -9,7 +10,7 @@ import org.jlab.geom.prim.Point3D;
  * @author baltzell
  */
 public class MatchTrajDistance extends AMatch {
-   
+
     private final double distanceLimit;
 
     public MatchTrajDistance(double distanceLimit) {
@@ -23,6 +24,11 @@ public class MatchTrajDistance extends AMatch {
 
     @Override
     public double quality(DetectorParticle p, DetectorResponse r) {
+        if (p.getTrack().getDetectorID() == DetectorType.DC.getDetectorId()) {
+            if (p.getTrack().getSector() != r.getSector()) {
+                return Double.POSITIVE_INFINITY;
+            }
+        }
         if (p.getTrackTrajectory().contains(r.getDescriptor())) { 
             Point3D traj = p.getTrackTrajectory().get(r.getDescriptor()).getCross().origin();
             return traj.distance(r.getPosition().toPoint3D());
